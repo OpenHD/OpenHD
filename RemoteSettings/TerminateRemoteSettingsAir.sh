6ea29f8686c98a1ae9380be098e155cb55cd3d86
Luke <pilotnbr1@gmail.com>
2019-01-29 01:32:19 +0100

    Htcohio fix for remote settings not terminating

--- RemoteSettings/TerminateRemoteSettingsAir.sh
+++ RemoteSettings/TerminateRemoteSettingsAir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ps -ef | nice grep "wfb_rx -c 127.0.0.1 -u 9393 -p 90" | nice grep -v grep | awk '{print $2}' | xargs kill -9
-ps -ef | nice grep "wfb_tx -m 10 -u 9292 -p 91" | nice grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | nice grep "wfb_tx -u 9292 -p 91 -B 20 -M 0" | nice grep -v grep | awk '{print $2}' | xargs kill -9
