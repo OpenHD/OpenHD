b9b1e90fface651bb14542bb335d7bfcf4a0de8c
user1321 <33780612+user1321@users.noreply.github.com>
2019-02-22 23:47:16 +0200

    Fix wrong param

--- RemoteSettings/Air/AudioTX.sh
+++ RemoteSettings/Air/AudioTX.sh
@@ -7,7 +7,7 @@ NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep 
 while true
 do
         echo "strt Audio transfer at channel 67 \n"
-        ./wfb_tx -m 2 -u 5051 -p 67 -B 20 -M 0 $NICS_LIST
+        ./wfb_tx -u 5051 -p 67 -B 20 -M 0 $NICS_LIST
 
         NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
         echo " Restating Audio Transfer with:  $NICS_LIST \n"
