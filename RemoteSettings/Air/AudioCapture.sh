4dc7aada85043af7457421bb497b98529be981c2
user1321 <33780612+user1321@users.noreply.github.com>
2019-02-23 01:05:09 +0200

    USB microphone volume adjust

--- RemoteSettings/Air/AudioCapture.sh
+++ RemoteSettings/Air/AudioCapture.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [ -e "/tmp/settings.sh" ]; then
+    OK=`bash -n /tmp/settings.sh`
+    if [ "$?" == "0" ]; then
+        source /tmp/settings.sh
+    else
+        echo "ERROR: openhd settings file contains syntax error(s)!"
+        collect_errorlog
+        sleep 365d
+    fi else
+    echo "ERROR: openhd settings file not found!"
+    collect_errorlog
+    sleep 365d
+fi
+
+amixer -c 1 sset Mic $MicBoost%
+
 while true
 do
 
