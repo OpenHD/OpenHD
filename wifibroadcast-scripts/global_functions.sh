d077d097e29e5dc0acfbdaadc4f8488d0a18d71e
Luke <pilotnbr1@gmail.com>
2019-03-02 23:36:04 +0100

    Changed IF statement for wifihotspot

--- wifibroadcast-scripts/global_functions.sh
+++ wifibroadcast-scripts/global_functions.sh
@@ -4,9 +4,6 @@ function tmessage {
     fi
 }
 
-function set_config(){
-    sudo sed -i "s/^\($1\s*=\s*\).*\$/\1$2/" $THISCONFIG
-}
 
 function check_exitstatus {
     STATUS=$1
@@ -504,7 +501,7 @@ function detect_nics {
 
     if [ "$CAM" == "0" ]; then # only do relay/hotspot stuff if RX
 	    # get wifi hotspot card out of the way
-	    if [ "$WIFI_HOTSPOT" == "Y" ]; then
+	    if [ "$WIFI_HOTSPOT" != "N" ]; then
 			if [ "$WIFI_HOTSPOT_NIC" != "internal" ]; then
 				# only configure it if it's there
 				if ls /sys/class/net/ | grep -q $WIFI_HOTSPOT_NIC; then
