94acd751efc2cba41fc9c73b0bfd42760ae69647
Consti10 <geierconstantinabc@gmail.com>
2019-03-12 14:35:00 +0100

    remove terminate

--- RemoteSettings2/Air/TxRemoteSettings.sh
+++ RemoteSettings2/Air/TxRemoteSettings.sh
@@ -5,7 +5,7 @@ cd /home/pi/cameracontrol/IPCamera/svpcom_wifibroadcast/
 NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
 
 
-while ! [ -e /tmp/IsTerminateRemoteSettingsPath.txt ]
+while true
 do
 	echo "start wfb_tx -u 9292 -p 91 -B 20 -M 0 $NICS_LIST \n"
 	./wfb_tx -u 9292 -p 91 -B 20 -M 0 $NICS_LIST
