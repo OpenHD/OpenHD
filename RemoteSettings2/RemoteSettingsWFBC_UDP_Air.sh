7c8ba643c859279cde30dc519e07b0843e4d355f
Consti10 <geierconstantinabc@gmail.com>
2019-03-15 12:09:28 +0100

    might be an issue

--- RemoteSettings2/RemoteSettingsWFBC_UDP_Air.sh
+++ RemoteSettings2/RemoteSettingsWFBC_UDP_Air.sh
@@ -3,4 +3,4 @@
 echo "starting remote settings scripts on air pi\n"
 /home/pi/RemoteSettings2/Air/TxRemoteSettings.sh &
 #sleep 0.5
-/home/pi/RemoteSettings2/Air/RxRemoteSettings.sh 
+/home/pi/RemoteSettings2/Air/RxRemoteSettings.sh &
