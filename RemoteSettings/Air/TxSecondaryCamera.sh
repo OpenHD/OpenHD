ce947f1285ee73728d3b42ff6bb40300e90d1015
zipray <zipray@hotmail.com>
2019-05-13 14:53:56 +0800

    Update TxSecondaryCamera.sh

--- RemoteSettings/Air/TxSecondaryCamera.sh
+++ RemoteSettings/Air/TxSecondaryCamera.sh
@@ -7,7 +7,7 @@ NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep 
 
 while  [ 1 ]
 do
-	echo "start wfb_tx -u 9292 -p 91 -B 20 -M 0 $NICS_LIST \n"
+	echo "start wfb_tx -u 5600 -t 2 -p 23 -B 20 -M 0 $NICS_LIST \n"
 	./wfb_tx -u 5600 -t 2 -p 23 -B 20 -M 0 $NICS_LIST
 
 	NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
