c096899fb246e4b1ca3f3014624a7d3c0fd6bfbb
user1321 <33780612+user1321@users.noreply.github.com>
2019-06-09 16:56:49 +0300

    autodetect imx290 part2

--- wifibroadcast-scripts/global_functions.sh
+++ wifibroadcast-scripts/global_functions.sh
@@ -119,7 +119,14 @@ function check_camera_attached {
 	if [ "$TTY" == "/dev/tty1" ]; then
 		CAM=`/usr/bin/vcgencmd get_camera | nice grep -c detected=1`
 		if [ "$CAM" == "0" ]; then # if we are RX ...
-			echo  "0" > /tmp/cam
+			i2cdetect -y 0 | grep  "30: -- -- -- -- -- -- -- -- -- -- -- 3b -- -- -- --"
+        		grepRet=$?
+        		if [[ $grepRet -eq 0 ]] ; then
+				echo  "1" > /tmp/cam
+				CAM="1"
+		        else
+				echo  "0" > /tmp/cam
+			fi
 		else # else we are TX ...
 			touch /tmp/TX
 			echo  "1" > /tmp/cam
