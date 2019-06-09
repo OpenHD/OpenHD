febe564d1ed4f1cceb48fc294023b96f6f9a76e6
user1321 <33780612+user1321@users.noreply.github.com>
2019-06-10 02:34:38 +0300

    Update KillUSBCamera.sh

--- RemoteSettings/KillUSBCamera.sh
+++ RemoteSettings/KillUSBCamera.sh
@@ -1,1 +1,2 @@
 ps -ef | nice grep "gst-launch-1.0 uvch264src device=/dev/vid" | nice grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | nice grep "gst-launch-1.0 v4l2src device=/dev/vid" | nice grep -v grep | awk '{print $2}' | xargs kill -9
