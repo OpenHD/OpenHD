90850cc93f746d89bc36150ce924e6b9cb49b6f6
Petru Soroaga <petrusoroaga@yahoo.com>
2019-06-14 16:48:55 +0300

    Use default gpio pins

--- RemoteSettings/Ground/RemoteSettingsSync.py
+++ RemoteSettings/Ground/RemoteSettingsSync.py
@@ -34,12 +34,12 @@ print("Selected: " + SelectedControl)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-input_state0 = GPIO.input(23)
-input_state1 = GPIO.input(24)
+input_state0 = GPIO.input(20)
+input_state1 = GPIO.input(21)
 input_state2 = GPIO.input(7)
 nb = 1
 if not input_state0:
