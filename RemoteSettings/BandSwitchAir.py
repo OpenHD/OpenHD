f4c195dbbafbfd7e9c4e2055b7023b47953413b3
user1321 <33780612+user1321@users.noreply.github.com>
2019-07-21 22:28:48 +0300

    Add files via upload

--- RemoteSettings/BandSwitchAir.py
+++ RemoteSettings/BandSwitchAir.py
@@ -21,8 +21,8 @@ lock = threading.Lock()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-DefaultBandWidthAth9k", type=int, help="")
-parser.add_argument("-SecondaryCamera", help="")
-parser.add_argument("-BitrateMeasured", type=int, help="")
+
+
 
 args = parser.parse_args()
 tmp = args.DefaultBandWidthAth9k
@@ -36,17 +36,13 @@ elif tmp == 5:
 else:
     CurrentBand = "0"
 
-CameraType = "RPi"
 
-SecondaryCamera = args.SecondaryCamera
-BitrateMeasured = args.BitrateMeasured
+
 
 InMsgBand5 = bytearray(b'5') #Air to 5
 InMsgBand10 = bytearray(b'a') #Air to 10
 InMsgBand20 = bytearray(b'0') #Air to 20
-InMsgCameraTypeRPi = bytearray(b'RPi')
-InMsgCameraTypeRPiAndSecondary = bytearray(b'RPiAndSecondary')
-InMsgCameraTypeSecondary = bytearray(b'Secondary')
+
 
 PrimaryCardPath = "Non"
 IsAth9kFound = False
@@ -106,130 +102,61 @@ def AirToGroundRSSI():
         sleep(0.5)
 
 
-def SwitchCamera():
-    print("CameraType: ", CameraType)
+def NotifyCameraControl(MessageBuf):
     print("CurrentBand: ", CurrentBand )
-    if CameraType == "RPi":
-        try:
-            os.system('/home/pi/RemoteSettings/KillIPCamera.sh')
-            os.system('/home/pi/RemoteSettings/KillUSBCamera.sh')
-            os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
-            if CurrentBand == "0":
-                os.system('/dev/shm/startReadCameraTransfer.sh &')
-            if CurrentBand == "a":
-                os.system('/dev/shm/startReadCameraTransfer_10.sh &')
-            if CurrentBand == "5":
-                os.system('/dev/shm/startReadCameraTransfer_5.sh &')
-        except Exception as e:
-            print(e)
-
-    if CameraType == "RPiAndSecondary":
-        try:
-            BitrateSecondaryCamera = 1000000
-            BitrateMainCamera = 1000000
-            if CurrentBand == "0":
-                BitrateSecondaryCamera = int(BitrateMeasured / 2)
-                BitrateMainCamera = int(BitrateMeasured / 2)
-            if CurrentBand == "a":
-                BitrateSecondaryCamera = int(BitrateMeasured / 4)
-                BitrateMainCamera = int(BitrateMeasured / 4)
-            if CurrentBand == "5":
-                BitrateSecondaryCamera = int(BitrateMeasured / 8)
-                BitrateMainCamera = int(BitrateMeasured / 8)
+    #add code that will send message over UDP  4324 to cameracontrolUDP.py
+    UDP_PORT = 4324
+    sockToAir = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sockToAir.sendto( bytes(MessageBuf,'utf-8'), ('127.0.0.1', UDP_PORT))
 
 
-            print("BitrateMainCamera: ",  BitrateMainCamera )
-            print("BitrateUSBCamera: ", BitrateSecondaryCamera )
-            os.system('/home/pi/RemoteSettings/KillIPCamera.sh')
-            os.system('/home/pi/RemoteSettings/KillUSBCamera.sh')
-            os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
-            subprocess.Popen( ['/dev/shm/startReadCameraTransferExteranlBitrate.sh', str(BitrateMainCamera) ] )
-
-            if SecondaryCamera == "IP":
-                os.system('/home/pi/RemoteSettings/KillIPCamera.sh')
-                os.system('/dev/shm/startReadIPCameraLowRes.sh &')
-            if SecondaryCamera == "USB":
-                os.system('/home/pi/RemoteSettings/KillUSBCamera.sh')     
-                subprocess.Popen( ['/dev/shm/startReadUSBCamera.sh', str(BitrateSecondaryCamera) ] )
-        except Exception as e:
-            print(e)
-    if CameraType == "Secondary":
-        try:
-            BitrateSecondaryCamera = 1000000
-            if CurrentBand == "0":
-                BitrateSecondaryCamera = int(BitrateMeasured) 
-            if CurrentBand == "a":
-                BitrateSecondaryCamera = int(BitrateMeasured / 2)
-            if CurrentBand == "5":
-                BitrateSecondaryCamera = int(BitrateMeasured / 4)
-
-            os.system('/home/pi/RemoteSettings/KillIPCamera.sh')
-            os.system('/home/pi/RemoteSettings/KillUSBCamera.sh')
-            os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
-
-            if SecondaryCamera == "IP":
-                os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
-                os.system('/home/pi/RemoteSettings/KillIPCamera.sh')
-                os.system('/dev/shm/startReadIPCameraHiRes.sh &')
-            if SecondaryCamera == "USB":
-                os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
-                os.system('/home/pi/RemoteSettings/KillUSBCamera.sh')
-                subprocess.Popen( ['/dev/shm/startReadUSBCamera.sh', str(BitrateSecondaryCamera) ] )
-        except Exception as e:
-            print(e)
 
 
 def StartRecv():
     global CurrentBand
-    global CameraType
     UDP_IP = ""
-    UDP_PORT = 4321 #3033 - UDP DownLink from Ground
+    UDP_PORT = 4322 #3033 - UDP DownLink from Ground
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 	#switch band only with ath9k
         if IsAth9kFound == True:
-            if data == InMsgBand5:
+            if data == InMsgBand5 and CurrentBand != "5":
                 print("InMsgBand5\n")
                 if SwitchLocalBandTo(PrimaryCardPath,5) == True:
                     print("InMsgBand5\n")
+                    lock.acquire()
+                    CurrentBand = "5"
+                    lock.release()
+                    NotifyCameraControl(CurrentBand)
                     #os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
                     #os.system('/dev/shm/startReadCameraTransfer_5.sh &')
-                lock.acquire()
-                CurrentBand = "5"
-                lock.release()
-                SwitchCamera()
-            if data == InMsgBand10:
+
+                
+            if data == InMsgBand10 and CurrentBand != "a":
                 print("InMsgBand10\n")
                 if SwitchLocalBandTo(PrimaryCardPath,10) == True:
                     print("InMsgBand10\n")
+                    lock.acquire()
+                    CurrentBand = "a"
+                    lock.release()
+                    NotifyCameraControl(CurrentBand)
                     #os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
                     #os.system('/dev/shm/startReadCameraTransfer_10.sh &')
-                lock.acquire()
-                CurrentBand = "a"
-                lock.release()
-                SwitchCamera()
-            if data == InMsgBand20:
+
+                
+            if data == InMsgBand20 and CurrentBand != "0":
                 print("InMsgBand20\n")
                 if SwitchLocalBandTo(PrimaryCardPath,20) == True:
                     print("InMsgBand20\n")
+                    lock.acquire()
+                    CurrentBand = "0"
+                    lock.release()
+                    NotifyCameraControl(CurrentBand)
                     #os.system('/home/pi/RemoteSettings/KillRaspivid.sh')
                     #os.system('/dev/shm/startReadCameraTransfer.sh &')
-                lock.acquire()
-                CurrentBand = "0"
-                lock.release()
-                SwitchCamera()
-	#Switch camera with any wifi card
-        if data == InMsgCameraTypeRPi and CameraType != "RPi":
-            CameraType = "RPi"
-            SwitchCamera()
-        if data == InMsgCameraTypeRPiAndSecondary and CameraType != "RPiAndSecondary":
-            CameraType = "RPiAndSecondary"
-            SwitchCamera()
-        if data == InMsgCameraTypeSecondary and CameraType != "Secondary":
-            CameraType = "Secondary"
-            SwitchCamera()
+                 
         print(".")
 
 
