f3a48515fe86b63f97c633cd7c6330f3e2c3e843
user1321 <33780612+user1321@users.noreply.github.com>
2019-07-24 18:51:08 +0300

    suppor more than one remove device

---
+++ RemoteSettings/Ground/KillForwardRTPSecondaryCamera.sh
@@ -0,0 +1,4 @@
+ps -ef | nice grep "wfb_rx -u 5621 -p 23 -c 127.0.0.1" | nice grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | nice grep "wfb_rx -u 5612 -p 23 -c 127.0.0.1" | nice grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | nice grep "hello_video.bin.240-befi" | nice grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | nice grep "gst-launch-1.0 udpsrc port=5612" | nice grep -v grep | awk '{print $2}' | xargs kill -9
--- RemoteSettings/Ground/RxForwardSecondaryRTP.sh
+++ RemoteSettings/Ground/RxForwardSecondaryRTP.sh
@@ -18,6 +18,6 @@ cd /home/pi/cameracontrol/IPCamera/svpcom_wifibroadcast/
 
 NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
 
-./wfb_rx -u $VIDEO_UDP_PORT2 -p 23 -c 192.168.2.2 $NICS_LIST >/dev/null 2>/dev/null
+./wfb_rx -u 5621 -p 23 -c 127.0.0.1 $NICS_LIST >/dev/null 2>/dev/null
 
 
