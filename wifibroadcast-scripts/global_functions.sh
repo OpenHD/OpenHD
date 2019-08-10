aa78abe2764aba57485243e4fb01c9a499322b57
user1321 <33780612+user1321@users.noreply.github.com>
2019-08-10 20:47:20 +0300

    80211n modes and STBC part 1

--- wifibroadcast-scripts/global_functions.sh
+++ wifibroadcast-scripts/global_functions.sh
@@ -195,21 +195,45 @@ function datarate_to_wifi_settings {
 		UPLINK_WIFI_BITRATE=11
 		TELEMETRY_WIFI_BITRATE=11
 		VIDEO_WIFI_BITRATE=5.5
+                if [ "$UseMCS" == "Y" ]; then
+                        UseMCS="1"
+                        VIDEO_WIFI_BITRATE="0"
+		else
+			UseMCS="0"
+		fi
 		;;
 		2)
 		UPLINK_WIFI_BITRATE=11
 		TELEMETRY_WIFI_BITRATE=11
 		VIDEO_WIFI_BITRATE=11
+                if [ "$UseMCS" == "Y" ]; then
+                        UseMCS="1"
+                        VIDEO_WIFI_BITRATE="1"
+                else
+                        UseMCS="0"
+                fi
 		;;
 		3)
 		UPLINK_WIFI_BITRATE=11
 		TELEMETRY_WIFI_BITRATE=12
 		VIDEO_WIFI_BITRATE=12
+                if [ "$UseMCS" == "Y" ]; then
+                        UseMCS="1"
+                        VIDEO_WIFI_BITRATE="1"
+                else
+                        UseMCS="0"
+                fi
 		;;
 		4)
 		UPLINK_WIFI_BITRATE=11
 		TELEMETRY_WIFI_BITRATE=19.5
 		VIDEO_WIFI_BITRATE=19.5
+                if [ "$UseMCS" == "Y" ]; then
+                        UseMCS="1"
+                        VIDEO_WIFI_BITRATE="2"
+                else
+                        UseMCS="0"
+                fi
 		;;
 		5)
 		UPLINK_WIFI_BITRATE=11
@@ -222,6 +246,17 @@ function datarate_to_wifi_settings {
 		VIDEO_WIFI_BITRATE=36
 		;;
 	esac
+
+        if [ "$UseSTBC" == "Y" ]; then
+                UseSTBC="1"
+        else
+                UseSTBC="0"
+        fi
+        if [ "$UseLDPC" == "Y" ]; then
+                UseLDPC="1"
+        else
+                UseLDPC="0"
+        fi
 }
 
 function set_video_player_based_fps {
