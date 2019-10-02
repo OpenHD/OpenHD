4e713cf48a63a0d1ade9ced04947e6a063463061
Luke <pilotnbr1@gmail.com>
2019-10-02 14:43:46 +0200

    Added eagletree support

--- wifibroadcast-osd/main.c
+++ wifibroadcast-osd/main.c
@@ -51,6 +51,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "mavlink.h"
 #elif defined(SMARTPORT)
 #include "smartport.h"
+#elif defined(VOT)
+#include "vot.h"
 #endif
 
 long long current_timestamp() {
@@ -163,6 +165,8 @@ int main(int argc, char *argv[]) {
 		do_render = mavlink_read(&td, buf, n);
 #elif defined(SMARTPORT)
 		smartport_read(&td, buf, n);
+#elif defined(VOT)
+		do_render =  vot_read(&td, buf, n);
 #endif
 	    }
 	    counter++;
