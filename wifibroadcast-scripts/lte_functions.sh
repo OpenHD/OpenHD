a80c72b138ba71094b5396c1f9e219b2b0df5c53
pilotnbr1 <pilotnbr1@gmail.com>
2020-02-03 23:11:06 +0100

    Made lte functions dynamic

--- wifibroadcast-scripts/lte_functions.sh
+++ wifibroadcast-scripts/lte_functions.sh
@@ -1,15 +1,17 @@
 function lte_function {  
 
-	PI_NIC = `ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot | nice grep -v $NIC_BLACKLIST`
+	PI_NIC = "ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot | nice grep -v $NIC_BLACKLIST"
 
-	sudo touch /var/lib/zerotier-one/local.conf
+echo "PI_NIC=$PI_NIC"
+
+	#sudo touch /var/lib/zerotier-one/local.conf
 
 	echo `{
         	"settings": {
-                	"interfacePrefixBlacklist": [ "eth0","wifihotspot0","$PI_NIC","lo" ],
+                	"interfacePrefixBlacklist": [ "eth0","wifihotspot0","`"$PI_NIC"`","lo" ],
                 	"allowTcpFallbackRelay": false
         		}
-		}` >> /var/lib/zerotier-one/local.conf
+		}` > /var/lib/zerotier-one/local.conf
 
 
 	sudo systemctl start zerotier-one
