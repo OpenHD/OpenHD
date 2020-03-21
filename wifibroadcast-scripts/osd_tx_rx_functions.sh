fe94bd57adb1d0c152d4f1dbea7053a1f79b190c
Stephen Oliver <steve@infincia.com>
2020-03-20 22:56:53 -0400

    Fix whitespace and indentation in osd_tx_rx_func.

--- wifibroadcast-scripts/osd_tx_rx_functions.sh
+++ wifibroadcast-scripts/osd_tx_rx_functions.sh
@@ -2,18 +2,18 @@ source osd_rx_functions.sh
 source osd_tx_functions.sh
 
 function MAIN_OSD_TX_RX_FUNCTION {
-	echo "================== OSD (tty2) ==========================="
-	
-	# only run osdrx if no cam found
-	if [ "$CAM" == "0" ]; then
-	    osdrx_function
-	else
-	    # only run osdtx if cam found, osd enabled and telemetry input is the tx
-	    if [ "$CAM" == "1" ] && [ "$TELEMETRY_TRANSMISSION" == "wbc" ]; then
-	        osdtx_function
-	    fi
-	fi
-	
+    echo "================== OSD (tty2) ==========================="
+    
+    # only run osdrx if no cam found
+    if [ "$CAM" == "0" ]; then
+        osdrx_function
+    else
+        # only run osdtx if cam found, osd enabled and telemetry input is the tx
+        if [ "$CAM" == "1" ] && [ "$TELEMETRY_TRANSMISSION" == "wbc" ]; then
+            osdtx_function
+        fi
+    fi
+    
     echo "OSD not enabled in configfile"
-	sleep 365d
+    sleep 365d
 }
