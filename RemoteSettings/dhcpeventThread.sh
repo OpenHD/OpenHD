7030b8644af12a8bb425c7b67bec00afbe55d14d
Stephen Oliver <steve@infincia.com>
2020-06-02 22:48:24 -0400

    Fix log level on status messages

--- RemoteSettings/dhcpeventThread.sh
+++ RemoteSettings/dhcpeventThread.sh
@@ -32,7 +32,7 @@ if [[ $op == "old" || $op == "add" ]]; then
 	echo "add udp $IP $IP 14550 0" > /tmp/mavlink_router_pipe
 	if [ "$QUIET" == "N" ]; then
             if [ "$ENABLE_QOPENHD" == "Y" ]; then
-                qstatus "External device connected: $IP" 3
+                qstatus "External device connected: $IP" 5
             else
                 wbc_status "External device connected: $IP" 5 55 0 &
             fi
@@ -61,7 +61,7 @@ if [[ $op == "old" || $op == "add" ]]; then
 
         if [ "$QUIET" == "N" ]; then
                 if [ "$ENABLE_QOPENHD" == "Y" ]; then
-                    qstatus "External device gone: $IP" 3
+                    qstatus "External device gone: $IP" 5
                 else
                     wbc_status "External device gone: $IP" 5 55 0 &
                 fi
