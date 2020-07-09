33f96e3a3c9f6390054737bdfcf9f03d3046bce3
Stephen Oliver <steve@infincia.com>
2020-07-09 12:10:00 -0400

    Don't stop hotspot video because of 1 ping fail
    When hotspot clients connect, they request an IP address using DHCP
    from dnsmasq, which then starts this script as a sort of callback
    event.
    
    This script then registers that device with the various telemetry and
    video streams and causes the device to start receiving them. It then
    periodically sends a ping to the device to see if it's still
    connected. However there are cases where a connected device might not
    respond to pings quick enough, which would cause the video to stop.
    
    We will now allow devices 10 seconds to reply to each ping, and if 6
    pings in a row fail then we consider the device disconnected. In
    other words, if the device doesn't respond to pings for a full minute
    we stop the video and telemetry streams.
    
    We can afford to continue sending video to devices that are no longer
    connected for 60 seconds without causing much trouble, but the
    reverse case where a connected device suddenly stops receiving video,
    potentially during flight, is not something we can allow to happen.

--- RemoteSettings/dhcpeventThread.sh
+++ RemoteSettings/dhcpeventThread.sh
@@ -36,21 +36,27 @@ if [[ ${COMMAND} == "old" || ${COMMAND} == "add" ]]; then
         fi
     fi
 
+    PINGFAIL=0
     IPTHERE=1
     while [ ${IPTHERE} -eq 1 ]; do
         ping -c 3 -W 1 -n -q ${IP} > /dev/null 2>&1
         if [ $? -eq 0 ]; then
             IPTHERE=1
+            PINGFAIL=0
         else
-            ping -c 3 -W 1 -n -q ${IP} > /dev/null 2>&1
+            ping -c 1 -W 10 -n -q ${IP} > /dev/null 2>&1
             if [ $? -ne 0 ]; then
-                IPTHERE=0
-                
-                echo "del ${IP}" > /dev/udp/127.0.0.1/9120
-                echo "del ${IP}" > /dev/udp/127.0.0.1/9121
-                echo "del ${IP}" > /dev/udp/127.0.0.1/9122
-                echo "del ${IP}" > /dev/udp/127.0.0.1/9123
-                echo "del ${IP}" > /dev/udp/127.0.0.1/9124
+                if [ ${PINGFAIL} -ge 6 ]; then
+                    IPTHERE=0
+
+                    echo "del ${IP}" > /dev/udp/127.0.0.1/9120
+                    echo "del ${IP}" > /dev/udp/127.0.0.1/9121
+                    echo "del ${IP}" > /dev/udp/127.0.0.1/9122
+                    echo "del ${IP}" > /dev/udp/127.0.0.1/9123
+                    echo "del ${IP}" > /dev/udp/127.0.0.1/9124
+                else
+                    PINGFAIL=$((PINGFAIL+1))
+                fi
             fi
         fi
 
