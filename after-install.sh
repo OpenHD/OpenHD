98261746436657553083d23fd4926a4cf7e2e9b3
Stephen Oliver <steve@infincia.com>
2020-07-21 16:58:21 -0400

    Use after-install hook to change fstab
    This was hardcoding a specific partition layout that will only match
    the pi, and would not have worked for things like USB boot either.
    
    It also causes problems during image building when a package contains
    the fstab file.

--- after-install.sh
+++ after-install.sh
@@ -11,4 +11,23 @@ touch /boot/ssh || true
 # crude hack to avoid making people put fonts somewhere else
 cp -a /usr/local/share/openhd/osdfonts/*.ttf /boot/osdfonts/ > /dev/null 2>&1 || true
 
+# these are intentionally written with spaces around them to avoid false negatives if people
+# edit the fstab file and change the rest of the line, the way these are written it will still
+# find them as long as a line for each of these mountpoints is present
+grep " /tmp " /etc/fstab
+if [[ "$?" -ne 0 ]]; then
+    echo "tmpfs /tmp tmpfs nosuid,nodev,noatime,size=10M 0 0" >> /etc/fstab
+fi
+
+grep " /var/log " /etc/fstab
+if [[ "$?" -ne 0 ]]; then
+    echo "tmpfs /var/log tmpfs nosuid,nodev,noatime,size=10M 0 0" >> /etc/fstab
+fi
+
+grep " /var/tmp " /etc/fstab
+if [[ "$?" -ne 0 ]]; then
+    echo "tmpfs /var/tmp tmpfs nosuid,nodev,noatime,size=10M 0 0" >> /etc/fstab
+fi
+
+
 mount -oremount,ro /boot || true
