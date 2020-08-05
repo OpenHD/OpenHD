cbfa0cc8da629f5d10ec3ad3ee1e79531f91f29f
Stephen Oliver <steve@infincia.com>
2020-08-05 17:20:34 -0400

    Allow 8188eu cards to use RTS frames for telem

--- wifibroadcast-base/tx_telemetry.c
+++ wifibroadcast-base/tx_telemetry.c
@@ -521,6 +521,8 @@ int main(int argc, char *argv[]) {
         if (strncmp(line, "DRIVER=ath9k_htc", 16) == 0 ||
             strncmp(line, "DRIVER=88x2bu",    13) == 0 ||
             strncmp(line, "DRIVER=rtl88x2bu", 16) == 0 ||
+            strncmp(line, "DRIVER=8188eu",    13) == 0 ||
+            strncmp(line, "DRIVER=rtl8188eu", 16) == 0 ||
            (strncmp(line, "DRIVER=8812au",    13) == 0 ||
             strncmp(line, "DRIVER=8814au",    13) == 0 ||
             strncmp(line, "DRIVER=rtl8812au", 16) == 0 ||
