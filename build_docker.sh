e4fce741d6f7c043508c45e7f866462571a1e75e
LimitlessGeen <limitlessgreen@outlook.de>
2020-08-07 17:15:07 +0200

    Only build locally if image dosn't exist

--- build_docker.sh
+++ build_docker.sh
@@ -4,7 +4,7 @@ PLATFORM=$1
 DISTRO=$2   
 
 # only if ENV not set
-if [ -z "${DOCKER_USERNAME}" ]; then DOCKER_USERNAME=openhd; fi
+if [ -z "$DOCKER_USERNAME" ]; then DOCKER_USERNAME=openhd; fi
 
 DOCKERFILE=Dockerfile-${PLATFORM}-${DISTRO}
 CONTAINER=openhd_${PLATFORM}_${DISTRO}_build
@@ -24,32 +24,39 @@ if ! $DOCKER ps > /dev/null; then
 fi
 set -e
 
-function create_docker_container() {
-    if $DOCKER create -it -v $PWD:/src --name ${CONTAINER} --env CLOUDSMITH_API_KEY=$CLOUDSMITH_API_KEY ${IMAGE} ; then
-        return 0
-    else
-        return 1
-    fi
-}
 
 # Only (re)build image, if SHA of Dockerfile has changed
 function build_docker_image() {
 
-    if ! create_docker_container ; then
-        $DOCKER rm -v ${CONTAINER} > /dev/null 2>&1 || true
-	    $DOCKER build -f=./${DOCKERFILE} -t=${IMAGE} .
-
-        if [[ -n $TRAVIS ]]; then
-            echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || true
-            $DOCKER push ${IMAGE} || true
-        fi
-        create_docker_container
+    $DOCKER build -f=./${DOCKERFILE} -t=${IMAGE} .
+    if [[ -n $TRAVIS ]]; then
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || true
+        $DOCKER push ${IMAGE} || true
     fi
 }
+
+
+function create_docker_container() {
+    if [[ (! -n "$($DOCKER images -q ${IMAGE})") && (-n "$($DOCKER images -q $DOCKER_USERNAME/${PLATFORM}_${DISTRO}_openhd_builder)")]] ; then
+        $DOCKER rm -v $DOCKER_USERNAME/${PLATFORM}_${DISTRO}_openhd_builder > /dev/null 2>&1 || true
+        create_docker_container
+
+    elif [[ -n "$($DOCKER images -q ${IMAGE})" ]] ; then
+        return 0
+
+    else
+        if ! $DOCKER create -it -v $PWD:/src --name ${CONTAINER} --env CLOUDSMITH_API_KEY=$CLOUDSMITH_API_KEY ${IMAGE} ; then
+            build_docker_image
+            $DOCKER create -it -v $PWD:/src --name ${CONTAINER} --env CLOUDSMITH_API_KEY=$CLOUDSMITH_API_KEY ${IMAGE}
+        fi
+    fi
+}
+
 
 function run_build() {
     $DOCKER start -i ${CONTAINER}
 }
+
 
 build_docker_image
 run_build
