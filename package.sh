4311598169018874edad42beb5ce63a2cec194f6
Stephen Oliver <steve@infincia.com>
2020-09-01 11:10:11 -0400

    Replace omxh264enc with v4l2h264 during packaging
    This ensures that buster users don't have to change it themselves,
    it is correct by default.

--- package.sh
+++ package.sh
@@ -217,6 +217,13 @@ EOF
     cp -a config/openhd-settings-1.txt ${TMPDIR}/boot/openhd-settings-3.txt || exit 1
     cp -a config/openhd-settings-1.txt ${TMPDIR}/boot/openhd-settings-4.txt || exit 1
 
+    if [[ "${OS}" == "raspbian" && "${DISTRO}" == "buster" ]]; then
+        sed -i s/omxh264enc control-rate=1 target-bitrate=600000 interval-intraframes=1 periodicty-idr=1/videoconvert ! v4l2h264enc /g ${TMPDIR}/boot/openhd-settings-1.txt
+        sed -i s/omxh264enc control-rate=1 target-bitrate=600000 interval-intraframes=1 periodicty-idr=1/videoconvert ! v4l2h264enc /g ${TMPDIR}/boot/openhd-settings-2.txt
+        sed -i s/omxh264enc control-rate=1 target-bitrate=600000 interval-intraframes=1 periodicty-idr=1/videoconvert ! v4l2h264enc /g ${TMPDIR}/boot/openhd-settings-3.txt
+        sed -i s/omxh264enc control-rate=1 target-bitrate=600000 interval-intraframes=1 periodicty-idr=1/videoconvert ! v4l2h264enc /g ${TMPDIR}/boot/openhd-settings-4.txt
+    fi
+
     cp -a driver-helpers/* ${TMPDIR}/usr/local/bin/ || exit 1
 }
 
