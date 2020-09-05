088f1eea8058d6a5f24fa39f46ce1f4d0ef23972
Stephen Oliver <steve@infincia.com>
2020-09-05 16:45:16 -0400

    Store video/screenshots separate from telemetry
    When the video recording inevitably fills the /wbc_tmp area it leaves
    no more room to store telemetry or gnuplot charts.
    
    Video is now always stored separately, preventing it from affecting
    anything else.

--- wifibroadcast-scripts/tx_rx_functions.sh
+++ wifibroadcast-scripts/tx_rx_functions.sh
@@ -3,24 +3,12 @@ source tx_functions.sh
 
 function MAIN_TX_RX_FUNCTION {
     ###############################################################################
-    # Create /wbc_tmp dynamically based on available ram
+    # Create /wbc_tmp for telemetry storage
     ###############################################################################
-
-    detect_memory
-
     mkdir -p /wbc_tmp
     
-    # use 1/3 of available ram by default for /wbc_tmp, which is used for recording telemetry and video
-    # when VIDEO_TMP=memory. We need to do this to avoid crashes or safety issues caused by running out of
-    # memory, which is easy to do when the ground station has just 512MB ram to start with and has 128MB set
-    # aside for the GPU, like the Pi3a+
-    available_for_wbc_tmp=$((${TOTAL_MEMORY} / 3))
 
-    # add a little extra margin 
-    available_for_wbc_tmp_final=$((${available_for_wbc_tmp} + 30000))
-
-    mount -t tmpfs -o size=${available_for_wbc_tmp_final}K tmpfs /wbc_tmp
-
+    mount -t tmpfs -o size=15024K tmpfs /wbc_tmp
     mkdir -p /wbc_tmp/rssi/
 
     if [ "$CAM" == "0" ]; then
