fae10e8dc52787fd081d10bed027c553044cc9fb
Stephen Oliver <steve@infincia.com>
2020-10-05 21:24:29 -0400

    Add to_uppercase() helper function

--- openhd-common/openhd-util.hpp
+++ openhd-common/openhd-util.hpp
@@ -7,6 +7,15 @@
 #include "openhd-structs.h"
 
 
+
+inline std::string to_uppercase(std::string input) {
+    for (std::string::iterator it = input.begin(); it != input.end(); ++ it) {
+        *it = toupper((unsigned char)*it);
+    }
+
+    return input;
+}
+
 inline std::string camera_type_to_string(CameraType camera_type) {
     switch (camera_type) {
         case CameraTypeRaspberryPiCSI: {
