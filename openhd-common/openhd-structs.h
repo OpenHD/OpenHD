33bc044f1de01fed2c5a5e2b76d590007e13fa98
Stephen Oliver <steve@infincia.com>
2020-10-05 21:25:57 -0400

    Add ethernet card type

--- openhd-common/openhd-structs.h
+++ openhd-common/openhd-structs.h
@@ -37,4 +37,12 @@ struct WiFiCard {
     bool supports_rts;
 };
 
+struct EthernetCard {
+    std::string name = "unknown";
+    std::string vendor = "unknown";
+    std::string vid;
+    std::string pid;
+    std::string usb_bus;
+};
+
 #endif
