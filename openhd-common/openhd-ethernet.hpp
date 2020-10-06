ba06d6910eba9d98e88f092a9d05c9e3d11b7931
Stephen Oliver <steve@infincia.com>
2020-10-06 01:05:12 -0400

    Add ethernet and tethering types
    In most cases we don't care what kind of ethernet interface it is,
    but iPhone and android devices must be detected and marked
    accordingly.
    
    If another ethernet device is found, by default it should be used for
    ethernet hotspot purposes (the interface service handles this),
    unless the system is running as an air unit.

--- openhd-common/openhd-ethernet.hpp
+++ openhd-common/openhd-ethernet.hpp
@@ -6,13 +6,76 @@
 
 #include "openhd-util.hpp"
 
+
+typedef enum EthernetCardType {
+    EthernetCardTypeiPhoneTether,
+    EthernetCardTypeAndroidTether,
+    EthernetCardTypeUnknown
+} EthernetCardType;
+
+
+typedef enum EthernetHotspotType {
+    EthernetHotspotTypeInternal,
+    EthernetHotspotTypeNone
+} EthernetHotspotType;
+
+
+
 struct EthernetCard {
+    EthernetCardType type;
     std::string name = "unknown";
     std::string vendor = "unknown";
     std::string vid;
     std::string pid;
     std::string usb_bus;
 };
+
+
+inline std::string ethernet_card_type_to_string(EthernetCardType card_type) {
+    switch (card_type) {
+        case EthernetCardTypeiPhoneTether: {
+            return "ipheth";
+        }
+        case EthernetCardTypeAndroidTether: {
+            return "rndis_host";
+        }
+        default: {
+            return "unknown";
+        }
+    }
+}
+
+
+inline EthernetCardType string_to_ethernet_card_type(std::string driver_name) {
+    if (to_uppercase(driver_name).find(to_uppercase("ipheth")) != std::string::npos) {
+        return EthernetCardTypeiPhoneTether;
+    } else if (to_uppercase(driver_name).find(to_uppercase("rndis_host")) != std::string::npos) {
+        return EthernetCardTypeAndroidTether;
+    }
+
+    return EthernetCardTypeUnknown;
+}
+
+
+inline std::string ethernet_hotspot_type_to_string(EthernetHotspotType ethernet_hotspot_type) {
+    switch (ethernet_hotspot_type) {
+        case EthernetHotspotTypeInternal: {
+            return "internal";
+        }
+        default: {
+            return "none";
+        }
+    }
+}
+
+
+inline EthernetHotspotType string_to_ethernet_hotspot_type(std::string hotspot_type) {
+    if (to_uppercase(hotspot_type).find(to_uppercase("internal")) != std::string::npos) {
+        return EthernetHotspotTypeInternal;
+    }
+
+    return EthernetHotspotTypeNone;
+}
 
 
 #endif
