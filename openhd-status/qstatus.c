2a4d412458df5f22cfa229344f73f270825e55be
Stephen Oliver <steve@infincia.com>
2020-10-10 10:58:15 -0400

    Allow code to submit status service messages
    This will allow anything that can call a c++ function to send messages
    with severity levels to the status service.

--- openhd-status/qstatus.c
+++ openhd-status/qstatus.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
 
 
-typedef struct {
-    uint8_t level;
-    uint8_t message[50];
-} __attribute__((packed)) localmessage_t;
-
-
+#include "openhd-status.hpp"
 
 int main(int argc, char **argv) {
     if (argc < 3) {
@@ -25,33 +14,9 @@ int main(int argc, char **argv) {
 
     int level = atoi(argv[2]);
 
-    localmessage_t message;
-    message.level = level;
-    strncpy(message.message, argv[1], 50);
-    if (message.message[49] != '\0') {
-        message.message[49] = '\0';
-    }
+    const char* message = argv[1];
 
-
-    int sockfd; 
-
-    struct sockaddr_in servaddr; 
-  
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("Socket create failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-  
-    memset(&servaddr, 0, sizeof(servaddr)); 
-      
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(50000); 
-    inet_aton("127.0.0.1", &servaddr.sin_addr.s_addr);
-
-    int n, len; 
-      
-    sendto(sockfd, &message, sizeof(message), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-
+    status_message((STATUS_LEVEL)level, message);
     
     return 0;
 }
