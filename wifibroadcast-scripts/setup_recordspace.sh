448cd88ef866c0b61ae626deaa3776c555ecd356
Stephen Oliver <steve@infincia.com>
2020-10-23 21:06:40 -0400

    Add recording space setup script
    Easier to do in bash than code. This should be called early at boot,
    probably in early_init.sh or just from a systemd unit since it doesn't
    depend on anything else.

---
+++ wifibroadcast-scripts/setup_recordspace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /usr/local/bin/wifibroadcast-scripts/global_functions.sh
+
+detect_memory
+
+mkdir -p /video_tmp
+
+# use 1/3 of available ram by default for /video_tmp, which is used for recording telemetry and video
+# when VIDEO_TMP=memory. We need to do this to avoid crashes or safety issues caused by running out of
+# memory, which is easy to do when the ground station has just 512MB ram to start with and has 128MB set
+# aside for the GPU, like the Pi3a+
+available_for_video_tmp=$((${TOTAL_MEMORY} / 3))
+
+# add a little extra margin 
+available_for_video_tmp_final=$((${available_for_video_tmp} + 30000))
+
+mount -t tmpfs -o size=${available_for_video_tmp_final}K tmpfs /video_tmp
+
+VIDEOFILE=/video_tmp/videotmp.raw
+
+echo "VIDEOFILE=${VIDEOFILE}" > /tmp/videofile
+
+qstatus "Saving video to memory" 5
