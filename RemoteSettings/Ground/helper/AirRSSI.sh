4386525ed2cfc3b5a97cdb7939350b98fa15a29d
Stephen Oliver <steve@infincia.com>
2020-10-23 20:33:49 -0400

    Remove helper scripts
    These were intended to send specific strings to various other
    components, like the old settings app, so that they could do things
    like figure out what the ground IP was automatically.

--- RemoteSettings/Ground/helper/AirRSSI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while [ ! -f /tmp/ReadyToGo ]
-do
-	echo "test" > /dev/udp/127.0.0.1/1376
-        sleep 1
-done
-exit
--- RemoteSettings/ipchecker/iphelper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while :
-do
-	#echo "Send ACK packet to phone (IP: $PHONEIP)"
-	echo "GroundIP" > /dev/udp/127.0.0.1/5116
-	
-	sleep 1
-done
