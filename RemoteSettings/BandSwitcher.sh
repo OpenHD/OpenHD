3ba399b02086a62946358dc15488a797d56af7f7
Stephen Oliver <steve@infincia.com>
2020-10-23 20:29:36 -0400

    Remove old camera code, replaced by camera service
    The camera handling code here is entirely replaced by the camera
    microservice, which handles multiple cameras by default and just
    connects to the telemetry router(s) to see RC channel values and
    other messages from GCS.
    
    The bandwidth switching system in this code is not yet added to
    openhd-interface, but it would not be difficult and would be
    substantially cleaner and easier to deal with.

--- RemoteSettings/BandSwitcher.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-/usr/local/share/RemoteSettings/Ground/TxForwardBandSwitcher.sh &
-sleep 0.5
-/usr/local/share/RemoteSettings/Ground/RxForwardToBandSwitcher.sh &
-sleep 0.5
-/usr/local/share/RemoteSettings/Ground/BandSwitcher.sh &
