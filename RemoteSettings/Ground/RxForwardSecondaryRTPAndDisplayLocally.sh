0edaf119e4adfd835b1f20c51d12ae7e45b7ea86
Stephen Oliver <steve@infincia.com>
2020-11-13 15:51:51 -0500

    Allow USB/IP cameras to be main video in QOpenHD
    Temporary setting. When set to N, this allows for 2 scenarios:
    
    1) You want a USB/IP camera to be the main video view in QOpenHD
    
    2) You want to switch the main video between pi and USB/IP/thermal

--- RemoteSettings/Ground/RxForwardSecondaryRTPAndDisplayLocally.sh
+++ RemoteSettings/Ground/RxForwardSecondaryRTPAndDisplayLocally.sh
@@ -26,7 +26,7 @@ NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep 
 
 /usr/local/share/cameracontrol/IPCamera/svpcom_wifibroadcast/wfb_rx -u 5612 -p 23 -c 127.0.0.1 -n $VIDEO_BLOCKS_SECONDARY -k $VIDEO_FECS_SECONDARY -K /tmp/rx.key $NICS_LIST >/dev/null 2>/dev/null &
 
-if [ "$ENABLE_QOPENHD" == "Y" ]; then
+if [ "${ENABLE_QOPENHD}" == "Y" ] && [ "${ENABLE_QOPENHD_PIP}" == "Y" ]; then
     gst-launch-1.0 udpsrc port=5612 !  tee name=t !  queue ! udpsink host=127.0.0.1 port=5621 t. ! udpsink host=127.0.0.1 port=${VIDEO_UDP_PORT2}
 else
     gst-launch-1.0 udpsrc port=5612 !  tee name=t !  queue ! udpsink host=127.0.0.1 port=5621 t. ! "application/x-rtp,media=video" ! rtph264depay  ! video/x-h264, stream-format="byte-stream" ! filesink location=/dev/stdout | /usr/local/bin/hello_video.bin.240-befi ${HELLO_VIDEO_ARGS}
