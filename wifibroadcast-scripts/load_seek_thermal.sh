c3f46c625feabd0e9ac4c2c028b33fccb96e5901
Stephen Oliver <steve@infincia.com>
2020-11-27 23:43:12 -0500

    Auto calibrate Seek Compact at power on
    When the drone is powering up, place something flat like a piece of
    paper (not reflective) in front of the Seek camera until the thermal
    video appears on the OSD.
    
    This will calibrate the sensor and eliminate a lot of noise from the
    image.
    
    This is not a permanent calibration, it needs to be done each time
    the camera is turned on.

--- wifibroadcast-scripts/load_seek_thermal.sh
+++ wifibroadcast-scripts/load_seek_thermal.sh
@@ -9,9 +9,13 @@ fi
 
 modprobe v4l2loopback devices=5
 
+FlatFieldFile=/var/run/openhd/flat_field.png
+
+/usr/local/bin/seek_create_flat_field -o ${FlatFieldFile} -t ${SeekModel}
+
 while true
 do
-    /usr/local/bin/seek_viewer -m v4l2 -o /dev/video4 -f ${SeekFramerate} -c ${SeekColormap} -r ${SeekRotate} -t ${SeekModel}
+    /usr/local/bin/seek_viewer -m v4l2 -o /dev/video4 -f ${SeekFramerate} -c ${SeekColormap} -r ${SeekRotate} -t ${SeekModel} -F ${FlatFieldFile}
     echo "Restarting Seek Thermal"
     sleep 2
 done
