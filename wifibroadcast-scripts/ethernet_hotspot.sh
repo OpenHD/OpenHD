cb1e19bb701e0c8348e99a38bbb0b38aca5d8167
Stephen Oliver <steve@infincia.com>
2020-12-06 23:55:30 -0500

    Remove nice/sudo from eth hotspot setup script

--- wifibroadcast-scripts/ethernet_hotspot.sh
+++ wifibroadcast-scripts/ethernet_hotspot.sh
@@ -3,9 +3,10 @@
 HOTSPOT_INTERFACE=$1
 
 # todo: pass in the address and dhcp range so it can be set or changed automatically
-sudo sed -i -e "s/interface=eth0/interface=${HOTSPOT_INTERFACE}/g" /etc/dnsmasqEth0.conf
-sudo sed -i -e "s/dhcp-range=eth0/dhcp-range==${HOTSPOT_INTERFACE}/g" /etc/dnsmasqEth0.conf
+sed -i -e "s/interface=eth0/interface=${HOTSPOT_INTERFACE}/g" /etc/dnsmasqEth0.conf
+sed -i -e "s/dhcp-range=eth0/dhcp-range=${HOTSPOT_INTERFACE}/g" /etc/dnsmasqEth0.conf
 
 
-nice ifconfig ${HOTSPOT_INTERFACE} 192.168.1.1 up
-nice /usr/sbin/dnsmasq --conf-file=/etc/dnsmasqEth0.conf
+ifconfig ${HOTSPOT_INTERFACE} 192.168.1.1 up
+/usr/sbin/dnsmasq --conf-file=/etc/dnsmasqEth0.conf
+
