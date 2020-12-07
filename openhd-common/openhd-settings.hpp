03c7317b08a1e1ad1f9f0a3e2e963ec7d79c0b3e
Stephen Oliver <steve@infincia.com>
2020-12-07 01:47:41 -0500

    Add support for item types in settings parser
    They don't need to be tracked by name because the context the file
    is used in tells the system which item type will be found, but they
    still need to be separated cleanly so users know what they are.
    
    For example the camera.conf file can have several [Camera] items,
    one for each camera that is currently or has been connected to a
    vehicle at some point. On first boot if a camera is found that does
    not have an item in the settings file, it will be added automatically
    with the default settings for that camera.
    
    Users can also choose to add an item manually even if the hardware
    it describes has never been connected before. This means settings for
    different wifi cards or cameras can be preserved without duplicating
    entire profiles or making people manually edit things to change the
    system behavior.

--- openhd-common/openhd-settings.hpp
+++ openhd-common/openhd-settings.hpp
@@ -4,10 +4,26 @@
 
 #include <exception> // exception
 #include <stdexcept> // runtime_error
+#include <optional>
 #include <string>
 #include <utility> // make_pair
 
 #include <boost/regex.hpp>
+
+std::optional<std::string> parse_section(std::string line) {
+    boost::smatch result;
+
+    boost::regex r{ "^\\[([\\w\\s]+)\\]"};
+    if (!boost::regex_match(line, result, r)) {
+        return {};
+    }
+
+    if (result.size() != 2) {
+        return {};
+    }
+
+    return result[1];
+}
 
 std::pair<std::string, std::string> parse_kv(std::string kv) {
     boost::smatch result;
@@ -25,18 +41,37 @@ std::pair<std::string, std::string> parse_kv(std::string kv) {
 }
 
 
-std::map<std::string, std::string> read_config(std::string path) {
+std::vector<std::map<std::string, std::string> > read_config(std::string path) {
     std::ifstream in(path);
 
-    std::map<std::string, std::string> settings;
+    std::vector<std::map<std::string, std::string> > settings;
 
     std::string str;
+
+    std::map<std::string, std::string> section;
 
     while (std::getline(in, str)) {
         if (str.size() > 0) {
             try {
+                auto section_name = parse_section(str);
+
+                if (section_name) {
+                    if (section.size() > 0) {
+                        settings.push_back(section);
+                        section.clear();
+                    }
+
+                    continue;
+                }
+            } catch (std::exception &ex) {
+                /* 
+                 * Ignore, likely a comment or user error in which case we will use a default.
+                 */
+            }
+
+            try {
                 auto pair = parse_kv(str);
-                settings.insert(pair);
+                section.insert(pair);
             } catch (std::exception &ex) {
                 /* 
                  * Ignore, likely a comment or user error in which case we will use a default.
