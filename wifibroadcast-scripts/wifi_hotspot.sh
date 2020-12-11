6989853f07dbf7f748e8758826912297849d8700
Stephen Oliver <steve@infincia.com>
2020-12-11 01:42:02 -0500

    Add wifi hotspot password support in setup script

--- wifibroadcast-scripts/wifi_hotspot.sh
+++ wifibroadcast-scripts/wifi_hotspot.sh
@@ -4,6 +4,7 @@ HOTSPOT_BAND=$1
 HOTSPOT_CHANNEL=$2
 HOTSPOT_INTERFACE=$3
 HOTSPOT_TXPOWER=$4
+HOTSPOT_PASSWORD=$5
 
 #
 # Convert hostap config from DOS format to UNIX format
@@ -18,6 +19,7 @@ source /tmp/apconfig.txt
 sed -i -e "s/hw_mode=$hw_mode/hw_mode=$HOTSPOT_BAND/g" /tmp/apconfig.txt
 sed -i -e "s/channel=$channel/channel=$HOTSPOT_CHANNEL/g" /tmp/apconfig.txt
 sed -i -e "s/interface=wifihotspot0/interface=${HOTSPOT_INTERFACE}/g" /tmp/apconfig.txt
+sed -i -e "s/wpa_passphrase=wifiopenhd/wpa_passphrase=${HOTSPOT_PASSWORD}/g" /tmp/apconfig.txt
 
 
 sed -i -e "s/interface=wifihotspot0/interface=${HOTSPOT_INTERFACE}/g" /etc/dnsmasqWifi.conf
