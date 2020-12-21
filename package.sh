dc0e57fddf397577b108efc017d5a2e230259bc1
Stephen Oliver <steve@infincia.com>
2020-12-21 15:22:10 -0500

    Remove dependency on openhd-router
    The router code is being moved into this repo and would conflict with
    the router package

--- package.sh
+++ package.sh
@@ -199,7 +199,6 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TM
   -d "libasio-dev >= 1.10" \
   -d "libboost-system-dev >= 1.62.0" \
   -d "libboost-program-options-dev >= 1.62.0" \
-  -d "openhd-router >= 0.1.8" \
   -d "openhd-microservice >= 0.1.18" \
   -d "libseek-thermal >= 20200801.1" \
   -d "flirone-driver >= 20200704.3" \
