de58388fbe457ed890660f3151527c1f3b2e997b
Stephen Oliver <steve@infincia.com>
2020-12-23 03:10:56 -0500

    Install cmake before building package

--- install_dep.sh
+++ install_dep.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt -y install build-essential autotools-dev automake libtool autoconf \
+apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libpng-dev libsdl2-dev libsdl1.2-dev libconfig++-dev \
             libreadline-dev libjpeg-dev libusb-1.0-0-dev libsodium-dev \
             libfontconfig1-dev libfreetype6-dev ttf-dejavu-core \
