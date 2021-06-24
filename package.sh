3a70456b471577d8687d1adb4013d4c64d113338
Luke <pilotnbr1@gmail.com>
2021-06-24 11:32:26 -0400

    Undo hacky temp fix to build package

--- package.sh
+++ package.sh
@@ -306,11 +306,11 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TM
 # don't create a tag. You can build packages and test them locally without tagging.
 #
 git describe --exact-match HEAD > /dev/null 2>&1
-#if [[ $? -eq 0 ]]; then
+if [[ $? -eq 0 ]]; then
     echo "Pushing package to OpenHD repository"
     cloudsmith push deb openhd/openhd-2-0/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
-#else
-#    echo "Pushing package to OpenHD testing repository"
-#    cloudsmith push deb openhd/openhd-2-0-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
-#fi
+else
+    echo "Pushing package to OpenHD testing repository"
+    cloudsmith push deb openhd/openhd-2-0-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+fi
 
