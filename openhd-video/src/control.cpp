31ce31e3e1d88254171298b67ad3125399011966
pilotnbr1 <pilotnbr1@gmail.com>
2021-09-25 00:09:34 -0400

    Migrate to boost signal2 continued

--- openhd-video/src/control.cpp
+++ openhd-video/src/control.cpp
@@ -2,7 +2,7 @@
 
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
-#include <boost/signal.hpp>
+#include <boost/signals2.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
