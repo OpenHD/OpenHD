759b8679f9ee1332b6ff096e3390059ea731e7e5
MacDaddyFPV <63026018+MacDaddyFPV@users.noreply.github.com>
2021-12-02 22:20:19 +1100

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -3,7 +3,7 @@
 apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libpng-dev libsdl2-dev libsdl1.2-dev libconfig++-dev \
             libreadline-dev libjpeg-dev libusb-1.0-0-dev libsodium-dev \
-            libfontconfig1-dev libfreetype6-dev ttf-dejavu-core \
+            libfontconfig1-dev libfreetype6-dev \
             libgstreamer-plugins-base1.0-dev \
-            libboost-dev libboost-program-options-dev libboost-system-dev libboost-signals-dev libasio-dev libboost-chrono-dev libsystemd-dev \
+            libboost-dev libboost-program-options-dev libboost-system-dev libasio-dev libboost-chrono-dev libsystemd-dev \
             libboost-regex-dev libboost-filesystem-dev libboost-thread-dev indent libv4l-dev libnl-3-dev libnl-genl-3-dev || exit 1
