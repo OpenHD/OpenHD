c311593ff4b6e712105ba539ca6618aaa0e481d9
MacDaddyFPV <63026018+MacDaddyFPV@users.noreply.github.com>
2021-12-10 20:15:02 +1100

    Update global_functions.sh
    Add bullseye and remove stretch

--- wifibroadcast-scripts/global_functions.sh
+++ wifibroadcast-scripts/global_functions.sh
@@ -1,19 +1,17 @@
 function detect_os {
     source /etc/os-release
 
-    if [[ "${VERSION_ID}" == "9" ]]; then
-        export OPENHD_VERSION="stretch"
-    elif [[ "${VERSION_ID}" == "10" ]]; then
+    if [[ "${VERSION_ID}" == "10" ]]; then
         export OPENHD_VERSION="buster"
+    elif [[ "${VERSION_ID}" == "11" ]]; then
+        export OPENHD_VERSION="bullseye"
     else
         export OPENHD_VERSION="unknown"
     fi
 }
 
 function configure_hello_video_args {
-    if [[ "$OPENHD_VERSION" == "stretch" ]]; then
-        export HELLO_VIDEO_ARGS="0"
-    elif [[ "$OPENHD_VERSION" == "buster" ]]; then
+    if [[ "$OPENHD_VERSION" == "buster" ]]; then
         export HELLO_VIDEO_ARGS="1"
     else
         export HELLO_VIDEO_ARGS="0"
