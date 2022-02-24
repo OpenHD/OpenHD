2fd508af20d6db0709ebfd6937c216b9beefea42
cq112358 <98128829+cq112358@users.noreply.github.com>
2022-02-24 15:57:36 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -15,7 +15,7 @@ if [[ "${DISTRO}" == "buster" ]]; then
 fi
 
 if [[ "${DISTRO}" == "bullseye" ]]; then
-    PLATFORM_PACKAGES="-d veye-raspberrypi -d lifepoweredpi -d raspi2png -d gst-rpicamsrc"
+    PLATFORM_PACKAGES="-d wiringpi -d veye-raspberrypi -d lifepoweredpi -d raspi2png -d gstreamer1.0-omx-rpi-config -d gst-rpicamsrc -d qopenhd"
     PLATFORM_CONFIGS="--config-files /boot/cmdline.txt --config-files /boot/config.txt --config-files /usr/local/share/openhd/joyconfig.txt"
 fi
 
@@ -221,39 +221,46 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${PK
   --after-install after-install.sh \
   --before-install before-install.sh \
   $PLATFORM_PACKAGES \
+  -d "wiringpi" \
+  -d "trackermavfilter" \
   -d "libasio-dev >= 1.10" \
   -d "libboost-system-dev >= 1.62.0" \
   -d "libboost-program-options-dev >= 1.62.0" \
-  -d "libseek-thermal >= 20200801.1" \
+  -d "openhd-router >= 0.1.8" \
+  -d "openhd-microservice >= 0.1.18" \
+  -d "qopenhd" \
+  -d "openhd-linux-pi >= 20201122.2" \
+  -d "libseek-thermal >= 20201118.1" \
   -d "flirone-driver >= 20200704.3" \
-  -d "wifibroadcast >= 20200930.1" \
+  -d "veye-raspberrypi >= 20201122.1" \
+  -d "lifepoweredpi >= 20200704.2" \
+  -d "mavlink-router >= 20200704.3" \
+  -d "raspi2png >= 20200704.2" \
   -d "openhd-dump1090-mutability >= 20201122.2" \
   -d "gnuplot-nox" \
   -d "hostapd" \
   -d "iw" \
-  -d "isc-dhcp-common" \
   -d "pump" \
   -d "dnsmasq" \
   -d "aircrack-ng" \
+  -d "ser2net" \
   -d "i2c-tools" \
   -d "dos2unix" \
   -d "fuse" \
+  -d "socat" \
   -d "ffmpeg" \
   -d "indent" \
-  -d "libv4l-dev" \
-  -d "libusb-1.0-0" \
   -d "libpcap-dev" \
   -d "libpng-dev" \
-  -d "libnl-3-dev" \
-  -d "libnl-genl-3-dev" \
   -d "libsdl2-2.0-0" \
   -d "libsdl1.2debian" \
   -d "libconfig++9v5" \
   -d "libreadline-dev" \
-  -d "libjpeg-dev" \
+  -d "libjpeg62-turbo" \
   -d "libsodium-dev" \
   -d "libfontconfig1" \
   -d "libfreetype6" \
+  -d "ttf-dejavu-core" \
   -d "libgles2-mesa-dev" \
   -d "libboost-chrono-dev" \
   -d "libboost-regex-dev" \
@@ -266,7 +273,8 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${PK
   -d "gstreamer1.0-libav" \
   -d "gstreamer1.0-tools" \
   -d "gstreamer1.0-alsa" \
-  -d "gstreamer1.0-pulseaudio" || exit 1
+  -d "gstreamer1.0-pulseaudio" \
+  -d "gstreamer1.0-omx-rpi-config" || exit 1
 
 #
 # Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
