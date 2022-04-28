64532295bc2af39f12b7730cc03debd3dd3784ab
consti10 <geierconstantinabc@gmail.com>
2022-04-28 18:00:16 +0200

    documentation

--- openhd-video/inc/camerastream.h
+++ openhd-video/inc/camerastream.h
@@ -9,6 +9,17 @@
 #include "openhd-camera.hpp"
 #include "openhd-platform.hpp"
 
+/**
+ * Every camera stream should inherit from this class.
+ * This hides away the underlying implementation (for example gstreamer,...) for different platform(s).
+ * The paradigms developers should aim for with each camera stream are:
+ * 1) Once an instance is created, it will start generating video data, already encoded if possible.
+ * 2) If the camera disconnects or the underlying  process crashes (for whatever reason) the underlying implementation
+ *    should re-start the camera and encoding process
+ * 3) If the user changes camera parameters, it should store these changes locally (such that they are also set after the next
+ * re-start) and apply the changes. It is no problem to just restart the underlying camera/encoding process with the new parameters.
+ * 4) The implementation(s) should report if changing the various parameters is possible.
+ */
 class CameraStream {
 public:
     CameraStream(boost::asio::io_service &io_service, PlatformType platform, Camera &camera, uint16_t port);
