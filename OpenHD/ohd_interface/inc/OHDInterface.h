b8d33246ef6ee84838bba121e8994aa07c8d9180
consti10 <geierconstantinabc@gmail.com>
2022-05-03 01:17:24 +0200

    document

--- OpenHD/ohd_interface/inc/OHDInterface.h
+++ OpenHD/ohd_interface/inc/OHDInterface.h
@@ -15,8 +15,13 @@ class OHDInterface {
 public:
     /**
      * Note: due to json, needs to be crated after OHDSystem has been run once.
-     * @param is_air
-     * @param unit_id
+     * The constructor reads the generated json by OHDSystem and tries to initialize all its members.
+     * NOTE: We need to wire link - related settings into here.
+     * For example, changing the wifi frequency (needs to be synced between air and ground) but also
+     * settings that can be unsinced (like enable / disable wifi hotspot, which - as an example - is only
+     * a setting that affects the ground pi.
+     * @param is_air true if we run on the air pi, ground pi otherwise
+     * @param unit_id ?? Stephen no idea ?
      */
     OHDInterface(boost::asio::io_service& io_service,bool is_air,std::string unit_id);
     std::unique_ptr<WiFi> wifi;
