b7f6174d8adcac096e92c5cd3e16e03fea24d657
consti10 <geierconstantinabc@gmail.com>
2022-05-03 11:36:38 +0200

    update dependencies

--- install_dep.sh
+++ install_dep.sh
@@ -6,4 +6,5 @@ apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libfontconfig1-dev libfreetype6-dev \
             libgstreamer-plugins-base1.0-dev \
             libboost-dev libboost-program-options-dev libboost-system-dev libasio-dev libboost-chrono-dev libsystemd-dev \
-            libboost-regex-dev libboost-filesystem-dev libboost-thread-dev indent libv4l-dev libnl-3-dev libnl-genl-3-dev libfmt-dev || exit 1
+            libboost-regex-dev libboost-filesystem-dev libboost-thread-dev indent libv4l-dev libnl-3-dev libnl-genl-3-dev libfmt-dev \
+            libnl-3-dev libnl-nf-3-dev libnl-route-3-dev || exit 1
