56fe2a8990d0cce10d02486f9b282af5658d2adb
consti10 <geierconstantinabc@gmail.com>
2022-05-07 00:47:19 +0200

    compilation

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -7,7 +7,9 @@
 #include "openhd-read-util.hpp"
 #include "ohd_system/inc/OHDSystem.h"
 #include "ohd_interface/inc/OHDInterface.h"
-//#include "ohd_video/inc/OHDVideo.h"
+#include "ohd_video/inc/OHDVideo.h"
+#include "ohd_telemetry/src/GroundTelemetry.h"
+#include "ohd_telemetry/src/AirTelemetry.h"
 
 #include <memory>
 #include <boost/asio/io_service.hpp>
@@ -18,15 +20,22 @@ int main(int argc, char *argv[]) {
     std::cout<<"OpenHD START\n";
 
     // Always needs to run first.
-    OHDSystem::runOnceOnStartup();
+    /*OHDSystem::runOnceOnStartup();
 
+    // Now this is kinda stupid - we write json's during the discovery, then we read them back in
+    // merged stupidly with some fragments that resemble settings.
     const std::string unit_id=OHDReadUtil::get_unit_id();
     const bool is_air = OHDReadUtil::runs_on_air();
-    const auto platform_type=OHDReadUtil::get_platform_type();
+    const auto platform_type=
 
+    // These 2 get all the hardware info from json
+    auto ohdInterface=std::make_unique<OHDInterface>(is_air,unit_id);
     boost::asio::io_service io_service;
-    auto ohdInterface=std::make_unique<OHDInterface>(io_service,is_air,unit_id);
 
-    //auto ohdVideo=std::make_unique<OHDVideo>(io_service,is_air,unit_id,platform_type);
+    auto ohdVideo=std::make_unique<OHDVideo>(io_service,is_air,unit_id,platform_type);
+
+    // fake it for the moment so the service doesn't exit, won't be needed once the microservice channel is wired in
+    boost::asio::io_service::work work(io_service);
+    io_service.run();*/
 
 }
