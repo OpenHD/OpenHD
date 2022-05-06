7a121236ad56e36541857da6271a83d9324b5dba
consti10 <geierconstantinabc@gmail.com>
2022-05-06 19:58:19 +0200

    add a test for discovery module

--- OpenHD/ohd_system/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "OHDSystem.h"
-
-int main(int argc, char *argv[]) {
-
-    OHDSystem::runOnceOnStartup();
-
-    return 0;
-}
--- OpenHD/ohd_system/test/test_discovery.cpp
+++ OpenHD/ohd_system/test/test_discovery.cpp
@@ -1,5 +1,8 @@
 #include "OHDSystem.h"
 
+/**
+ * Test the discover process of OpenHD. After running this executable, look at the json's generated under /tmp
+ */
 int main(int argc, char *argv[]) {
 
     OHDSystem::runOnceOnStartup();
