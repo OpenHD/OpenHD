a9a6de124337525fd3692231842c69b0f66c4378
consti10 <geierconstantinabc@gmail.com>
2022-05-09 13:07:41 +0200

    document and refactor

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -25,13 +25,13 @@ int main(int argc, char *argv[]) {
 
     // Now this is kinda stupid - we write json's during the discovery, then we read them back in
     // merged stupidly with some fragments that resemble settings.
+    // Note that interface, telemetry and video might also read the or update the jsons
     const auto platform=platform_from_manifest();
     const auto profile=profile_from_manifest();
 
 
     // First start ohdInterface, which does wifibroadcast and more
     auto ohdInterface=std::make_unique<OHDInterface>(profile.is_air,profile.unit_id);
-    boost::asio::io_service io_service;
 
     // then we can start telemetry, which uses OHDInterface for wfb tx/rx (udp)
     if(profile.is_air){
@@ -42,11 +42,13 @@ int main(int argc, char *argv[]) {
 
     // and start ohdVideo if we are on the air pi
     if(profile.is_air){
-        auto ohdVideo=std::make_unique<OHDVideo>(io_service,profile.is_air,profile.unit_id,platform.platform_type);
+        auto ohdVideo=std::make_unique<OHDVideo>(profile.is_air,profile.unit_id,platform.platform_type);
     }
 
-    // fake it for the moment so the service doesn't exit, won't be needed once the microservice channel is wired in
-    boost::asio::io_service::work work(io_service);
-    io_service.run();
+    // run forever, everything has its own threads
+    while(true){
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout<<"OpenHD\n";
+    }
 
 }
