e6be543cf448b0815df1bd5e69e2e3d58113a600
consti10 <geierconstantinabc@gmail.com>
2022-05-11 11:44:41 +0200

    add sme options for development to main method (TODO extend)

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -16,13 +16,49 @@
 #include <GroundTelemetry.h>
 
 //TODO fix the cmake crap and then we can build a single executable.
+static const char optstr[] = "?:da";
+static const struct option long_options[] = {
+        {"skip_detection", no_argument, NULL, 'd'},
+        {"force_air", no_argument, NULL, 'a'},
+        {NULL, 0, NULL, 0},
+};
+
+struct OHDRunOptions{
+    bool skip_detection=false;
+    bool force_air=false;
+};
 
 int main(int argc, char *argv[]) {
-    std::cout<<"OpenHD START\n";
+    OHDRunOptions options;
+    // parse some arguments usefully for debugging
+    int c;
+    while ((c = getopt_long(argc, argv, optstr, long_options, NULL)) != -1) {
+        const char *tmp_optarg = optarg;
+        switch (c) {
+            case 'd':
+                options.skip_detection=true;
+                break;
+            case 'a':
+                options.force_air=true;
+                break;
+            case '?':
+            default:
+                std::cout<<"Usage: --skip_detection [Skip detection step, usefully for changing things in json manually] "<<
+                "force_air [Force to boot as air pi, even when no camera is detected] "<<
+                "\n";
+                return 0;
+        }
+    }
+    std::cout<<"OpenHD START with "<<
+             "skip_detection:"<<(options.skip_detection ? "Y": "N")<<
+             "force_air:"<<(options.force_air ? "Y": "N")<<
+             "\n";
 
     try{
-        // Always needs to run first.
-        OHDSystem::runOnceOnStartup();
+        if(!options.skip_detection){
+            // Always needs to run first.
+            OHDSystem::runOnceOnStartup();
+        }
 
         // Now this is kinda stupid - we write json's during the discovery, then we read them back in
         // merged stupidly with some fragments that resemble settings.
