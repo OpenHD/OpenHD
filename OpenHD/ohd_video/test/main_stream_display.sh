510f49c9ce6b9d1d1e1d191d08aa3d6052a38c02
consti10 <geierconstantinabc@gmail.com>
2022-05-13 14:11:32 +0200

    document

--- OpenHD/ohd_video/test/main_stream_display.sh
+++ OpenHD/ohd_video/test/main_stream_display.sh
@@ -1,3 +1,4 @@
 # bin/bash
+# If video shows up after some time, the main stream is working ;)
 
 gst-launch-1.0 -v udpsrc port=5620 caps = "application/x-rtp, media=(string)video, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
