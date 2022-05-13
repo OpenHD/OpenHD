523fb06847d6c5b5ad8bd439e013804f679c6605
consti10 <geierconstantinabc@gmail.com>
2022-05-13 22:17:39 +0200

    i think i've found the issue

--- OpenHD/ohd_common/openhd-util.hpp
+++ OpenHD/ohd_common/openhd-util.hpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iostream>
 #include <cctype>
+#include <stdlib.h>
 
 inline std::string to_uppercase(std::string input) {
     for (char & it : input) {
@@ -16,16 +17,20 @@ inline std::string to_uppercase(std::string input) {
 
 inline bool run_command(std::string command, std::vector<std::string> args) {
     std::stringstream ss;
-    ss<<"["<<command;
+    ss<<command;
     for(const auto& arg:args){
         ss<<arg<<" ";
     }
-    ss<<"]";
-    std::cout<<"run command begin "<<ss.str()<<"\n";
-    boost::process::child c(boost::process::search_path(command), args);
+    std::cout<<"run command begin ["<<ss.str()<<"]\n";
+    // Some weird locale issue ?!
+    // https://man7.org/linux/man-pages/man3/system.3.html
+    auto ret=system(ss.str().c_str());
+    /*boost::process::child c(boost::process::search_path(command), args);
     c.wait();
     std::cout<<"Run command end\n";
-    return c.exit_code() == 0;
+    return c.exit_code() == 0;*/
+    std::cout<<"Run command end\n";
+    return ret;
 }
 
 
