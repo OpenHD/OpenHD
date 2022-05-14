e8b4904f2aa47820a5ca3eff0a912bc08972a103
consti10 <geierconstantinabc@gmail.com>
2022-05-14 19:27:45 +0200

    remove all unneeded dependencies

--- install_dep.sh
+++ install_dep.sh
@@ -4,7 +4,7 @@
 
 apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libsodium-dev \
-            libusb-1.0-0-dev  libv4l-dev libnl-3-dev libnl-genl-3-dev libfmt-dev \
+            libusb-1.0-0-dev libv4l-dev libnl-3-dev libnl-genl-3-dev \
             libboost-dev libboost-system-dev libasio-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev \
             libgstreamer-plugins-base1.0-dev \
             libv4l-dev libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
