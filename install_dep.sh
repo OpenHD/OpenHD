b26439e89ba8ce39048fabb766644671470ebb3a
consti10 <geierconstantinabc@gmail.com>
2022-05-14 19:23:08 +0200

    remove all unneeded dependencies

--- install_dep.sh
+++ install_dep.sh
@@ -3,10 +3,9 @@
 # Install all the dependencies needed to build OpenHD from source.
 
 apt -y install cmake build-essential autotools-dev automake libtool autoconf \
-            libpcap-dev libpng-dev libsdl2-dev libsdl1.2-dev libconfig++-dev \
-            libreadline-dev libjpeg-dev libusb-1.0-0-dev libsodium-dev \
-            libfontconfig1-dev libfreetype6-dev \
+            libpcap-dev libsodium-dev \
+            libusb-1.0-0-dev  libv4l-dev libnl-3-dev libnl-genl-3-dev libfmt-dev \
+            libboost-dev libboost-system-dev libasio-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev \
             libgstreamer-plugins-base1.0-dev \
-            libboost-dev libboost-program-options-dev libboost-system-dev libasio-dev libboost-chrono-dev libsystemd-dev \
-            libboost-regex-dev libboost-filesystem-dev libboost-thread-dev indent libv4l-dev libnl-3-dev libnl-genl-3-dev libfmt-dev \
-            libnl-3-dev libnl-nf-3-dev libnl-route-3-dev || exit 1
+            libv4l-dev libnl-3-dev libnl-genl-3-dev ibnl-route-3-dev \
+            libfmt-dev || exit 1
