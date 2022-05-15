cdf9239dd25c4ea6981221a8929a15a6edf5a612
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-05-16 00:12:02 +0200

    install tree

--- install_dep.sh
+++ install_dep.sh
@@ -9,5 +9,6 @@ apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libgstreamer-plugins-base1.0-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
+            tree \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libfmt-dev || exit 1
