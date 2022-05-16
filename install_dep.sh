455b7279551edc8463dda3009487ddf7d0ce289d
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-05-16 15:46:34 +0200

    add dependencies

--- install_dep.sh
+++ install_dep.sh
@@ -10,5 +10,10 @@ apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libusb-1.0-0-dev \
             libv4l-dev \
             tree \
+            ruby-dev \
+            python3-pip \
+            cloudsmith-cli \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libfmt-dev || exit 1
+
+gem install fpm
