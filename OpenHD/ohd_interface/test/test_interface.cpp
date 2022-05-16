1c910b62bd919f07569c3b366f67e06c9343064b
consti10 <geierconstantinabc@gmail.com>
2022-05-16 20:18:03 +0200

    return string instead of std::cout

--- OpenHD/ohd_interface/test/test_interface.cpp
+++ OpenHD/ohd_interface/test/test_interface.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
   while (true) {
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	std::cout<<"XInterface\n";
-	ohdInterface.debug();
+	std::cout<<ohdInterface.createDebug();
   }
 
   std::cerr << "OHDInterface stopped\n";
