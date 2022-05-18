b8b26294bd217ace6068afe717ccecdf82572f48
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-05-18 19:09:15 +0200

    original dependencies

--- install_dep_x86.sh
+++ install_dep_x86.sh
@@ -3,17 +3,14 @@
 # Install all the dependencies needed to build OpenHD from source.
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
 
-apt -y install sudo snapd build-essential autotools-dev automake libtool autoconf \
+apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libsodium-dev \
-            libboost-dev libasio-dev \
+            libboost-dev libboost-system-dev libasio-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev \
             libgstreamer-plugins-base1.0-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
-            tree \
-            ruby-dev \
-            python3-pip \
+            python-pip \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
+            libfmt-dev || exit 1
             
-snap install cmake
-cmake --version
 gem install fpm
