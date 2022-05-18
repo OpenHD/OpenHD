5ba5d00f27eb2843df561ad7603ac61664e39cae
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-05-18 21:42:51 +0200

    install repos in ubuntu

--- before-install.sh
+++ before-install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [  -n "$(uname -a | grep Ubuntu)" ]; then
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo add-apt-repository ppa:mhier/libboost-latest -y
+    sudo add-apt-repository ppa:git-core/ppa -y
+    apt update
+fi
+
 echo "-------------trying to mount boot... might not exist if jetson --------------"
 
 mount -o remount,rw /boot || true
