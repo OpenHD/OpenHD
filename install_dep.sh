bb5100d6807d32058e84b7845abf141447fa5cdc
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-05-18 15:28:56 +0200

    remove fmt

--- install_dep.sh
+++ install_dep.sh
@@ -12,7 +12,8 @@ apt -y install snapd build-essential autotools-dev automake libtool autoconf \
             tree \
             ruby-dev \
             python3-pip \
-            libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \ || exit 1
+            libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
+            
 snap install cmake
 cmake --version
 gem install fpm
