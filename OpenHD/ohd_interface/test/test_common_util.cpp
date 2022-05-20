778eb9b7bfd4bae357292221c2f05df12422d6b3
consti10 <geierconstantinabc@gmail.com>
2022-05-20 12:08:00 +0200

    add tethering hotspot

--- OpenHD/ohd_interface/test/test_common_util.cpp
+++ OpenHD/ohd_interface/test/test_common_util.cpp
@@ -7,11 +7,14 @@
 
 int main(int argc, char *argv[]) {
 
+  // We do echo 1, but the method should return "0" which stands for
+  // command succesfully executed
   auto res = OHDUtil::run_command("echo", {"1"});
   std::cout << "Res is:" << res << "\n";
-  if(res!=1){
+  if(res!=0){
 	throw std::runtime_error("run_command return does not match expected\n");
   }
+  // Here we get the actual output in the shell, which should be 1
   auto res2=OHDUtil::run_command_out("echo 1");
   if(res2.has_value()){
 	std::cout << "Res2 is:[" << res2.value() << "]\n";
