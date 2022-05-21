ff6be3f2bc14305a30dbce65840599c399f002f1
consti10 <geierconstantinabc@gmail.com>
2022-05-21 12:24:06 +0200

    develop wifi hotspot

--- OpenHD/ohd_interface/test/test_wifi_hotspot.cpp
+++ OpenHD/ohd_interface/test/test_wifi_hotspot.cpp
@@ -6,6 +6,15 @@
 
 int main(int argc, char *argv[]) {
 
+  WiFiCard wifiCard;
+  // need to manually paste the stuff in here
+  wifiCard.interface_name="wlx244bfeb71c05";
+  wifiCard.mac="24:4b:fe:b7:1c:05";
+  wifiCard.supports_hotspot= true;
+
+  WifiHotspot wifiHotspot{wifiCard};
+  wifiHotspot.start();
+
   return 0;
 
 }
