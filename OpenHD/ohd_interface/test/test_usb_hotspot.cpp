cd4d6c619acd95ce2b0a59564c989810666b8378
consti10 <geierconstantinabc@gmail.com>
2022-05-25 14:30:22 +0200

    refactor

--- OpenHD/ohd_interface/test/test_usb_hotspot.cpp
+++ OpenHD/ohd_interface/test/test_usb_hotspot.cpp
@@ -1,12 +1,21 @@
 //
 // Created by consti10 on 20.05.22.
 //
+#include <csignal>
 #include "USBTetherListener.h"
 
 int main(int argc, char *argv[]) {
 
   USBTetherListener usb_tether_listener{nullptr};
-  usb_tether_listener.loopInfinite();
+  usb_tether_listener.startLooping();
+
+  static bool quit=false;
+  signal(SIGTERM, [](int sig){ quit= true;});
+  while (!quit){
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout<<"X\n";
+  }
+  usb_tether_listener.stopLooping();
 
   return 0;
 }
