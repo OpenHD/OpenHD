126adae0410b708a998bb02e85bd2dab8642d8ae
consti10 <geierconstantinabc@gmail.com>
2022-05-31 01:15:02 +0200

    test

---
+++ OpenHD/ohd_telemetry/src/MavlinkComponent.h
@@ -0,0 +1,21 @@
+//
+// Created by consti10 on 31.05.22.
+//
+
+#ifndef OPENHD_OPENHD_OHD_VIDEO_INC_MAVLINKCOMPONENT_H_
+#define OPENHD_OPENHD_OHD_VIDEO_INC_MAVLINKCOMPONENT_H_
+
+class MavlinkComponent{
+ public:
+  /**
+   * Process a mavlink message.
+   * @return true when the message can be handled by this component, false otherwise.
+   */
+  virtual bool processMavlinkMessage();
+
+
+ private:
+  const int component_id=0;
+
+};
+#endif //OPENHD_OPENHD_OHD_VIDEO_INC_MAVLINKCOMPONENT_H_
