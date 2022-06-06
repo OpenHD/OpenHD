99a67474cfc3de7cc2856933a3d6162c59698020
consti10 <geierconstantinabc@gmail.com>
2022-06-06 16:01:35 +0200

    argh what a pain

---
+++ OpenHD/ohd_telemetry/tests/test_air_locally.cpp
@@ -0,0 +1,25 @@
+//
+// For testing, run the air code such that it can communicate with a running ground instance (on localhost)
+//
+#include <iostream>
+
+#include "../src/OHDTelemetry.hpp"
+#include "openhd-profile.hpp"
+#include "openhd-platform.hpp"
+#include <thread>
+#include <memory>
+
+int main() {
+  std::cout<< "start\n";
+  std::unique_ptr<OHDTelemetry> air;
+  {
+	OHDProfile profile{true, "YY"};
+	OHDPlatform platform{};
+	air = std::make_unique<OHDTelemetry>(platform, profile);
+  }
+  while (true) {
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout<<air->createDebug();
+  }
+}
+
