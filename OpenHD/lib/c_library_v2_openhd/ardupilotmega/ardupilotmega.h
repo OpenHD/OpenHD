572aa4ff1a4d0f6fff9c02b966299bf7149e3a87
consti10 <geierconstantinabc@gmail.com>
2022-06-08 14:48:46 +0200

    use mavlink onboard computer status message instead of openhd custom one

--- OpenHD/lib/c_library_v2_openhd/ardupilotmega/ardupilotmega.h
+++ OpenHD/lib/c_library_v2_openhd/ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH 6019957913132324128
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH -6352562172477627614
 
 #ifdef __cplusplus
 extern "C" {
--- OpenHD/lib/c_library_v2_openhd/uAvionix/uAvionix.h
+++ OpenHD/lib/c_library_v2_openhd/uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH -9191577811978371423
+#define MAVLINK_UAVIONIX_XML_HASH -4224532423739033887
 
 #ifdef __cplusplus
 extern "C" {
