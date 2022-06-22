35f67235fcb3469047e22329dd40be99cbb94c9e
consti10 <geierconstantinabc@gmail.com>
2022-06-22 23:09:38 +0200

    complete refactoring

--- OpenHD/ohd_video/test/test_video.cpp
+++ OpenHD/ohd_video/test/test_video.cpp
@@ -9,19 +9,15 @@
 #include "OHDVideo.h"
 
 int main(int argc, char *argv[]) {
-  const auto profile = profile_from_manifest();
-  const auto platform = platform_from_manifest();
+  const OHDProfile profile{true,"0"};
+  const OHDPlatform platform{};
 
-  if (profile.is_air) {
-	OHDVideo ohdVideo(platform, profile);
-	std::cout << "OHDVideo started\n";
-	while (true) {
-	  std::this_thread::sleep_for(std::chrono::seconds(5));
-	  std::cout<<"XOHDVid\n";
-	  std::cout<<ohdVideo.createDebug();
-	}
-  } else {
-	std::cout << "OHDVideo not started - not on air pi\n";
+  OHDVideo ohdVideo(platform, profile);
+  std::cout << "OHDVideo started\n";
+  while (true) {
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	std::cout<<"XOHDVid\n";
+	std::cout<<ohdVideo.createDebug();
   }
 
   std::cerr << "OHDVideo stopped\n";
