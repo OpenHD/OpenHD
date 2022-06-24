da8a7a6f2a142805d686b77d7e3852f079894ec9
consti10 <geierconstantinabc@gmail.com>
2022-06-24 23:41:47 +0200

    add option to force OpenHD into either air or ground via file (like it used to work)

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -80,10 +80,11 @@ int main(int argc, char *argv[]) {
 	// Now we need to discover detected cameras, to determine the n of cameras and then
 	// decide if we are air or ground unit
 	std::vector<Camera> cameras{};
+    // To force ground, we just skip the discovery step (0 cameras means ground automatically)
 	if (!options.force_ground){
 	  cameras = DCameras::discover(*platform);
   	}
-	// by just adding a dummy camera we automatically become air
+	// and by just adding a dummy camera we automatically become air
   	if(options.force_air && cameras.empty()) {
 		cameras.emplace_back(createDummyCamera());
 	}
