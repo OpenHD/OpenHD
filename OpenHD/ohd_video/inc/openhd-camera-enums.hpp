b6474c68bb8aac9ebac8345abfd883fa0a34195e
consti10 <geierconstantinabc@gmail.com>
2022-07-01 15:07:58 +0200

    refactor

--- OpenHD/ohd_video/inc/openhd-camera-enums.hpp
+++ OpenHD/ohd_video/inc/openhd-camera-enums.hpp
@@ -79,4 +79,45 @@ static VideoCodec string_to_video_codec(const std::string &codec) {
   return VideoCodec::Unknown;
 }
 
+// A video format refers to a selected configuration supported by OpenHD.
+// It is possible that a camera cannot do the selected configuration in HW,
+// In this case a sw encoder can be used (in case of low res streams, that will
+// work even on the pi). Example string: h264|1280x720@60 This class also
+// provides a safe way to cast from/to a readable string. However, in case
+// someone manually enters a wrong string (for example h264OOPS|1280x720@60) the
+// behaviour is undefined.
+struct VideoFormat {
+  // The video codec, we default to h264
+  VideoCodec videoCodec = VideoCodec::H264;
+  // The width of this stream, in pixels
+  int width = 640;
+  // The height of this stream, in pixels
+  int height = 480;
+  // The framerate of this stream, in frames per second.
+  int framerate = 30;
+  // For checking if 2 video formats are exactly the same
+  bool operator==(const VideoFormat &o) const {
+    return this->width == o.width && this->height == o.height &&
+           this->framerate == o.framerate;
+  }
+  // Return true if the Video format is valid, aka the values set "make sense".
+  // values <=0 mean something went wrong during parsing or similar. And for
+  // simplicity, I go with 4k and 240fps max here.
+  [[nodiscard]] bool isValid() const {
+    return videoCodec != VideoCodec::Unknown && width > 0 && height > 0 &&
+           framerate > 0 && width <= 4096 && height <= 2160 && framerate <= 240;
+  }
+  /**
+   * Convert the VideoFormat into a readable string, in this format it can be
+   * parsed back by regex.
+   * @return the video format in a readable form.
+   */
+  [[nodiscard]] std::string toString() const {
+    std::stringstream ss;
+    ss << video_codec_to_string(videoCodec) << "|" << width << "x" << height
+       << "@" << framerate;
+    return ss.str();
+  }
+};
+
 #endif  // OPENHD_OPENHD_OHD_VIDEO_INC_OPENHD_CAMERA_ENUMS_H_
