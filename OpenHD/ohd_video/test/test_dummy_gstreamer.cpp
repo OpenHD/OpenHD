5a4366798113048901a180fc72d9ad4b063386a8
consti10 <geierconstantinabc@gmail.com>
2022-07-02 16:23:05 +0200

    use the macros to serialize structs as much as possible

--- OpenHD/ohd_video/test/test_dummy_gstreamer.cpp
+++ OpenHD/ohd_video/test/test_dummy_gstreamer.cpp
@@ -8,29 +8,37 @@
 // Independent of OpenHD, start a stream for the dummy camera
 // Which rn is implemented in gstreamer.
 
+static void update_settings(int index,CameraHolder& camera_holder){
+  if(index==0){
+    camera_holder.get_settings().userSelectedVideoFormat.videoCodec=VideoCodec::H264;
+  }else if(index==1){
+    camera_holder.get_settings().userSelectedVideoFormat.videoCodec=VideoCodec::H265;
+  }else{
+    camera_holder.get_settings().userSelectedVideoFormat.videoCodec=VideoCodec::MJPEG;
+  }
+}
+
 int main(int argc, char *argv[]) {
   //
-  Camera camera=createDummyCamera();
+  auto camera_holder=createDummyCamera2();
   // Depending on what you selected here, you will have to use the proper main_stream_display_XXX.sh if you want to see the video.
-  camera.settings.userSelectedVideoFormat.videoCodec=VideoCodec::H264;
+  camera_holder->get_settings().userSelectedVideoFormat.videoCodec=VideoCodec::H264;
   //camera.settings.userSelectedVideoFormat.videoCodec=VideoCodecH265;
   //camera.settings.userSelectedVideoFormat.videoCodec=VideoCodecMJPEG;
-  PlatformType platformType;
+  PlatformType platformType{};
   uint16_t video_port = OHD_VIDEO_AIR_VIDEO_STREAM_1_UDP;
-  auto stream = std::make_unique<GStreamerStream>(platformType, camera, video_port);
+  auto stream = std::make_unique<GStreamerStream>(platformType, camera_holder, video_port);
   stream->setup();
   stream->start();
+
+  int index=0;
 
   while (true) {
 	std::this_thread::sleep_for(std::chrono::seconds(10));
 	std::cout<<"XOHDVid\n";
 	std::cout << stream->createDebug() << "\n";
-    VideoFormat video_format;
-    video_format.videoCodec=camera.settings.userSelectedVideoFormat.videoCodec;
-    video_format.width=640;
-    video_format.height=480;
-    video_format.framerate=30;
-    stream->set_format(video_format);
+        update_settings(index,*camera_holder);
+        index++;
   }
 
   std::cerr << "OHDVideo stopped\n";
