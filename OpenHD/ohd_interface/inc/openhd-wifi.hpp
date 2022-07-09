1ce189a23feedef0b6a22601ed4136cde20962df
consti10 <geierconstantinabc@gmail.com>
2022-07-09 19:32:10 +0200

    work on interface / wifi settings

--- OpenHD/ohd_interface/inc/openhd-wifi.hpp
+++ OpenHD/ohd_interface/inc/openhd-wifi.hpp
@@ -7,6 +7,8 @@
 #include "json.hpp"
 #include "openhd-util.hpp"
 #include "openhd-log.hpp"
+#include "openhd-settings.hpp"
+#include "openhd-util-filesystem.hpp"
 
 enum class WiFiCardType {
   Unknown = 0,
@@ -116,20 +118,68 @@ struct WiFiCard {
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(WiFiCard,driver_name,type,interface_name,mac,supports_5ghz,supports_2ghz,
                                    supports_injection,supports_hotspot,supports_rts,settings)
 
+static const std::string WIFI_SETTINGS_DIRECTORY=std::string(BASE_PATH)+std::string("interface/");
 // WifiCardHolder is used to
 // 1) Differentiate between immutable information (like mac address) and
 // 2) mutable WiFi card settings.
 // Setting changes are propagated through this class.
 class WifiCardHolder{
  public:
-  explicit WifiCardHolder(WiFiCard card):_card(std::move(card)){
-
+  explicit WifiCardHolder(WiFiCard wifi_card):_wifi_card(std::move(wifi_card)){
+    if(!OHDFilesystemUtil::exists(WIFI_SETTINGS_DIRECTORY.c_str())){
+      OHDFilesystemUtil::create_directory(WIFI_SETTINGS_DIRECTORY);
+    }
+    const auto last_settings_opt=read_last_settings();
+    if(last_settings_opt.has_value()){
+      _settings=std::make_unique<WifiCardSettings>(last_settings_opt.value());
+      std::cout<<"Found settings in:"<<get_unique_filename()<<"\n";
+    }else{
+      std::cout<<"Creating default settings:"<<get_unique_filename()<<"\n";
+      // create default settings and persist them for the next reboot
+      _settings=std::make_unique<WifiCardSettings>();
+      persist_settings();
+    }
   }
   // delete copy and move constructor
   WifiCardHolder(const WifiCardHolder&)=delete;
   WifiCardHolder(const WifiCardHolder&&)=delete;
  public:
-  const WiFiCard _card;
+  const WiFiCard _wifi_card;
+  [[nodiscard]] const WifiCardSettings& get_settings()const{
+    assert(_settings);
+    return *_settings;
+  }
+ private:
+  std::unique_ptr<WifiCardSettings> _settings;
+  [[nodiscard]] std::string get_uniqe_hash()const{
+    std::stringstream ss;
+    ss<<wifi_card_type_to_string(_wifi_card.type)<<"_"<<_wifi_card.mac;
+    return ss.str();
+  }
+  [[nodiscard]] std::string get_unique_filename()const{
+    return WIFI_SETTINGS_DIRECTORY+get_uniqe_hash();
+  }
+  // write settings locally for persistence
+  void persist_settings()const{
+    assert(_settings);
+    const auto filename= get_unique_filename();
+    const nlohmann::json tmp=*_settings;
+    // and write them locally for persistence
+    std::ofstream t(filename);
+    t << tmp.dump(4);
+    t.close();
+  }
+  // read last settings, if they are available
+  [[nodiscard]] std::optional<WifiCardSettings> read_last_settings()const{
+    const auto filename= get_unique_filename();
+    if(!OHDFilesystemUtil::exists(filename.c_str())){
+      return std::nullopt;
+    }
+    std::ifstream f(filename);
+    nlohmann::json j;
+    f >> j;
+    return j.get<WifiCardSettings>();
+  }
 };
 
 
