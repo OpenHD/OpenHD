bba157ca6903fa6bf9bed1f51d0592cd615f1e87
consti10 <geierconstantinabc@gmail.com>
2022-07-10 16:57:31 +0200

    test wifi hotspot

--- OpenHD/ohd_interface/test/test_wifi_hotspot.cpp
+++ OpenHD/ohd_interface/test/test_wifi_hotspot.cpp
@@ -10,8 +10,10 @@ int main(int argc, char *argv[]) {
 
   WiFiCard wifiCard;
   // need to manually paste the stuff in here
-  wifiCard.interface_name="wlx244bfeb71c05";
-  wifiCard.mac="24:4b:fe:b7:1c:05";
+  //wifiCard.interface_name="wlx244bfeb71c05";
+  //wifiCard.mac="24:4b:fe:b7:1c:05";
+  wifiCard.interface_name="wlan0";
+  wifiCard.mac = "e4:5f:01:b0:55:92";
   wifiCard.supports_hotspot= true;
 
   WifiHotspot wifiHotspot{wifiCard};
