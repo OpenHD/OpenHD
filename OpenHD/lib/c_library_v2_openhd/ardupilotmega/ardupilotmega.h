490aa6a6b3f6ab23ec9edb5785afe2d39e0e7b2d
consti10 <geierconstantinabc@gmail.com>
2022-07-11 17:59:02 +0200

    generate mavlink c lib

--- OpenHD/lib/c_library_v2_openhd/ardupilotmega/ardupilotmega.h
+++ OpenHD/lib/c_library_v2_openhd/ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH -6352562172477627614
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH -2879982527469505124
 
 #ifdef __cplusplus
 extern "C" {
--- OpenHD/lib/c_library_v2_openhd/openhd/testsuite.h
+++ OpenHD/lib/c_library_v2_openhd/openhd/testsuite.h
@@ -93,6 +93,69 @@ static void mavlink_test_openhd_wifibroadcast_statistics(uint8_t system_id, uint
 #endif
 }
 
+static void mavlink_test_openhd_wifi_card(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_OPENHD_WIFI_CARD >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_openhd_wifi_card_t packet_in = {
+        963497464,963497672,963497880,963498088,963498296
+    };
+    mavlink_openhd_wifi_card_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.signal_millidBm = packet_in.signal_millidBm;
+        packet1.count_p_received = packet_in.count_p_received;
+        packet1.count_p_injected = packet_in.count_p_injected;
+        packet1.count_p_tx_err = packet_in.count_p_tx_err;
+        packet1.count_p_rx_err = packet_in.count_p_rx_err;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_OPENHD_WIFI_CARD_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_OPENHD_WIFI_CARD_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_wifi_card_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_openhd_wifi_card_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_wifi_card_pack(system_id, component_id, &msg , packet1.signal_millidBm , packet1.count_p_received , packet1.count_p_injected , packet1.count_p_tx_err , packet1.count_p_rx_err );
+    mavlink_msg_openhd_wifi_card_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_wifi_card_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.signal_millidBm , packet1.count_p_received , packet1.count_p_injected , packet1.count_p_tx_err , packet1.count_p_rx_err );
+    mavlink_msg_openhd_wifi_card_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_openhd_wifi_card_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_openhd_wifi_card_send(MAVLINK_COMM_1 , packet1.signal_millidBm , packet1.count_p_received , packet1.count_p_injected , packet1.count_p_tx_err , packet1.count_p_rx_err );
+    mavlink_msg_openhd_wifi_card_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("OPENHD_WIFI_CARD") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_OPENHD_WIFI_CARD) != NULL);
+#endif
+}
+
 static void mavlink_test_openhd_log_message(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -216,6 +279,7 @@ static void mavlink_test_openhd_version_message(uint8_t system_id, uint8_t compo
 static void mavlink_test_openhd(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_openhd_wifibroadcast_statistics(system_id, component_id, last_msg);
+    mavlink_test_openhd_wifi_card(system_id, component_id, last_msg);
     mavlink_test_openhd_log_message(system_id, component_id, last_msg);
     mavlink_test_openhd_version_message(system_id, component_id, last_msg);
 }
