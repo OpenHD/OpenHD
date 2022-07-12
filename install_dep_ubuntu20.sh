2957272c1efb9a2349e42fd784e1b449f5e0d21b
consti10 <geierconstantinabc@gmail.com>
2022-07-12 02:02:56 +0200

    document

---
+++ install_dep_ubuntu20.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Install all the dependencies needed to build OpenHD from source.
+# TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
+
+
+sudo apt -y install build-essential autotools-dev automake libtool autoconf \
+            libpcap-dev libsodium-dev \
+            libboost-all-dev libasio-dev \
+            libgstreamer-plugins-base1.0-dev \
+            libusb-1.0-0-dev \
+            libv4l-dev \
+            apt-transport-https \
+            curl \
+            ruby-dev \
+            python3-pip \
+            libnl-3-dev cmake snapd libboost-all-dev libnl-genl-3-dev \
+            libnl-route-3-dev \
+            libfmt-dev || exit 1
