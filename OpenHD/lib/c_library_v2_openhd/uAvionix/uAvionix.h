b64eb9cf3186caf2e39b46ae7e09534b797d4c64
consti10 <geierconstantinabc@gmail.com>
2022-07-14 14:38:05 +0200

    update mavlink

--- OpenHD/lib/c_library_v2_openhd/uAvionix/uAvionix.h
+++ OpenHD/lib/c_library_v2_openhd/uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH -6642050455488362292
+#define MAVLINK_UAVIONIX_XML_HASH 9021572791922078631
 
 #ifdef __cplusplus
 extern "C" {
