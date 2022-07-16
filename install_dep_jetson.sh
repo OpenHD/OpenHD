24c63ba844a7b0c77b5cc302e093682f650ae1c5
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-17 01:08:12 +0200

    Update install_dep_jetson.sh

--- install_dep_jetson.sh
+++ install_dep_jetson.sh
@@ -13,6 +13,7 @@ sudo apt -y install build-essential autotools-dev automake libtool autoconf \
             python3-pip \
             apt-transport-https \
             curl \
+            git \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
 
         sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
