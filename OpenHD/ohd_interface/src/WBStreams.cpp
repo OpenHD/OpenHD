25e9032e9126e031395e6c837240fcd3dfe306ad
consti10 <geierconstantinabc@gmail.com>
2022-07-20 02:35:28 +0200

    add async restart

--- OpenHD/ohd_interface/src/WBStreams.cpp
+++ OpenHD/ohd_interface/src/WBStreams.cpp
@@ -506,16 +506,17 @@ void WBStreams::process_new_setting(openhd::Setting changed_setting) {
 
 void WBStreams::restart_async(std::chrono::milliseconds delay){
   std::lock_guard<std::mutex> guard(_restart_async_lock);
- if(_restart_async_thread!= nullptr){
-   std::cerr<<"WBStreams::restart_async - settings changed too quickly\n";
-   if(_restart_async_thread->joinable()){
-	 _restart_async_thread->join();
-   }
-   _restart_async_thread=nullptr;
- }
- _restart_async_thread=std::make_unique<std::thread>([this,delay]{
-   std::this_thread::sleep_for(delay);
-   this->restart();
- }
- );
+  if(_restart_async_thread!= nullptr){
+	std::cerr<<"WBStreams::restart_async - settings changed too quickly\n";
+	if(_restart_async_thread->joinable()){
+	  _restart_async_thread->join();
+	}
+	_restart_async_thread=nullptr;
+  }
+  _restart_async_thread=
+	  std::make_unique<std::thread>([this,delay]{
+									  std::this_thread::sleep_for(delay);
+									  this->restart();
+									}
+	  );
 }
