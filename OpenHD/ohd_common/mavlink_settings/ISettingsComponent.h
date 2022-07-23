791908b77d057528bec5f8b4d79a361979703a26
consti10 <geierconstantinabc@gmail.com>
2022-07-23 12:51:22 +0200

    delete unused

--- OpenHD/ohd_common/mavlink_settings/ISettingsComponent.h
+++ OpenHD/ohd_common/mavlink_settings/ISettingsComponent.h
@@ -47,25 +47,6 @@ class ISettingsComponent{
 };
 
 
-/*using SettingsVariant= std::variant<int,float,std::string>;
-struct Setting{
-  const std::string id;
-  SettingsVariant value;
-};
-
-class XSettingsComponent{
- public:
-  // all the settings this component uses
-  virtual std::vector<Setting> get_all_settings()=0;
-  // perform required steps when a setting value changes.
-  virtual void process_setting_changed(Setting changed_setting)=0;
- public:
-  XSettingsComponent()=default;
-  // delete copy and move constructor
-  XSettingsComponent(const XSettingsComponent&)=delete;
-  XSettingsComponent(const XSettingsComponent&&)=delete;
-};*/
-
 static void validate_provided_ids(const std::vector<Setting>& settings){
   // we need to have unique setting string ids. If there is a duplicate, this would be a programmers error,
   // and when used correctly should be found during debugging by this method.
@@ -76,46 +57,6 @@ static void validate_provided_ids(const std::vector<Setting>& settings){
   }
 }
 
-/*bool safe_to(std::bool& value,const SettingsVariant& settings_variant){
-  if(std::holds_alternative<bool>(settings_variant)){
-    const auto new_value=std::get<bool>(settings_variant);
-    if(new_value!=value){
-      value=new_value;
-      return true;
-    }
-  }
-  return false;
-}*/
-/*static bool safe_to(int& value,const SettingsVariant& settings_variant){
-  if(std::holds_alternative<int>(settings_variant)){
-    const auto new_value=std::get<int>(settings_variant);
-    if(new_value!=value){
-      value=new_value;
-      return true;
-    }
-  }
-  return false;
-}
-static bool safe_to(float& value,const SettingsVariant& settings_variant){
-  if(std::holds_alternative<float>(settings_variant)){
-    const auto new_value=std::get<float>(settings_variant);
-    if(new_value!=value){
-      value=new_value;
-      return true;
-    }
-  }
-  return false;
-}
-static bool safe_to(std::string& value,const SettingsVariant& settings_variant){
-  if(std::holds_alternative<std::string>(settings_variant)){
-    const auto new_value=std::get<std::string>(settings_variant);
-    if(new_value!=value){
-      value=new_value;
-      return true;
-    }
-  }
-  return false;
-}*/
 
 // For testing
 namespace testing{
