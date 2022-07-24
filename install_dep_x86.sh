f32d07a889b2c701b2aaa06a1274574d9e682bce
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-24 13:13:26 +0200

    debug

--- install_dep_x86.sh
+++ install_dep_x86.sh
@@ -4,7 +4,6 @@
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
 
 apt -y install cmake build-essential autotools-dev automake libtool autoconf \
-           # libpcap-dev libsodium-dev \
             libboost-dev libboost-system-dev libasio-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev \
             libgstreamer-plugins-base1.0-dev \
             libusb-1.0-0-dev \
