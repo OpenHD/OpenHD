916916bf9c8c8c9b4335a3e6f8c159fbab1eda56
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-24 13:12:11 +0200

    debug

--- install_dep_x86.sh
+++ install_dep_x86.sh
@@ -4,7 +4,15 @@
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
 
 apt -y install cmake build-essential autotools-dev automake libtool autoconf \
-             || exit 1
+           # libpcap-dev libsodium-dev \
+            libboost-dev libboost-system-dev libasio-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev \
+            libgstreamer-plugins-base1.0-dev \
+            libusb-1.0-0-dev \
+            libv4l-dev \
+            apt-transport-https \
+            curl \
+            libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
+            libfmt-dev || exit 1
             
 gem install fpm
 pip install --upgrade cloudsmith-cli
