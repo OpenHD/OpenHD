2bcbddcd4b6d38790404f3dc2bba454bb63cfc0a
consti10 <geierconstantinabc@gmail.com>
2022-07-25 16:42:30 +0200

    serial - constant reconnect if broken

--- OpenHD/ohd_telemetry/src/mavsdk_temporary/XMavsdkWrapperSerialConnection.cpp
+++ OpenHD/ohd_telemetry/src/mavsdk_temporary/XMavsdkWrapperSerialConnection.cpp
@@ -8,41 +8,73 @@
 namespace mavsdk {
 
 XMavsdkWrapperSerialConnection::XMavsdkWrapperSerialConnection(
-    const std::string& path, int baudrate, bool flow_control)
-    : MEndpoint("SerialFC"),_path(path) {
+    const std::optional<std::string> path, int baudrate, bool flow_control)
+    : MEndpoint("SerialFC"),_path(path),_baudrate(baudrate),_flow_control(flow_control) {
   _for_mavsdk_receiver_callback=[this](mavlink_message_t& message, Connection* connection){
     MEndpoint::parseNewDataEmulateForMavsdk(message);
   };
-  _serial_connection=std::make_unique<SerialConnection>(_for_mavsdk_receiver_callback,path,baudrate,flow_control);
   establish_connection_thread = std::make_unique<std::thread>(&XMavsdkWrapperSerialConnection::constantConnect, this);
 }
 
 void XMavsdkWrapperSerialConnection::sendMessageImpl(
     const MavlinkMessage& message) {
   std::lock_guard<std::mutex> lock(_mutex);
-  if(!_started){
-	return;
-  }
-  const auto result=_serial_connection->send_message(message.m);
-  if(!result){
-    // cannot send - probably disconnected
+  if(_serial_connection){
+	const auto result=_serial_connection->send_message(message.m);
+	if(!result){
+	  std::cout<<"XMavsdkWrapperSerialConnection:: serial most likely disconnected\n";
+	  // cannot send - probably disconnected
+	  _serial_connection->stop();
+	  _serial_connection= nullptr;
+	}
   }
 }
-void XMavsdkWrapperSerialConnection::constantConnect() {
+
+std::optional<std::string> XMavsdkWrapperSerialConnection::check_serial_file_handles() {
+  if(_path.has_value()){
+	if(!OHDFilesystemUtil::exists(_path.value().c_str())){
+	  std::cout<<"UART file does not exist\n";
+	  return std::nullopt;
+	}
+	return _path.value();
+  }else{
+	// most common paths
+	const std::string path1="/dev/ttyACM0";
+	const std::string path2="/dev/ttyACM1";
+	if(OHDFilesystemUtil::exists(path1.c_str())){
+	  return path1;
+	}
+	if(OHDFilesystemUtil::exists(path2.c_str())){
+	  return path2;
+	}
+	return std::nullopt;
+  }
+}
+
+[[noreturn]] void XMavsdkWrapperSerialConnection::constantConnect() {
   while (true){
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	if(!OHDFilesystemUtil::exists(_path.c_str())){
-	  std::cout<<"UART file does not exist\n";
+	std::lock_guard<std::mutex> lock(_mutex);
+	if(_serial_connection){
 	  continue;
 	}
-    const auto result=_serial_connection->start();
-	std::cout<<"XMavsdkWrapperSerialConnection:"<<result<<"\n";
-    if(result!=mavsdk::ConnectionResult::Success){
-	  continue;
-    }
-	_started=true;
-	return;
+	const auto path_opt=check_serial_file_handles();
+	if(path_opt.has_value()){
+	  const auto& path=path_opt.value();
+	  std::cout<<"XMavsdkWrapperSerialConnection::start "<<path;
+	  _serial_connection=std::make_unique<SerialConnection>(_for_mavsdk_receiver_callback,path,_baudrate,_flow_control);
+	  const auto result=_serial_connection->start();
+	  std::cout<<"XMavsdkWrapperSerialConnection:"<<result<<"\n";
+	  if(result!=mavsdk::ConnectionResult::Success){
+		std::cerr<<"Cannot create serial connection on "<<path;
+		_serial_connection= nullptr;
+		continue;
+	  }
+	}else{
+	  std::cout<<"Cannot find valid UART file handle\n";
+	}
   }
 }
+
 
 }
--- OpenHD/ohd_telemetry/src/mavsdk_temporary/XMavsdkWrapperSerialConnection.h
+++ OpenHD/ohd_telemetry/src/mavsdk_temporary/XMavsdkWrapperSerialConnection.h
@@ -9,25 +9,28 @@
 #include "serial_connection.h"
 #include <memory>
 #include <thread>
+#include <optional>
 
 namespace mavsdk{
 
 class XMavsdkWrapperSerialConnection : public MEndpoint{
  public:
   explicit XMavsdkWrapperSerialConnection(
-      const std::string& path,
+      const std::optional<std::string> path,
       int baudrate,
       bool flow_control=false);
  private:
   void sendMessageImpl(const MavlinkMessage &message) override;
+  std::optional<std::string> check_serial_file_handles();
  private:
-  void constantConnect();
+  [[noreturn]] void constantConnect();
   Connection::receiver_callback_t _for_mavsdk_receiver_callback;
   std::unique_ptr<SerialConnection> _serial_connection;
   std::unique_ptr<std::thread> establish_connection_thread = nullptr;
   std::mutex _mutex{};
-  const std::string _path;
-  bool _started=false;
+  std::optional<std::string> _path;
+  int _baudrate;
+  bool _flow_control;
 };
 
 }
