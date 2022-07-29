bdae003a71ad4ea1381af011b390ee2b1bae5356
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-30 00:54:43 +0200

    add backports

--- install_dep_legacy.sh
+++ install_dep_legacy.sh
@@ -3,6 +3,8 @@
 # Install all the dependencies needed to build OpenHD from source.
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
    
+echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list.d/backports.list
+apt update 
 
 apt -y install build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libsodium-dev \
@@ -16,7 +18,7 @@ apt -y install build-essential autotools-dev automake libtool autoconf \
             curl \
             git \
             ruby-dev \
-            libboost-filesystem-dev \
+            libboost-filesystem1.74-dev \
             python3-pip \
             libnl-3-dev cmake snapd libnl-genl-3-dev \
             libnl-route-3-dev || exit 1
