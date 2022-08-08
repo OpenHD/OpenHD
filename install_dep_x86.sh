0c87182e0c323a8a8099b34085efafc12cb0b34f
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-08-08 15:58:48 +0200

    x86 boost

--- install_dep_x86.sh
+++ install_dep_x86.sh
@@ -5,7 +5,7 @@
 
 apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libsodium-dev \
-            libboost-dev libboost-system-dev libasio-dev libboost-filesystem-dev libboost-thread-dev libboost-regex-dev \
+            libboost1.74-dev libboost1.74-system-dev libasio-dev libboost1.74-filesystem-dev libboost1.74-thread-dev libboost1.74-regex-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
             apt-transport-https \
@@ -15,6 +15,3 @@ apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             
 gem install fpm
 pip install --upgrade cloudsmith-cli
-
-
-# fails            libgstreamer-plugins-base1.0-dev \
