c474f83fa3a3b034f1ade0bd7d7902b6e352c3ed
consti10 <geierconstantinabc@gmail.com>
2022-08-15 21:18:31 +0200

    add led test

--- OpenHD/ohd_interface/test/test_led.cpp
+++ OpenHD/ohd_interface/test/test_led.cpp
@@ -9,10 +9,19 @@
 
 int main(int argc, char *argv[]) {
 
-  while(true){
+  const auto start1=std::chrono::steady_clock::now();
+  while((std::chrono::steady_clock::now()-start1)<std::chrono::seconds(10)){
 	openhd::rpi::toggle_red_led(false);
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	openhd::rpi::toggle_red_led(false);
+	openhd::rpi::toggle_red_led(true);
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  const auto start2=std::chrono::steady_clock::now();
+  while((std::chrono::steady_clock::now()-start2)<std::chrono::seconds(10)){
+	openhd::rpi::toggle_green_led(false);
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	openhd::rpi::toggle_green_led(true);
 	std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 }
