a96b070f35dae7cfe3ba49cc329e520f40ec93ff
consti10 <geierconstantinabc@gmail.com>
2022-08-15 14:41:44 +0200

    simplify

--- OpenHD/ohd_interface/test/test_usb_hotspot.cpp
+++ OpenHD/ohd_interface/test/test_usb_hotspot.cpp
@@ -1,12 +1,16 @@
 //
 // Created by consti10 on 20.05.22.
 //
-#include <csignal>
+
 #include "USBTetherListener.h"
+#include "openhd-util.hpp"
 
 int main(int argc, char *argv[]) {
 
-  USBTetherListener usb_tether_listener{nullptr};
+  auto cb=[](openhd::ExternalDevice external_device,bool connected){
+	std::cout<<"Callback called with "<<external_device.to_string()<<" connected:"<<OHDUtil::yes_or_no(connected)<<"\n";
+  };
+  USBTetherListener usb_tether_listener{cb};
   usb_tether_listener.startLooping();
 
   OHDUtil::keep_alive_until_sigterm();
