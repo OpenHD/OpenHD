a5188a2a4019df4f2f11adaf0908999348f2f4ed
consti10 <geierconstantinabc@gmail.com>
2022-08-16 00:02:23 +0200

    add keyframe interval

--- OpenHD/ohd_video/src/veyestream.cpp
+++ OpenHD/ohd_video/src/veyestream.cpp
@@ -51,6 +51,9 @@ void VEYEStream::setup() {
   if(setting.userSelectedVideoFormat.videoCodec==VideoCodec::H264){
 	ss<<"--codec H264 ";
 	ss<<"--profile baseline ";
+	// TODO check
+	// -g, --intra	: Specify the intra refresh period (key frame rate/GoP size). Zero to produce an initial I-frame and then just P-frames.
+	//ss<<"--intra "<<setting.keyframe_interval<<" ";
   }else if(setting.userSelectedVideoFormat.videoCodec==VideoCodec::MJPEG){
 	ss<<"--codec MJPEG ";
   }else{
