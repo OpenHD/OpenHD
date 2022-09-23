892ba4e948ed93ad39184fc4c058006afd0113b2
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-23 22:56:15 +0200

    remove gstreamer in x86

--- install_dep_ubuntu20.sh
+++ install_dep_ubuntu20.sh
@@ -5,9 +5,8 @@
 # PLEASE KEEP THIS FILE AS CLEAN AS POSSIBLE, Ubuntu20 is the baseline - for other platforms / OS versions, create their own files
 
 sudo apt -y install cmake build-essential autotools-dev automake libtool autoconf \
-            libpcap-dev \
+            libpcap-dev libsodium-dev \
             libboost-dev libboost-filesystem-dev \
-            libgstreamer-plugins-base1.0-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
             libavcodec-dev \
