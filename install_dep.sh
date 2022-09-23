6213fcbb8f3f6f45dcb854d487130d2575f83c41
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-09-23 21:18:37 +0200

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -4,24 +4,10 @@
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
    
 
-apt -y install build-essential autotools-dev automake libtool autoconf \
-            libpcap-dev libsodium-dev \
-            libboost-all-dev libasio-dev \
-            libgstreamer-plugins-base1.0-dev \
-            libusb-1.0-0-dev \
-            libv4l-dev \
-            git \
-            apt-transport-https \
-            curl \
-            libavcodec-dev \
-            ruby-dev \
-            python3-pip \
-            libnl-3-dev cmake snapd libnl-genl-3-dev \
-            libnl-route-3-dev \
-			libcamera-dev || exit 1
-
+apt -y install build-essential autotools-dev automake libtool autoconf libpcap-dev libsodium-dev libboost-all-dev 
+apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl libavcodec-dev 
+apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev libcamera-dev 
+apt -y install fmt
 
 gem install fpm
-pip install --upgrade cloudsmith-cli
 
-apt -y install fmt
