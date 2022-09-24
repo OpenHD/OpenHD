0f5c8a781ba92224e713bb19a898ec745097fcd7
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-24 16:08:02 +0200

    syntax

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -7,9 +7,9 @@
 sudo apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libsodium-dev \
             libboost-dev libboost-filesystem-dev \
-            libgstreamer-plugins-base1.0-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
             libavcodec-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
-            libfmt-dev libunwind-14-dev- || exit 1
+            libfmt-dev  || exit 1
+sudo apt -y install libgstreamer-plugins-base1.0-dev libunwind-14-dev-
