6cab86e785c3d55ab0f83f7dc08422803f622c37
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-02 19:08:18 +0200

    typo

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -106,19 +106,19 @@ static OHDRunOptions parse_run_parameters(int argc, char *argv[]){
       }
       if(OHDFilesystemUtil::exists("/boot/OpenHD/libcamera.txt"))
       {
-      OHDUtil::run_command("sed -i '/camera_auto_detect=1/d' /boot/config.txt"),{};
-      OHDUtil::run_command("sed -i '/dtoverlay=vc4-kms-v3d/d' /boot/config.txt"),{};
-      OHDUtil::run_command("sed -i '/dtoverlay=vc4-fkms-v3d/d' /boot/config.txt"),{};
-      OHDUtil::run_command("sed -i '/start_x=1/d' /boot/config.txt"),{};
-      OHDUtil::run_command("sed -i '/enable_uart=1/d' /boot/config.txt"),{};
+      OHDUtil::run_command("sed -i '/camera_auto_detect=1/d' /boot/config.txt",{});
+      OHDUtil::run_command("sed -i '/dtoverlay=vc4-kms-v3d/d' /boot/config.txt",{});
+      OHDUtil::run_command("sed -i '/dtoverlay=vc4-fkms-v3d/d' /boot/config.txt",{});
+      OHDUtil::run_command("sed -i '/start_x=1/d' /boot/config.txt",{});
+      OHDUtil::run_command("sed -i '/enable_uart=1/d' /boot/config.txt",{});
       // Add raspicamsrc configs
-      OHDUtil::run_command("sed -i '$ a start_x=1' /boot/config.txt"),{};
-      OHDUtil::run_command("sed -i '$ a dtoverlay=vc4-fkms-v3d' /boot/config.txt"),{};
-      OHDUtil::run_command("sed -i '$ a enable_uart=1' /boot/config.txt"),{};
+      OHDUtil::run_command("sed -i '$ a start_x=1' /boot/config.txt",{});
+      OHDUtil::run_command("sed -i '$ a dtoverlay=vc4-fkms-v3d' /boot/config.txt",{});
+      OHDUtil::run_command("sed -i '$ a enable_uart=1' /boot/config.txt",{});
       // Remove config File and reboot
-      OHDUtil::run_command("rm -Rf /boot/OpenHD/raspicam.txt"),{};
+      OHDUtil::run_command("rm -Rf /boot/OpenHD/raspicam.txt",{});
       OHDUtil::run_command("echo",{"This device will now reboot to enable configs"});
-      OHDUtil::run_command("shutdown -r now"),{};
+      OHDUtil::run_command("shutdown -r now",{});
       }
 
     } 
