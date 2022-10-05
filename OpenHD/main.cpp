22435b450f4444e3f8c1ac3e06106a4033c951c6
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-05 12:58:34 +0200

    Update main.cpp

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -104,7 +104,7 @@ static OHDRunOptions parse_run_parameters(int argc, char *argv[]){
       OHDUtil::run_command("echo",{"This device will now reboot to enable configs"});
       OHDUtil::run_command("reboot",{});
       }
-      if(OHDFilesystemUtil::exists("/boot/OpenHD/libcamera.txt"))
+      if(OHDFilesystemUtil::exists("/boot/OpenHD/raspicamsrc.txt"))
       {
       OHDUtil::run_command("sed -i '/camera_auto_detect=1/d' /boot/config.txt",{});
       OHDUtil::run_command("sed -i '/dtoverlay=vc4-kms-v3d/d' /boot/config.txt",{});
