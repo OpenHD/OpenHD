a27958c4894be406b7b82b858459eb456d15ce0f
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-28 22:22:22 +0200

    add spedlog

--- install_dep.sh
+++ install_dep.sh
@@ -7,7 +7,7 @@
 apt -y install build-essential autotools-dev automake libtool autoconf libpcap-dev libsodium-dev libboost-all-dev 
 apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl libavcodec-dev 
 apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev libcamera-dev 
-apt -y install fmt
+apt -y install fmt libspdlog-dev
 
 gem install fpm
 
