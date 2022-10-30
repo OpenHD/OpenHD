d490fb6204a03d922101eeb78ffea77151a558a6
consti10 <geierconstantinabc@gmail.com>
2022-10-30 18:34:47 +0100

    document

--- OpenHD/ohd_video/inc/libcamera_provider.hpp
+++ OpenHD/ohd_video/inc/libcamera_provider.hpp
@@ -30,10 +30,11 @@ class LibcameraProvider {
           }
 	}
         // This should free all the shared pointers we might still have, such that hopefully
-        // we can call stop() later without erros. The documentation is a bit dubios here -
+        // we can call stop() later without errors. The documentation is a bit dubios here -
         // https://libcamera.org/api-html/classlibcamera_1_1CameraManager.html
         // Before stopping the camera manager the caller is responsible for making sure all cameras provided by the manager are returned to the manager.
         // well, I don't think we call get so to say but we kept the shared pointer(s) around before calling stop() previously
+        // Here we get rid of all the shared pointers we still hold
         lcCameras.resize(0);
 
 	// We need to stop camera manager because it can be only one run manager in process.
