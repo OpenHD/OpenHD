f703f8f0c329cd2c67e9632ac6ccc3587bed1cc7
consti10 <geierconstantinabc@gmail.com>
2022-11-03 00:35:26 +0100

    debug

--- OpenHD/ohd_common/openhd-rpi-os-configure-vendor-cam.hpp
+++ OpenHD/ohd_common/openhd-rpi-os-configure-vendor-cam.hpp
@@ -126,6 +126,7 @@ class ConfigChangeHandler{
   // Returns true if checks passed, false otherwise (param rejected)
   bool change_rpi_os_camera_configuration(int new_value_as_int){
     std::lock_guard<std::mutex> lock(m_mutex);
+    openhd::loggers::get_default()->debug("Cam config request change to {}",new_value_as_int);
     if(!validate_cam_config_settings_int(new_value_as_int)){
       // reject, not a valid value
       return false;
