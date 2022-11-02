d2314e580eef3cbc87e14581a3928deb695c1cba
consti10 <geierconstantinabc@gmail.com>
2022-11-02 15:21:29 +0100

    we need to update the openhd messages once more, unfortunately

--- OpenHD/lib/c_library_v2_openhd/icarous/icarous.h
+++ OpenHD/lib/c_library_v2_openhd/icarous/icarous.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ICAROUS.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ICAROUS_XML_HASH 2233269166669825572
+#define MAVLINK_ICAROUS_XML_HASH -965415002856938151
 
 #ifdef __cplusplus
 extern "C" {
--- OpenHD/lib/c_library_v2_openhd/openhd/openhd.h
+++ OpenHD/lib/c_library_v2_openhd/openhd/openhd.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_OPENHD.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_OPENHD_XML_HASH -7590463776179114564
+#define MAVLINK_OPENHD_XML_HASH 5521345177822503665
 
 #ifdef __cplusplus
 extern "C" {
