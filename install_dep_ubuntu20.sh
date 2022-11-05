fcb9cabddfc146997b4a38e5208b3a03784a520c
consti10 <geierconstantinabc@gmail.com>
2022-11-05 15:00:46 +0100

    we do not need fmt anymore - we get it from spdlog

--- install_dep_ubuntu20.sh
+++ install_dep_ubuntu20.sh
@@ -12,5 +12,4 @@ sudo apt -y install cmake build-essential autotools-dev automake libtool autocon
             libv4l-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libsdl2-dev \
-            libfmt-dev \
             libspdlog-dev || exit 1
