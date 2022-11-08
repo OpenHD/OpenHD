5bc3b73f8fef844b9b2618f6250c4dfffb09f944
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-08 01:30:06 +0100

    replace libboost-all with libboost-filesystem

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -4,7 +4,7 @@
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
    
 
-apt -y install build-essential autotools-dev automake libtool autoconf libpcap-dev libsodium-dev libboost-all-dev 
+apt -y install build-essential autotools-dev automake libtool autoconf libpcap-dev libsodium-dev libboost-filesystem1.74-dev
 apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl libavcodec-dev 
 apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev libcamera-dev
 apt -y install libspdlog-dev
