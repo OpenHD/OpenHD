142ef2ebe4eea207b41a73297f959191679ea33b
consti10 <geierconstantinabc@gmail.com>
2022-11-11 01:43:58 +0100

    was duplicated

--- install_dep_ubuntu20.sh
+++ install_dep_ubuntu20.sh
@@ -11,7 +11,6 @@ sudo apt -y install cmake build-essential autotools-dev automake libtool autocon
             libusb-1.0-0-dev \
             libv4l-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
-            libsdl2-dev \
             libspdlog-dev || exit 1
 
 # Needed for RC, optional
