78a5a4999b973b17c19fd2fcd6f96420abbbcf33
consti10 <geierconstantinabc@gmail.com>
2022-11-13 04:45:31 +0100

    exp stats

--- OpenHD/ohd_telemetry/src/internal/OHDLinkStatisticsHelper.h
+++ OpenHD/ohd_telemetry/src/internal/OHDLinkStatisticsHelper.h
@@ -23,7 +23,7 @@ static MavlinkMessage
 stats_total_all_wifibroadcast_streams_pack(const uint8_t system_id,const uint8_t component_id,
 												const openhd::link_statistics::StatsTotalAllStreams& all_stats){
   MavlinkMessage msg;
-  openhd::log::get_default()->debug("curr_video0_tx_pps {}",all_stats.curr_video0_tx_pps);
+  openhd::log::get_default()->debug("curr_video0_tx_pps X{}",all_stats.curr_video0_tx_pps);
   mavlink_openhd_stats_total_all_wifibroadcast_streams_t tmp;
   tmp.count_wifi_packets_received=all_stats.count_wifi_packets_received;
   tmp.count_bytes_received=all_stats.count_bytes_received;
