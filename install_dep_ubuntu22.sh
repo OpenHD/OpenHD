07713965878b37c851f289ab6f5525b125eeea51
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-14 18:43:05 +0100

    gstreamer

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -9,6 +9,7 @@ sudo apt -y install cmake build-essential autotools-dev automake libtool autocon
             libboost-dev libboost-filesystem-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
+            gstreamer-1.0 \
             libavcodec-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libspdlog-dev || exit 1
