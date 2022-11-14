870d5e31260197b0fd9bfdff36423cce4c40d6c2
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-14 18:52:51 +0100

    debug

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -9,12 +9,12 @@ sudo apt -y install cmake build-essential autotools-dev automake libtool autocon
             libboost-dev libboost-filesystem-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
-            libgstreamer-plugins-base1.0-dev \
+            gstreamer1.0-plugins-good \
             libavcodec-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libspdlog-dev || exit 1
 
-sudo apt -y install gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
+sudo apt -y install gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
 
 # Needed for RC, optional
 sudo apt -y install libsdl2-dev
