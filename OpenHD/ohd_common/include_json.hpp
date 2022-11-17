bab7f142207f7aa7d6587f81afa7db19693751c8
consti10 <geierconstantinabc@gmail.com>
2022-11-17 04:36:05 +0100

    exp can we stop using the single-header json include this way ?

--- OpenHD/ohd_common/include_json.hpp
+++ OpenHD/ohd_common/include_json.hpp
@@ -7,6 +7,7 @@
 
 // NOTE: Please use this one everywhere instead of including the json.hpp directly, since we want to eventually
 // switch to using json as a library
-#include "json.hpp"
+//#include "json.hpp"
+#include <nlohmann/json.hpp>
 
 #endif  // OPENHD_OPENHD_OHD_COMMON_INCLUDE_JSON_H_
