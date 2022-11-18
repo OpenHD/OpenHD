ec786e9a3d604e6bacd031fb13fcfdff8236c77e
consti10 <geierconstantinabc@gmail.com>
2022-11-18 15:26:09 +0100

    make onboard computer status test compile again

--- OpenHD/ohd_telemetry/src/internal/LogCustomOHDMessages.hpp
+++ OpenHD/ohd_telemetry/src/internal/LogCustomOHDMessages.hpp
@@ -17,43 +17,31 @@
  */
 namespace LogCustomOHDMessages {
 
-static void logOnboardComputerStatus(const mavlink_message_t &msg){
-  assert(msg.msgid == MAVLINK_MSG_ID_ONBOARD_COMPUTER_STATUS);
-  mavlink_onboard_computer_status_t decoded;
-  mavlink_msg_onboard_computer_status_decode(&msg,&decoded);
+static void logOnboardComputerStatus(const mavlink_onboard_computer_status_t& decoded){
   std::stringstream ss;
-  ss<<"MAVLINK_MSG_ID_ONBOARD_COMPUTER_STATUS: cpu_usage:"<<decoded.cpu_cores[0]<<" temp:"<<decoded.temperature_core[0]<<"\n";
-  openhd::log::get_default->debug(ss.str());
+  ss<<"MAVLINK_MSG_ID_ONBOARD_COMPUTER_STATUS: cpu_usage:"<<(int)decoded.cpu_cores[0]<<" temp:"<<(int)decoded.temperature_core[0];
+  openhd::log::get_default()->debug(ss.str());
 }
 
-static void logWifiBroadcast(const mavlink_message_t &msg) {
-  assert(msg.msgid == MAVLINK_MSG_ID_OPENHD_WIFIBROADCAST_STATISTICS);
-  mavlink_openhd_wifibroadcast_statistics_t decoded;
-  mavlink_msg_openhd_wifibroadcast_statistics_decode(&msg, &decoded);
-  std::stringstream ss;
-  ss << "MAVLINK_MSG_ID_OPENHD_WIFIBROADCAST_STATISTICS:count_p_all:" << (int)decoded.count_p_all << "count_p_lost:"
-     << (int)decoded.count_p_lost << "\n";
-  openhd::log::get_default->debug(ss.str());
-}
 static void logLogMessage(const mavlink_message_t &msg) {
   assert(msg.msgid == MAVLINK_MSG_ID_OPENHD_LOG_MESSAGE);
   mavlink_openhd_log_message_t decoded;
   mavlink_msg_openhd_log_message_decode(&msg, &decoded);
   std::stringstream ss;
-  ss << "LOG:" << decoded.severity << ":" << decoded.text << "\n";
-  openhd::log::get_default->debug(ss.str());
+  ss << "LOG:" << decoded.severity << ":" << decoded.text;
+  openhd::log::get_default()->debug(ss.str());
 }
 
 static void logOpenHDMessages(const std::vector<MavlinkMessage> &msges) {
   for (const auto &msg: msges) {
     if (msg.m.msgid == MAVLINK_MSG_ID_ONBOARD_COMPUTER_STATUS) {
-      logOnboardComputerStatus(msg.m);
-    } else if (msg.m.msgid == MAVLINK_MSG_ID_OPENHD_WIFIBROADCAST_STATISTICS) {
-      logWifiBroadcast(msg.m);
+      mavlink_onboard_computer_status_t decoded;
+      mavlink_msg_onboard_computer_status_decode(&msg.m,&decoded);
+      logOnboardComputerStatus(decoded);
     } else if (msg.m.msgid == MAVLINK_MSG_ID_OPENHD_LOG_MESSAGE) {
       logLogMessage(msg.m);
     } else {
-      openhd::log::get_default->debug("unknown ohd msg with msgid:{}",msg.m.msgid);
+      openhd::log::get_default()->debug("unknown ohd msg with msgid:{}",msg.m.msgid);
     }
   }
 }
--- OpenHD/ohd_telemetry/tests/test_onboard_computer_status_read_stuff.cpp
+++ OpenHD/ohd_telemetry/tests/test_onboard_computer_status_read_stuff.cpp
@@ -1,21 +1,24 @@
 // For the onboard computer status we read a lot of stuff,
 // This is for testing these functionalities
 
-#include "../src/internal/OnboardComputerStatus.hpp"
+#include "../src/internal/OnboardComputerStatusProvider.h"
+#include "../src/internal/LogCustomOHDMessages.hpp"
 #include "openhd-platform.hpp"
 #include "openhd-platform-discover.hpp"
+
+#include <memory>
 
 int main() {
 
   const auto platform=DPlatform::discover();
-  const auto cpu_load=openhd::read_cpuload_once_blocking();
-  if(cpu_load.has_value()){
-	std::cout<<"cpu load:"<<cpu_load.value()<<"\n";
-  }
-  std::cout<<"temp:"<<OnboardComputerStatus::readTemperature()<<"\n";
-  if(platform->platform_type==PlatformType::RaspberryPi || true){
-    std::cout<<"Rpi:\n";
-    std::cout<<""<<(int)OnboardComputerStatus::rpi::read_temperature_soc_degree()<<"\n";
+  const auto provider=std::make_unique<OnboardComputerStatusProvider>(*platform);
+
+  static bool quit=false;
+  signal(SIGTERM, [](int sig){ quit= true;});
+  while (!quit){
+    auto tmp=provider->get_current_status();
+    LogCustomOHDMessages::logOnboardComputerStatus(tmp);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
   return 0;
 }
