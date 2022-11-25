392c46300195e77268bef9d3c409dcd6df6fc6f1
consti10 <geierconstantinabc@gmail.com>
2022-11-25 04:11:47 +0100

    update mavlink

--- OpenHD/lib/c_library_v2_openhd/minimal/minimal.h
+++ OpenHD/lib/c_library_v2_openhd/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH -8088360793287203122
+#define MAVLINK_MINIMAL_XML_HASH -6968577779992209918
 
 #ifdef __cplusplus
 extern "C" {
--- OpenHD/lib/c_library_v2_openhd/openhd/openhd.h
+++ OpenHD/lib/c_library_v2_openhd/openhd/openhd.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_OPENHD.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_OPENHD_XML_HASH 5521345177822503665
+#define MAVLINK_OPENHD_XML_HASH -6670629003237026931
 
 #ifdef __cplusplus
 extern "C" {
