7845a01c9734aa232ce882c6b167e849dc49d60c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-30 19:58:41 +0100

    Update install_dep_rpi.sh

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -6,7 +6,9 @@
 
 apt -y install build-essential autotools-dev automake libtool autoconf libpcap-dev libsodium-dev libboost-filesystem1.74-dev
 apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl
-apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev libcamera-openhd 
+apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev 
+apt -y remove  python3-libcamera
+apt -y install libcamera-openhd 
 apt -y install libspdlog-dev
 
 # Needed for RC, optional
