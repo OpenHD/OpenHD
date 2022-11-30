eff351dc2140f2937c60a4bcb1fb8bc25b4e14c0
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-30 20:20:35 +0100

    also remove libcamera0

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -7,7 +7,7 @@
 apt -y install build-essential autotools-dev automake libtool autoconf libpcap-dev libsodium-dev libboost-filesystem1.74-dev
 apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl
 apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev 
-apt -y remove  python3-libcamera
+apt -y remove  python3-libcamera libcamera0
 apt -y -o Dpkg::Options::="--force-overwrite" --no-install-recommends install libcamera-openhd 
 apt -y install libspdlog-dev
 
