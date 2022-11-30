536606b37f80e5bfdb65876fb5b7cf0402658a12
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-30 20:07:53 +0100

    force our libcamera install

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -8,7 +8,7 @@ apt -y install build-essential autotools-dev automake libtool autoconf libpcap-d
 apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl
 apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev 
 apt -y remove  python3-libcamera
-apt -y install libcamera-openhd 
+apt -y -o Dpkg::Options::="--force-overwrite" --no-install-recommends install libcamera-openhd 
 apt -y install libspdlog-dev
 
 # Needed for RC, optional
