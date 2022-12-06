2c2f54bf7731fc4d2672b943006d88b4bf31af1b
consti10 <geierconstantinabc@gmail.com>
2022-12-06 19:24:54 +0100

    dirty, test going from UDP to raw data callbacks

--- OpenHD/ohd_video/src/gstreamerstream.cpp
+++ OpenHD/ohd_video/src/gstreamerstream.cpp
@@ -472,7 +472,7 @@ void GStreamerStream::test_add_data_listener() {
 }
 
 void GStreamerStream::on_new_rtp_fragmented_frame(std::vector<std::shared_ptr<std::vector<uint8_t>>> frame_fragments) {
-  //m_console->debug("Got frame with {} fragments",frame_fragments.size());
+  m_console->debug("Got frame with {} fragments",frame_fragments.size());
   if(m_transmit_interface){
     m_transmit_interface->transmit_video_data(0,openhd::FragmentedVideoFrame{frame_fragments});
   }else{
