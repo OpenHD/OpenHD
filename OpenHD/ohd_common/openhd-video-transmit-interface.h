07207eed06fefbabb11875ba22444dce24f6dc0a
consti10 <geierconstantinabc@gmail.com>
2022-12-06 17:48:58 +0100

    dirty, test going from UDP to raw data callbacks

--- OpenHD/ohd_common/openhd-video-transmit-interface.h
+++ OpenHD/ohd_common/openhd-video-transmit-interface.h
@@ -11,8 +11,12 @@ namespace openhd{
 
 class ITransmitVideo{
  public:
-  // Video, unidirectional
-  // only valid on air (transmit)
+  /**
+   * Video, unidirectional
+   * only valid on air (transmit)
+   * @param stream_index 0 -> primary video stream, 1 -> secondary video stream
+   * @param fragmented_video_frame the "frame" to transmit
+   */
   virtual void transmit_video_data(int stream_index,const FragmentedVideoFrame& fragmented_video_frame)=0;
 };
 
--- OpenHD/ohd_video/src/gstreamerstream.cpp
+++ OpenHD/ohd_video/src/gstreamerstream.cpp
@@ -463,6 +463,8 @@ void GStreamerStream::on_new_rtp_fragmented_frame(std::vector<std::shared_ptr<st
   m_console->debug("Got frame with {} fragments",frame_fragments.size());
   if(m_transmit_interface){
     m_transmit_interface->transmit_video_data(0,openhd::FragmentedVideoFrame{frame_fragments});
+  }else{
+    m_console->debug("No transmit interface");
   }
 }
 
