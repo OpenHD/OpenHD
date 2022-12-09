f12b6df1afc804675c5880197dfdb9b356a60e75
consti10 <geierconstantinabc@gmail.com>
2022-12-09 22:25:55 +0100

    add the n of rx cards as a read-only param

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -514,7 +514,8 @@ std::vector<openhd::Setting> WBLink::get_all_settings(){
     ret.push_back(Setting{WB_MAX_FEC_BLOCK_SIZE_FOR_PLATFORM,openhd::IntSetting{(int)m_settings->get_settings().wb_max_fec_block_size_for_platform, cb_wb_max_fec_block_size_for_platform}});
   }
   if(m_profile.is_air){
-    // We display the total n of detected RX cards such that users can validate their multi rx setup(s)
+    // We display the total n of detected RX cards such that users can validate their multi rx setup(s) if there is more than one rx card detected
+    // (Note: air always has exactly one monitor mode wi-fi card)
     const int n_rx_cards=static_cast<int>(m_broadcast_cards.size());
     if(n_rx_cards>1){
       auto cb_read_only=[this](std::string,int value){
