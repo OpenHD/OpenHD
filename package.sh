7ef3ca46b803a37efa98e43c246c05aae7fbf213
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-12-09 14:59:46 +0100

    edit package version

--- package.sh
+++ package.sh
@@ -60,7 +60,7 @@ echo "copied files"
 echo ${PKGDIR}
 
 
-VERSION="2.2.2-evo-$(date '+%m%d%H%M')-${VER2}"
+VERSION="2.2.4-evo-$(date '+%m%d%H%M')-${VER2}"
 echo ${VERSION}
 
 rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
