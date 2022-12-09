d7ff0e419039c6986073660a17506f7b67c2d1ec
consti10 <geierconstantinabc@gmail.com>
2022-12-09 21:30:37 +0100

    and now we can also get rid of the video rx

--- OpenHD/ohd_interface/inc/ground_video_forwarder.h
+++ OpenHD/ohd_interface/inc/ground_video_forwarder.h
@@ -7,13 +7,16 @@
 
 #include "../../lib/wifibroadcast/src/UdpWBReceiver.hpp"
 
+// Video data received from wifibroadcast is made available via UDP on the ground unit.
+// Localhost 5600 is always on by default, other client(s) can be added / removed dynamically.
+// TODO: R.n it is not needed, but with even more clients we might have to use multithreading here.
 class GroundVideoForwarder{
  public:
   GroundVideoForwarder();
   // Start forwarding to another ip::port
   void addForwarder(std::string client_addr, int client_udp_port);
-  // safe to call if not already forwarding to ip::port
   // stop forwarding to ip::port
+  // safe to call if not already forwarding to ip::port
   void removeForwarder(std::string client_addr, int client_udp_port);
   // called by the wb receiver
   void forward_data(const uint8_t* data, int data_len);
