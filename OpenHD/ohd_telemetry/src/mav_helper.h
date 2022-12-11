fc0df05da4dcea34c45e4eaf0343ddc84221d0bf
consti10 <geierconstantinabc@gmail.com>
2022-12-11 07:18:14 +0100

    optimization - do not send messages with target sys id == ground unit to the air unit or FC

--- OpenHD/ohd_telemetry/src/mav_helper.h
+++ OpenHD/ohd_telemetry/src/mav_helper.h
@@ -10,6 +10,8 @@
 #include <sstream>
 #include <iostream>
 
+#include "openhd-spdlog.hpp"
+
 namespace MExampleMessage {
 // mostly from https://github.com/mavlink/mavlink/blob/master/examples/linux/mavlink_udp.c
 static uint64_t microsSinceEpoch() {
@@ -200,6 +202,7 @@ static std::tuple<std::vector<MavlinkMessage>,std::vector<MavlinkMessage>> split
   for(const auto& msg:messages){
     const auto target= get_target_from_message_if_available(msg.m);
     if(target.has_target() && target.sys_id==local_target_sys_id){
+      openhd::log::get_default()->debug("local only message");
       local_only.push_back(msg);
     }else{
       generic.push_back(msg);
