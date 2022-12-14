25d083affbe8c9dbf9a00942b20a6a494ba2d2f5
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-12-14 18:50:15 +0100

    simple debug

--- OpenHD/ohd_common/openhd-rpi-os-configure-vendor-cam.hpp
+++ OpenHD/ohd_common/openhd-rpi-os-configure-vendor-cam.hpp
@@ -118,7 +118,7 @@ static std::string get_file_name_for_cam_config(const OHDPlatform& platform,cons
   const bool is_rpi4=platform.board_type==BoardType::RaspberryPi4B || platform.board_type==BoardType::RaspberryPiCM4;
   std::string base_filename="/boot/openhd/configs/";
   if(cam_config==CamConfig::MMAL){
-    return base_filename+"rpi_raspicam.txt";
+    return base_filename+"rpi_raspicam.txt"+cam_config;
   }else if(cam_config==CamConfig::LIBCAMERA){
     if(is_rpi4){
       return base_filename+"rpi_4_libcamera.txt";
