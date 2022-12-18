3242280df53c03c715b393a1e0fed2e157d874ae
consti10 <geierconstantinabc@gmail.com>
2022-12-18 12:44:20 +0100

    dcument

--- OpenHD/ohd_telemetry/src/GroundTelemetry.cpp
+++ OpenHD/ohd_telemetry/src/GroundTelemetry.cpp
@@ -181,12 +181,10 @@ void GroundTelemetry::settings_generic_ready() {
 }
 
 void GroundTelemetry::add_external_ground_station_ip(const std::string& ip_openhd,const std::string& ip_dest_device) {
-  std::stringstream ss;
-  ss<<"GroundTelemetry::add_external_ground_station_ip:ip_openhd:["<<ip_openhd<<",ip_dest_device:"<<ip_dest_device<<"]\n";
-  m_console->debug(ss.str());
+  m_console->debug("GroundTelemetry::add_external_ground_station_ip: {} {}",ip_openhd,ip_dest_device);
   if(!OHDUtil::is_valid_ip(ip_openhd) || !OHDUtil::is_valid_ip(ip_dest_device)){
-	m_console->warn("These are no valid IPs, skipping");
-	return;
+    m_console->warn("These are no valid IPs, skipping");
+    return;
   }
   std::lock_guard<std::mutex> guard(other_udp_ground_stations_lock);
   assert(OHDUtil::is_valid_ip(ip_openhd));
