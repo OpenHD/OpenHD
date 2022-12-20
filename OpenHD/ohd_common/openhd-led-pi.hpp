c9901dcc0e16c4f409bcddd020c6a9a9c65e5492
consti10 <geierconstantinabc@gmail.com>
2022-12-20 19:01:50 +0100

    improve performance of led blink

--- OpenHD/ohd_common/openhd-led-pi.hpp
+++ OpenHD/ohd_common/openhd-led-pi.hpp
@@ -18,23 +18,23 @@
 namespace openhd::rpi{
 // so far, I have only tested this on the RPI 4 and CM4
 static void toggle_red_led(const bool on,const bool debug=true){
-  int ret;
-  if(!OHDFilesystemUtil::exists("/sys/class/leds/led1/brightness")){
+  static constexpr auto filename="/sys/class/leds/led0/brightness";
+  if(!OHDFilesystemUtil::exists(filename)){
     openhd::log::get_default()->debug("RPI LED1 brightness does not exist\n");
     return;
   }
   const auto content=on ? "1":"0";
-  OHDFilesystemUtil::write_file("/sys/class/leds/led1/brightness",content);
+  OHDFilesystemUtil::write_file(filename,content);
 }
 // I think the green led only supports on/off on the 4th generation pis
 static void toggle_green_led(const bool on,const bool debug=true){
-  int ret;
-  if(!OHDFilesystemUtil::exists("/sys/class/leds/led0/brightness")){
+  static constexpr auto filename="/sys/class/leds/led1/brightness";
+  if(!OHDFilesystemUtil::exists(filename)){
     openhd::log::get_default()->debug("RPI LED0 brightness does not exist");
     return;
   }
   const auto content=on ? "1":"0";
-  OHDFilesystemUtil::write_file("/sys/class/leds/led0/brightness",content);
+  OHDFilesystemUtil::write_file(filename,content);
 }
 // toggle red led off, wait for delay, then toggle it on,wait for delay
 static void red_led_on_off_delayed(const std::chrono::milliseconds &delay1,const std::chrono::milliseconds &delay2,const bool debug=true) {
