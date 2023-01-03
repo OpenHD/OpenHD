12b467c2aaefa612575864f346620dbf09b7b027
consti10 <geierconstantinabc@gmail.com>
2023-01-04 00:51:25 +0100

    work on improved forwarding

--- OpenHD/ohd_interface/inc/wb_link.h
+++ OpenHD/ohd_interface/inc/wb_link.h
@@ -17,7 +17,6 @@
 #include "openhd-profile.hpp"
 #include "openhd-spdlog.hpp"
 #include "openhd-telemetry-tx-rx.h"
-#include "video_forwarder.h"
 #include "wb_link_settings.hpp"
 #include "wifi_card.hpp"
 
