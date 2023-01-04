e7cb35eac32a2316684f8670b8ec75f214752146
consti10 <geierconstantinabc@gmail.com>
2023-01-04 22:14:18 +0100

    work on improved forwarding

--- OpenHD/ohd_interface/inc/ethernet_hotspot.h
+++ OpenHD/ohd_interface/inc/ethernet_hotspot.h
@@ -5,13 +5,19 @@
 #ifndef OPENHD_OPENHD_OHD_INTERFACE_INC_ETHERNET_HOTSPOT_H_
 #define OPENHD_OPENHD_OHD_INTERFACE_INC_ETHERNET_HOTSPOT_H_
 
+#include <ISettingsComponent.hpp>
 #include <string>
+
 #include "ethernet_hotspot_settings.h"
 
 class EthernetHotspot{
  public:
   explicit EthernetHotspot(std::string  device);
+  std::vector<openhd::Setting> get_all_settings();
  private:
+  void start_async();
+  void stop_async();
+  std::shared_ptr<spdlog::logger> m_console;
   const std::string m_device;
   std::unique_ptr<EthernetHotspotSettingsHolder> m_settings;
 };
--- OpenHD/ohd_interface/src/ethernet_hotspot.cpp
+++ OpenHD/ohd_interface/src/ethernet_hotspot.cpp
@@ -22,5 +22,38 @@ static void create_ethernet_hotspot_connection(const std::string& eth_device_nam
 }
 
 EthernetHotspot::EthernetHotspot(std::string  device):m_device(std::move(device)) {
+  m_console = openhd::log::create_or_get("wifi_hs");
+  m_console->debug("begin create hotspot connection");
+  create_ethernet_hotspot_connection(m_device);
+  m_console->debug("end create hotspot connection");
+  if(m_settings->get_settings().enable){
+    start_async();
+  }
+}
 
+void EthernetHotspot::start_async() {
+
+}
+
+void EthernetHotspot::stop_async() {
+
+}
+
+std::vector<openhd::Setting> EthernetHotspot::get_all_settings() {
+  using namespace openhd;
+  std::vector<openhd::Setting> ret{};
+  const auto settings=m_settings->get_settings();
+  auto cb_enable=[this](std::string,int value){
+    if(!validate_yes_or_no(value))return false;
+    m_settings->unsafe_get_settings().enable=value;
+    m_settings->persist();
+    if(m_settings->get_settings().enable){
+      start_async();
+    }else{
+      stop_async();
+    }
+    return true;
+  };
+  ret.push_back(openhd::Setting{"I_ETH_HOTSPOT_E",openhd::IntSetting{settings.enable,cb_enable}});
+  return ret;
 }
