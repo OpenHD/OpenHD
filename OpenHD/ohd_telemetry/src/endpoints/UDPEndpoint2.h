55e66e4afa4eb3b438dfb641f871d2d674ec78ff
consti10 <geierconstantinabc@gmail.com>
2023-01-11 02:00:43 +0100

    reduce time we need to hold the mutex

--- OpenHD/ohd_telemetry/src/endpoints/UDPEndpoint2.h
+++ OpenHD/ohd_telemetry/src/endpoints/UDPEndpoint2.h
@@ -39,6 +39,7 @@ class UDPEndpoint2 : public MEndpoint {
   //
   std::mutex m_sender_mutex;
   std::map<std::string,void*> m_other_dest_ips{};
+  std::vector<std::string> get_all_curr_dest_ips();
 };
 
 #endif //OPENHD_OPENHD_OHD_TELEMETRY_SRC_ENDPOINTS_UDPENDPOINT2_H_
