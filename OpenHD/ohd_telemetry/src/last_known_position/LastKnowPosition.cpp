4cbdd6bd1d1d61d0a7773d3883b0eb2cc222540c
consti10 <geierconstantinabc@gmail.com>
2023-01-23 01:02:06 +0100

    add dummy for last known position

---
+++ OpenHD/ohd_telemetry/src/last_known_position/LastKnowPosition.cpp
@@ -0,0 +1,5 @@
+//
+// Created by consti10 on 23.01.23.
+//
+
+#include "LastKnowPosition.h"
---
+++ OpenHD/ohd_telemetry/src/last_known_position/LastKnowPosition.h
@@ -0,0 +1,18 @@
+//
+// Created by consti10 on 23.01.23.
+//
+
+#ifndef OPENHD_OPENHD_OHD_TELEMETRY_SRC_LAST_KNOWN_POSITION_LASTKNOWPOSITION_H_
+#define OPENHD_OPENHD_OHD_TELEMETRY_SRC_LAST_KNOWN_POSITION_LASTKNOWPOSITION_H_
+
+/**
+ * This class exposes the following simple functionality:
+ * Constantly listen for updates from the FC in regards to its position
+ * (aka MAVLINK_MSG_ID_GLOBAL_POSITION_INT messages) and write the coordinates to a file
+ * overwriting the previous position. This way, if the drone crashes, the user can always
+ * get the last known position from a file in openhd.
+ * TODO
+ */
+class LastKnowPosition {};
+
+#endif  // OPENHD_OPENHD_OHD_TELEMETRY_SRC_LAST_KNOWN_POSITION_LASTKNOWPOSITION_H_
