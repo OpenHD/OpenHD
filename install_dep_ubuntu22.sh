6cf6bc5de3eb5700281b3763f8af4f4c855c1e20
consti10 <geierconstantinabc@gmail.com>
2023-01-25 03:24:50 +0100

    improve ubuntu 20 and ubuntu 22 build test ci's

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -9,12 +9,13 @@ sudo apt -y install cmake build-essential autotools-dev automake libtool autocon
             libboost-dev libboost-filesystem-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
-            gstreamer1.0-plugins-good \
             libavcodec-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libspdlog-dev || exit 1
 
-sudo apt -y install gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
+# Gstreamer is separate from the "rest".
+# Note that there is no compile time validation for all the elements required by gstreamer
+sudo apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly || exit 1
 
 # Needed for RC, optional
 sudo apt -y install libsdl2-dev
