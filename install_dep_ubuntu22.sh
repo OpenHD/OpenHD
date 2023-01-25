2cee3a306b4b0fb45b103d13d7ed51d00d7ab12d
consti10 <geierconstantinabc@gmail.com>
2023-01-25 03:37:04 +0100

    improve ubuntu 20 and ubuntu 22 build test ci's

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 # Install all the dependencies needed to build OpenHD from source.
-# This is for the simple_build_test.yml github CI or when setting up a development environment
-# PLEASE KEEP THIS FILE AS CLEAN AS POSSIBLE, Ubuntu20 is the baseline - for other platforms / OS versions, create their own files
+# This is for the xxx_build_test.yml github CI or when setting up a development environment
+# PLEASE KEEP THIS FILE AS CLEAN AS POSSIBLE, Ubuntu22 is the baseline - for other platforms / OS versions, create their own files
 
 sudo apt -y install cmake build-essential autotools-dev automake libtool autoconf \
             libpcap-dev libsodium-dev \
             libboost-dev libboost-filesystem-dev \
             libusb-1.0-0-dev \
             libv4l-dev \
-            libavcodec-dev \
             libnl-3-dev libnl-genl-3-dev libnl-route-3-dev \
             libspdlog-dev || exit 1
 
