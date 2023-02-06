7756954e475357898dc8585771e2e2546fb2649e
consti10 <geierconstantinabc@gmail.com>
2023-02-06 14:12:30 +0100

    better CMake - with if(TARGET) we can let ohd_interface, ohd_telemetry and ohd_video depend on ohd_common without pollution the main openhd build

--- OpenHD/build_submodules_independently.sh
+++ OpenHD/build_submodules_independently.sh
@@ -1,7 +1,8 @@
 # bin/bash
 
 # For the github CIs
-# Here we build all the submodules independently from each other
+# Here we build all the submodules independently from each other to make sure
+# no unwanted dependencies were introduced during OpenHD development
 
 cd ohd_interface
 cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug
