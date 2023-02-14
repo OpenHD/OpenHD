5eda36e8f09527640cee01bbbdc29179f7110647
consti10 <geierconstantinabc@gmail.com>
2023-02-14 05:59:22 +0100

    only log warning when receive is set to true

--- OpenHD/ohd_telemetry/src/endpoints/SerialEndpoint.cpp
+++ OpenHD/ohd_telemetry/src/endpoints/SerialEndpoint.cpp
@@ -264,7 +264,7 @@ void SerialEndpoint::receive_data_until_error() {
       // but on a FC which constantly provides a data stream it most likely is an error.
       m_n_failed_reads++;
       const auto elapsed_since_last_log=std::chrono::steady_clock::now()-m_last_log_serial_read_failed;
-      if(elapsed_since_last_log>=MIN_DELAY_BETWEEN_SERIAL_READ_FAILED_LOG_MESSAGES){
+      if(elapsed_since_last_log>=MIN_DELAY_BETWEEN_SERIAL_READ_FAILED_LOG_MESSAGES && m_options.enable_reading){
         m_last_log_serial_read_failed=std::chrono::steady_clock::now();
         m_console->warn("{} failed polls(reads)",m_n_failed_reads);
       }else{
