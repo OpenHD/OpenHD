820d1a8cffc91856eb40165ff0f5ccffe2b2f94e
consti10 <geierconstantinabc@gmail.com>
2023-02-16 00:56:01 +0100

    improve rate control

--- OpenHD/ohd_video/inc/ohd_video_air_generic_settings.hpp
+++ OpenHD/ohd_video/inc/ohd_video_air_generic_settings.hpp
@@ -18,9 +18,16 @@ struct AirCameraGenericSettings {
   // On startup, we wait for up to X seconds until this many camera(s) have been discovered
   // and create one or more dummy camera(s) if they are not found
   //int n_cameras_to_wait_for=1;
+  // the link recommends a total video bitrate to us - in case of dual camera, we need to split that up into
+  // bitrate for primary and secondary video
+  int dualcam_primary_video_allocated_bandwidth=60; // Default 70%:30% split
 };
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(AirCameraGenericSettings,switch_primary_and_secondary);
+
+static bool is_valid_dualcam_primary_video_allocated_bandwidth(int primary_video_allocated_bandwidth){
+  return primary_video_allocated_bandwidth>=10 && primary_video_allocated_bandwidth<=90;
+}
 
 class AirCameraGenericSettingsHolder: public openhd::settings::PersistentSettings<AirCameraGenericSettings>{
  public:
