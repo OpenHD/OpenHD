1d6fe62606cdff03d280a22e363fa39a49f46278
consti10 <geierconstantinabc@gmail.com>
2023-02-15 13:38:00 +0100

    clang tidy

--- OpenHD/ohd_common/inc/openhd_external_device.hpp
+++ OpenHD/ohd_common/inc/openhd_external_device.hpp
@@ -14,7 +14,7 @@
 
 namespace openhd {
 
-// This is how we handle "external" devices in regards to forwarding.
+// This is how we handle "external" devices in regard to forwarding.
 // 1) What are external device(s): A smartphone, tablet or similar connected via (WIFI,USB Tethering) to
 //    the ground unit
 // 2) How are these device(s) handled: Once detected (e.g. when a USB tethering connection is detected), find their IP adress,
