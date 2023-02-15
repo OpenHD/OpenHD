15491b67a4c120593beb539265c3cb024c354d11
consti10 <geierconstantinabc@gmail.com>
2023-02-15 05:40:52 +0100

    document

--- OpenHD/ohd_interface/inc/wb_link_helper.h
+++ OpenHD/ohd_interface/inc/wb_link_helper.h
@@ -14,29 +14,38 @@
  */
 namespace openhd::wb{
 
-// return true if the "disable all frequency checks" file exists
+/**
+ * @return true if the "disable all frequency checks" file exists
+ */
 bool disable_all_frequency_checks();
 
-// returns true if all cards support setting an MCS index
-// false otherwise
+/**
+ * @param m_broadcast_cards the cards to check capabilities from
+ * @return true if all cards support setting an MCS index,
+ */
 bool cards_support_setting_mcs_index(const std::vector<WiFiCard>& m_broadcast_cards);
 
-// returns true if all cards support setting the channel width (otherwise 20Mhz default is fixed (most likely))
-// false otherwise
+/**
+ * @param m_broadcast_cards the cards to check capabilities from
+ * @return  true if all cards support setting the channel width (otherwise 20Mhz default is fixed (most likely))
+ */
 bool cards_support_setting_channel_width(const std::vector<WiFiCard>& m_broadcast_cards);
 
-// returns true if the given card supports the given frequency, taking into account if the kernel was modified or not
+/**
+ * returns true if the given cards supports the given frequency, taking into account if the kernel was modified or not
+ */
 bool cards_support_frequency(
     uint32_t frequency,
     const std::vector<WiFiCard>& m_broadcast_cards,
     const OHDPlatform& platform,
     const std::shared_ptr<spdlog::logger>& m_console);
 
-// fixup any settings coming from a previous use with a different wifi card (e.g. if user swaps around cards)
+/**
+ * fixup any settings coming from a previous use with a different wifi card (e.g. if user swaps around cards)
+ */
 void fixup_unsupported_settings(openhd::WBStreamsSettingsHolder& settings,
                                 const std::vector<WiFiCard>& m_broadcast_cards,
                                 std::shared_ptr<spdlog::logger> m_console);
-
 
 bool set_frequency_and_channel_width_for_all_cards(uint32_t frequency,uint32_t channel_width,const std::vector<WiFiCard>& m_broadcast_cards);
 
