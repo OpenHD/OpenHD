2541665296f115d68c0395f714eecf2a1f686564
consti10 <geierconstantinabc@gmail.com>
2023-02-16 01:17:01 +0100

    improve rate control

--- OpenHD/ohd_video/src/gstreamerstream.cpp
+++ OpenHD/ohd_video/src/gstreamerstream.cpp
@@ -413,7 +413,8 @@ void GStreamerStream::restart_async() {
 void GStreamerStream::handle_change_bitrate_request(openhd::ActionHandler::LinkBitrateInformation lb) {
   std::lock_guard<std::mutex> guard(m_pipeline_mutex);
   m_console->debug("handle_change_bitrate_request {}kBit/s",lb.recommended_encoder_bitrate_kbits);
-  const auto max_bitrate_kbits=m_camera_holder->get_settings().h26x_bitrate_kbits;
+  //const auto max_bitrate_kbits=m_camera_holder->get_settings().h26x_bitrate_kbits;
+  const auto max_bitrate_kbits=19*1000;
   auto recommended_encoder_bitrate_kbits=lb.recommended_encoder_bitrate_kbits;
   if(m_camera_holder->requires_half_bitrate_workaround()){
     m_console->debug("applying hack - reduce bitrate by 2 to get actual correct bitrate");
