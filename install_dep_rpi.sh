98e1feb9dc99ec98c8d6ee4b977cea17a2702b31
consti10 <geierconstantinabc@gmail.com>
2023-02-22 21:14:50 +0100

    not needed anymore

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -8,8 +8,7 @@ apt -y install build-essential autotools-dev automake libtool autoconf libpcap-d
 apt -y install libasio-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libv4l-dev git apt-transport-https curl
 apt -y install ruby-dev python3-pip libnl-3-dev cmake snapd libnl-genl-3-dev libnl-route-3-dev 
 apt -y remove  python3-libcamera libcamera0
-apt -y -o Dpkg::Options::="--force-overwrite" --no-install-recommends install libcamera-openhd 
-apt -y install libspdlog-dev
+apt -y -o Dpkg::Options::="--force-overwrite" --no-install-recommends install libcamera-openhd
 
 # Needed for RC, optional
 apt -y install libsdl2-dev
