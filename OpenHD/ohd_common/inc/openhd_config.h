800201564c7dbb021c69c45ac2610c3a8acbaac6
consti10 <geierconstantinabc@gmail.com>
2023-02-23 10:45:09 +0100

    remove json dependency from wifi_card .h

--- OpenHD/ohd_common/inc/openhd_config.h
+++ OpenHD/ohd_common/inc/openhd_config.h
@@ -10,6 +10,8 @@
 
 namespace openhd{
 
+static constexpr auto RPI_ETHERNET_ONLY="RPI_ETHERNET_ONLY";
+
 // NOTE: Read the .config - file itself for documentation what these variables do
 struct Config{
   // WIFI
@@ -22,6 +24,7 @@ struct Config{
   std::string CAMERA_CAMERA0_TYPE;
   std::string CAMERA_CAMERA1_TYPE;
   // NETWORKING
+  std::string NW_ETHERNET_CARD=RPI_ETHERNET_ONLY;
   std::vector<std::string> NW_MANUAL_FORWARDING_IPS;
 };
 
@@ -29,6 +32,9 @@ Config load_config();
 
 void debug_config(const Config& config);
 void debug_config();
+
+// Control ethernet via mavlink & Network manager - only on on rpi by default, otherwise card needs to be manually specified
+bool nw_ethernet_card_manual_active(const Config& config);
 
 }
 
