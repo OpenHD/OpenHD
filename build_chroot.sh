f542f751c91892154329b70d2e9bb812a342d4ce
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 22:35:53 +0100

    dumbtest

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./install_dep_ubuntu22.sh
-sudo ./package.sh arm64 debian bullseye
+#./install_dep_ubuntu22.sh
+#sudo ./package.sh arm64 debian bullseye
 echo "here is a debug step"
 ls -a
 sudo apt install tree
