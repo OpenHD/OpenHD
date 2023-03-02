d78472debdda6510626a03033d87f327da94879c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 23:54:27 +0100

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -6,13 +6,13 @@ sudo pip3 install --upgrade cloudsmith-cli
 
 echo "here is a debug step"
 ls -a
-FILENAME="example.deb" && SIZE="1m" && touch $FILENAME && touch -m $FILENAME && dd if=/dev/urandom of=$FILENAME bs=$SIZE count=1
+FILENAME="example.deb" && SIZE="1M" && touch $FILENAME && touch -m $FILENAME && dd if=/dev/urandom of=$FILENAME bs=$SIZE count=1
 mkdir -p /opt/out/ || exit 1
 cp -v *.dep /opt/out/
 echo "copied deb file"
 echo "push to cloudsmith"
 git describe --exact-match HEAD >/dev/null 2>&1
 echo "Pushing package to OpenHD 2.3 repository"
-cloudsmith push deb openhd/openhd-2-3-evo/debian/bullseye openhd_2.3-Evo-Rock_arm64.deb
+cloudsmith push deb openhd/openhd-2-3-evo/debian/bullseye example.deb
 
 
