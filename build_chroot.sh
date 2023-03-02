1f0c64379f0b7568b9d2288280291a0baa8611a7
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 23:40:39 +0100

    debug

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+sudo apt install -y python3-pip
+sudo pip3 install --upgrade cloudsmith-cli
 #./install_dep_ubuntu22.sh
 #sudo ./package.sh arm64 debian bullseye
+
 echo "here is a debug step"
 ls -a
+FILENAME="example.deb" && SIZE="1m" && touch $FILENAME && touch -m $FILENAME && dd if=/dev/urandom of=$FILENAME bs=$SIZE count=1
 mkdir -p /opt/out/ || exit 1
 cp -v *.dep /opt/out/
 echo "copied deb file"
-zip file.zip * 
-cp -v *.zip /opt/out/
-echo "copied zip file"
-echo "end"
+echo "push to cloudsmith"
+git describe --exact-match HEAD >/dev/null 2>&1
+echo "Pushing package to OpenHD 2.3 repository"
+cloudsmith push deb openhd/openhd-2-3-evo/debian/bullseye openhd_2.3-Evo-Rock_arm64.deb
+
+
