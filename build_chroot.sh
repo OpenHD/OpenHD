d069c5375f9dc3335b56df77c88a17248d3a3b62
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 22:24:34 +0100

    more debugging

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ./install_dep_ubuntu22.sh
 sudo ./package.sh arm64 debian bullseye
-echo"here is a debug step"
+echo "here is a debug step"
 ls -a
 sudo apt install tree
 tree
+mkdir -p /opt/out
 cp *.dep /opt/out/
+zip file.zip * 
+cp *.zip /opt/out
 echo "end"
