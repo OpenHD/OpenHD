472924dde307c83284437ec069c9e2ef4ef4345c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-03 15:43:45 +0100

    first rock5 build

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
-#./install_dep_ubuntu22.sh
-#sudo ./package.sh arm64 debian bullseye
+./install_dep_ubuntu22.sh
+sudo ./package.sh arm64 debian bullseye
 
 echo "here is a debug step"
 ls -a
-FILENAME="example.deb" && SIZE="1M" && touch $FILENAME && touch -m $FILENAME && dd if=/dev/urandom of=$FILENAME bs=$SIZE count=1
 mkdir -p /opt/out/ || exit 1
 cp -v *.dep /opt/out/
 echo "copied deb file"
