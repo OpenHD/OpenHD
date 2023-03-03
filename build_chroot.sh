d9e7888fcb5e7d15963cafa2b496e712fa14e6fc
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-03 16:42:38 +0100

    dirty test

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #This file is the install instruction for the CHROOT build
 #We're using cloudsmith-cli to upload the file in CHROOT
+
 sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
 ./install_dep_ubuntu22.sh
@@ -13,5 +14,5 @@ git describe --exact-match HEAD >/dev/null 2>&1
 echo "Pushing the package to OpenHD 2.3 repository"
 ls -a
 API_KEY=$(cat cloudsmith_api_key.txt)
-cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/debian/bullseye example.deb || exit 1
+cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/debian/bullseye *.deb || exit 1
 
