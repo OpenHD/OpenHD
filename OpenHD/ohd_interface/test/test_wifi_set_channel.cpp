0ea06dbfea44aba17a7017d11216be2fcd318db5
consti10 <geierconstantinabc@gmail.com>
2023-04-01 19:01:39 +0200

    add new method to set channel via ioctl

---
+++ OpenHD/ohd_interface/test/test_wifi_set_channel.cpp
@@ -0,0 +1,64 @@
+//
+// Created by consti10 on 28.03.23.
+//
+
+#include "openhd_util.h"
+#include "wifi_command_helper.h"
+//#include "wifi_command_helper2.h"
+#include "wifi_card_discovery.h"
+
+#include <vector>
+#include <utility>
+
+//
+#include <linux/wireless.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <csignal>
+
+
+static bool test_set_wifi_channel(const std::string& device_name,int channel_number){
+  auto console=openhd::log::get_default();
+  console->debug("test_set_wifi_channel {} {}",device_name,channel_number);
+  int sockfd;
+  struct iwreq wrq{};
+
+  // Open socket
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    openhd::log::get_default()->warn("test_set_wifi_channel - cannot create socket");
+    return false;
+  }
+
+  // Set interface name
+  strncpy(wrq.ifr_name, device_name.c_str(), IFNAMSIZ);
+
+  // Set channel
+  wrq.u.freq.m = channel_number;
+  wrq.u.freq.e=0;
+  wrq.u.freq.flags = IW_FREQ_FIXED;
+
+  // Send IOCTL to set channel
+  if (ioctl(sockfd, SIOCSIWFREQ, &wrq) < 0) {
+    openhd::log::get_default()->warn("test_set_wifi_channel - ioctl");
+    perror("ioctl");
+    close(sockfd);
+    return false;
+  }
+  // Close socket
+  close(sockfd);
+
+  openhd::log::get_default()->debug("Channel set to {}",channel_number);
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  OHDUtil::terminate_if_not_root();
+
+  // Hard coded for testing
+  const std::string card_name="wlx244bfeb71c05";
+
+  test_set_wifi_channel(card_name,177);
+  return 0;
+}
