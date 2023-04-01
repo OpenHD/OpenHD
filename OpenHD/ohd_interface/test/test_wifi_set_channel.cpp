0a3eeedc2057318aa765d5e31dbacd17ae63a72d
consti10 <geierconstantinabc@gmail.com>
2023-04-01 20:23:17 +0200

    add new method to set channel via ioctl

--- OpenHD/ohd_interface/test/test_wifi_set_channel.cpp
+++ OpenHD/ohd_interface/test/test_wifi_set_channel.cpp
@@ -56,13 +56,15 @@ static bool test_set_wifi_channel(const std::string& device_name,int channel_num
 int main(int argc, char *argv[]) {
   OHDUtil::terminate_if_not_root();
 
-  if(argc<3){
-    openhd::log::get_default()->debug("Usage [wifi card] [channel number]");
-    return -1;
-  }
-  const std::string card_name = argv[1];
-  const int channel_num = atoi(argv[2]);
+  std::string card_name="wlx244bfeb71c05";
+  int channel_num=177;
 
+  if(argc<3){
+    openhd::log::get_default()->warn("Usage [wifi card] [channel number], using defaults");
+  }else{
+    card_name = argv[1];
+    channel_num = atoi(argv[2]);
+  }
   test_set_wifi_channel(card_name,177);
   return 0;
 }
