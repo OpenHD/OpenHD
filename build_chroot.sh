915b12146b6c88a7bc938548bb94ec4c8c1ce516
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-04-04 00:52:08 +0200

    provide also ubuntu packages fpr arm64/rock

--- build_chroot.sh
+++ build_chroot.sh
@@ -5,7 +5,7 @@
 sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
 ./install_dep_ubuntu22.sh
-sudo ./package.sh arm64 debian bullseye || exit 1
+sudo ./package.sh arm64 ubuntu jammy || exit 1
 mkdir -p /opt/out/
 cp -v *.dep /opt/out/
 echo "copied deb file"
@@ -14,5 +14,5 @@ git describe --exact-match HEAD >/dev/null 2>&1
 echo "Pushing the package to OpenHD 2.3 repository"
 ls -a
 API_KEY=$(cat cloudsmith_api_key.txt)
-cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/debian/bullseye *.deb || exit 1
+cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/ubuntu/jammy *.deb || exit 1
 
