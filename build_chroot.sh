1fc46c66eb1d7f8515230d75229a548a2990a439
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-04-28 04:30:30 +0200

    enhance chroot

--- build_chroot.sh
+++ build_chroot.sh
@@ -2,10 +2,13 @@
 #This file is the install instruction for the CHROOT build
 #We're using cloudsmith-cli to upload the file in CHROOT
 
+DISTRO=$1
+FLAVOR=$2
+
 sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
 ./install_build_dep.sh rock5
-sudo ./package.sh arm64 ubuntu jammy || exit 1
+sudo ./package.sh arm64 ${DISTRO} ${FLAVOR} || exit 1
 mkdir -p /opt/out/
 cp -v *.dep /opt/out/
 echo "copied deb file"
@@ -14,5 +17,5 @@ git describe --exact-match HEAD >/dev/null 2>&1
 echo "Pushing the package to OpenHD 2.3 repository"
 ls -a
 API_KEY=$(cat cloudsmith_api_key.txt)
-cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/ubuntu/jammy *.deb || exit 1
+cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/${DISTRO}/${FLAVOR} *.deb || exit 1
 
