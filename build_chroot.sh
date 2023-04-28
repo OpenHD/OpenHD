bae23e3e47d17053583f2b96c75eb983c22f6fe2
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-04-28 04:39:41 +0200

    make it more complex ;)

--- build_chroot.sh
+++ build_chroot.sh
@@ -2,11 +2,9 @@
 #This file is the install instruction for the CHROOT build
 #We're using cloudsmith-cli to upload the file in CHROOT
 
-DISTRO=$1
-FLAVOR=$2
-
 sudo apt install -y python3-pip
 sudo pip3 install --upgrade cloudsmith-cli
+ls -a
 ./install_build_dep.sh rock5
 sudo ./package.sh arm64 ${DISTRO} ${FLAVOR} || exit 1
 mkdir -p /opt/out/
@@ -17,5 +15,7 @@ git describe --exact-match HEAD >/dev/null 2>&1
 echo "Pushing the package to OpenHD 2.3 repository"
 ls -a
 API_KEY=$(cat cloudsmith_api_key.txt)
+DISTRO=$(cat distro.txt)
+FLAVOR=$(cat flavor.txt)
 cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/${DISTRO}/${FLAVOR} *.deb || exit 1
 
