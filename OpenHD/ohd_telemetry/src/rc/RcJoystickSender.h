43087cb6b0a9d37a9dc24c336572dd41107378e6
consti10 <geierconstantinabc@gmail.com>
2023-05-03 15:01:07 +0200

    Revert "exp allow start and stop at run time"
    This reverts commit 2dfa1e55d45361ce87173dc74c8390cf0141a966.

--- OpenHD/ohd_telemetry/src/rc/RcJoystickSender.h
+++ OpenHD/ohd_telemetry/src/rc/RcJoystickSender.h
@@ -7,9 +7,7 @@
 
 #include "JoystickReader.h"
 #include "../mav_helper.h"
-#include "openhd_spdlog.h"
 #include <memory>
-#include <atomic>
 
 // Really simple, have a thread that send out telemetry RC data at a fixed rate
 // (we cannot just use the thread that fetches data from the joystick, at least not for now)
@@ -19,23 +17,16 @@ class RcJoystickSender {
   // If there is something wrong with the joystick / no joystick connected this cb is not called (such that FC can do failsafe)
   typedef std::function<void(std::array<uint16_t,18> channels)> SEND_MESSAGE_CB;
   RcJoystickSender(SEND_MESSAGE_CB cb,int update_rate_hz,JoystickReader::CHAN_MAP chan_map);
-  ~RcJoystickSender();
-  // Enables sending of rc joystick data IF joystick is connected & alive
-  // If no joystick is connected, this does nothing until a joystick is connected
-  void start();
-  // stops sending joystick data
-  void stop();
   void change_update_rate(int update_rate_hz);
   // update the channel mapping, thread-safe
   void update_channel_maping(const JoystickReader::CHAN_MAP& new_chan_map);
+  ~RcJoystickSender();
  private:
   void send_data_until_terminate();
-  std::shared_ptr<spdlog::logger> m_console;
   std::unique_ptr<JoystickReader> m_joystick_reader;
-  std::unique_ptr<std::thread> m_send_data_thread= nullptr;
+  std::unique_ptr<std::thread> m_send_data_thread;
   const SEND_MESSAGE_CB m_cb;
-  std::atomic<int> m_delay_in_milliseconds;
-  JoystickReader::CHAN_MAP m_chan_map;
+  int m_delay_in_milliseconds;
   bool terminate=false;
 };
 
