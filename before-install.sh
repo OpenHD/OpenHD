891803a5a2d145016758626b7f9d2d3524de20e7
consti10 <geierconstantinabc@gmail.com>
2023-05-30 22:41:36 +0200

    obsolete

--- before-install.sh
+++ before-install.sh
@@ -6,4 +6,3 @@ rm -Rf /boot/openhd/rpi_camera_configs/*
 mv /boot/openhd/scripts/custom_unmanaged_camera.sh /boot/openhd/scripts/custom_unmanaged_camera_old.sh
 # NOTE: Updating overwrites the .config file and also the service file
 rm -rf /boot/openhd/hardware.config
-rm -rf /boot/openhd/enable_ip_camera.sh
