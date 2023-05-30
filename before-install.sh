2103365269344b055b4c7d3c06b4c6812c06682c
consti10 <geierconstantinabc@gmail.com>
2023-05-30 22:40:52 +0200

    fix update not working due to overwriting file (# back it up in case the user has written valuable scripting that would be lost otherwise)

--- before-install.sh
+++ before-install.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /boot/openhd/rpi_camera_configs
 rm -Rf /boot/openhd/rpi_camera_configs/*
+# back it up in case the user has written valuable scripting that would be lost otherwise
+mv /boot/openhd/scripts/custom_unmanaged_camera.sh /boot/openhd/scripts/custom_unmanaged_camera_old.sh
 # NOTE: Updating overwrites the .config file and also the service file
 rm -rf /boot/openhd/hardware.config
 rm -rf /boot/openhd/enable_ip_camera.sh
