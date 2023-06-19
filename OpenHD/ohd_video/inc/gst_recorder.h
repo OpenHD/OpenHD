93cbcc1fcaee200d6c443a28a69c534ea24dff6a
consti10 <geierconstantinabc@gmail.com>
2023-06-19 15:12:01 +0200

    exp gst video recorder

--- OpenHD/ohd_video/inc/gst_recorder.h
+++ OpenHD/ohd_video/inc/gst_recorder.h
@@ -5,12 +5,15 @@
 #ifndef OPENHD_GST_RECORDER_H
 #define OPENHD_GST_RECORDER_H
 
-#include <utility>
 #include <cstdint>
+#include <memory>
+#include <utility>
+#include <vector>
 
 // TODO
 class GstVideoRecorder {
  public:
+  void enqueue_rtp_fragment(std::shared_ptr<std::vector<uint8_t>> fragment);
   void on_video_data(int codec,const uint8_t *data,int data_len);
  private:
   int m_curr_codec=-1;
--- OpenHD/ohd_video/src/gst_recorder.cpp
+++ OpenHD/ohd_video/src/gst_recorder.cpp
@@ -4,6 +4,26 @@
 
 #include "gst_recorder.h"
 
+#include <string>
+
+#include "camera_enums.hpp"
+#include "gst_helper.hpp"
+
+static std::string create_recording_pipeline(const VideoCodec videoCodec,const std::string& out_filename){
+  std::stringstream ss;
+  // input - appsrc
+  ss<<"appsrc ! ";
+  // first de-packetize rtp
+  ss<<OHDGstHelper::create_rtp_depacketize_for_codec(videoCodec);
+  // parse just to be safe
+  ss<<OHDGstHelper::create_parse_for_codec(videoCodec);
+  if(videoCodec==VideoCodec::H264 || videoCodec==VideoCodec::H265){
+    ss <<"matroskamux ! filesink location="<<out_filename;
+  }else{
+    ss <<"avimux ! filesink location="<<out_filename;
+  }
+  return ss.str();
+}
 
 void GstVideoRecorder::on_video_data(int codec, const uint8_t *data,
                                      int data_len) {
