cebf12e3cb55700788c955a7e21e093ecc5d899a
consti10 <geierconstantinabc@gmail.com>
2023-06-20 14:35:36 +0200

    add new feature - last known position

--- OpenHD/ohd_telemetry/src/last_known_position/LastKnowPosition.cpp
+++ OpenHD/ohd_telemetry/src/last_known_position/LastKnowPosition.cpp
@@ -2,7 +2,32 @@
 // Created by consti10 on 23.01.23.
 //
 
-static constexpr auto LAST_KNOWN_POSITION_PATH="/home/openhd/LastKnownPosition/";
-
-
 #include "LastKnowPosition.h"
+#include "openhd_util_filesystem.h"
+#include "openhd_spdlog.h"
+
+static constexpr auto LAST_KNOWN_POSITION_DIRECTORY="/home/openhd/LastKnownPosition/";
+
+static std::string get_position_filename(int index){
+    return LAST_KNOWN_POSITION_DIRECTORY+fmt::format("pos{}.txt",index);
+}
+
+LastKnowPosition::LastKnowPosition() {
+  OHDFilesystemUtil::create_directories(LAST_KNOWN_POSITION_DIRECTORY);
+}
+
+
+void LastKnowPosition::on_new_position(double latitude, double longitude,
+                                       double altitude) {
+  // We limit updates to once every X ms to not pollute the disk too much
+  const auto delta=std::chrono::steady_clock::now()-m_last_position_update;
+  if(delta<std::chrono::milliseconds(500)){
+    return ;
+  }
+  // Update the position in a rolling fashion, overwriting the oldest file
+  m_last_position_update=std::chrono::steady_clock::now();
+  const auto filename= get_position_filename(m_update_index);
+  OHDFilesystemUtil::write_file(filename,fmt::format("Lat:{},Lon:{},Alt:{}",latitude,longitude,altitude));
+  m_update_index++;
+  m_update_index = m_update_index % 10;
+}
--- OpenHD/ohd_telemetry/src/last_known_position/LastKnowPosition.h
+++ OpenHD/ohd_telemetry/src/last_known_position/LastKnowPosition.h
@@ -5,18 +5,24 @@
 #ifndef OPENHD_OPENHD_OHD_TELEMETRY_SRC_LAST_KNOWN_POSITION_LASTKNOWPOSITION_H_
 #define OPENHD_OPENHD_OHD_TELEMETRY_SRC_LAST_KNOWN_POSITION_LASTKNOWPOSITION_H_
 
+#include <string>
+#include <chrono>
+
 /**
  * This class exposes the following simple functionality:
  * Constantly listen for updates from the FC in regards to its position
- * (aka MAVLINK_MSG_ID_GLOBAL_POSITION_INT messages) and write the coordinates to a file
- * overwriting the previous position. This way, if the drone crashes, the user can always
- * get the last known position from a file in openhd.
+ * (aka MAVLINK_MSG_ID_GLOBAL_POSITION_INT messages) and write the coordinates
+ * to a file overwriting the previous position. This way, if the drone crashes,
+ * the user can always get the last known position from a file in openhd.
  * TODO
  */
 class LastKnowPosition {
  public:
   LastKnowPosition();
   void on_new_position(double latitude,double longitude,double altitude);
+ private:
+  std::chrono::steady_clock::time_point m_last_position_update=std::chrono::steady_clock::now();
+  int m_update_index=0;
 };
 
 #endif  // OPENHD_OPENHD_OHD_TELEMETRY_SRC_LAST_KNOWN_POSITION_LASTKNOWPOSITION_H_
