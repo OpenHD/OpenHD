81c111fa566dfb9808ad7a0d2cd7d2b10970da56
consti10 <geierconstantinabc@gmail.com>
2023-06-20 23:03:53 +0200

    new feature - automatically demux to .mp4

--- OpenHD/ohd_video/inc/gst_recording_demuxer.h
+++ OpenHD/ohd_video/inc/gst_recording_demuxer.h
@@ -8,8 +8,12 @@
 #include <memory>
 #include <thread>
 
+// Simple util class / namespace for demuxing .mkv air recording files
+// uses gstreamer & command line util
 class GstRecordingDemuxer {
  public:
+  // Find all files that end in .mkv in the openhd videos (air recording) directory and demuxes
+  // them to .mp4 for easier use.
   void demux_all_mkv_files_async();
   static GstRecordingDemuxer& instance();
  private:
