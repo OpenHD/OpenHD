6f66fc12af3dd6342645b980ae763c0016874757
consti10 <geierconstantinabc@gmail.com>
2023-06-22 17:23:48 +0200

    do not demux while armed (during flight)

--- OpenHD/ohd_video/inc/gst_recording_demuxer.h
+++ OpenHD/ohd_video/inc/gst_recording_demuxer.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 #include <thread>
+#include <vector>
 
 // Simple util class / namespace for demuxing .mkv air recording files
 // uses gstreamer & command line util
@@ -15,10 +16,12 @@ class GstRecordingDemuxer {
   ~GstRecordingDemuxer();
   // Find all files that end in .mkv in the openhd videos (air recording) directory and demuxes
   // them to .mp4 for easier use.
+  // If a demuxing task is already running, we just spawn another thread - e.g. this method is always
+  // guaranteed to return immediately
   void demux_all_mkv_files_async();
   static GstRecordingDemuxer& instance();
  private:
-  std::unique_ptr<std::thread> m_demux_thread=nullptr;
+  std::vector<std::unique_ptr<std::thread>> m_demux_threads;
 };
 
 #endif  // OPENHD_GST_RECORDING_DEMUXER_H
