1c706735f382eb6e877b122b43d1ec2e61ab8a58
consti10 <geierconstantinabc@gmail.com>
2023-06-27 16:19:36 +0200

    make demuxing thread safe

--- OpenHD/ohd_video/inc/gst_recording_demuxer.h
+++ OpenHD/ohd_video/inc/gst_recording_demuxer.h
@@ -15,12 +15,11 @@
 class GstRecordingDemuxer {
  public:
   ~GstRecordingDemuxer();
-  // Find all files that end in .mkv in the openhd videos (air recording) directory and demuxes
-  // them to .mp4 for easier use.
-  // If a demuxing task is already running, we just spawn another thread - e.g. this method is always
-  // guaranteed to return immediately
+  // Find all files that end in .mkv in the openhd videos (air recording) directory and then spawns a new thread to demux the file
+  // (unless it is already being demuxed)
   void demux_all_remaining_mkv_files_async();
   // demux a specific .mkv file (async) unless it is already being demuxed
+  // thread-safe
   void demux_mkv_file_async_threadsafe(std::string filename);
   static GstRecordingDemuxer& instance();
  private:
@@ -28,7 +27,7 @@ class GstRecordingDemuxer {
     std::string filename;
     std::shared_ptr<std::thread> thread;
   };
-  std::vector<DeMuxOperation> m_demux_ops;
+  std::vector<DeMuxOperation> m_demux_ops{};
   // makes sure we spawn exactly one demux thread per file
   std::mutex m_demux_ops_mutex;
 };
