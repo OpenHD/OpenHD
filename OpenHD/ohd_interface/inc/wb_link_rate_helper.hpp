947527d537af141aee528482e1419c7c3519caa2
consti10 <geierconstantinabc@gmail.com>
2023-07-12 13:33:07 +0200

    refactor rate into its own .hpp file

--- OpenHD/ohd_interface/inc/wb_link_rate_helper.hpp
+++ OpenHD/ohd_interface/inc/wb_link_rate_helper.hpp
@@ -6,70 +6,89 @@
 #define OPENHD_WB_LINK_RATE_HELPER_HPP
 
 #include "wifi_card.h"
+#include <stdint.h>
 
 namespace openhd::wb{
 
 // Theoretical rates can be found here: https://mcsindex.com/
 // These values are openhd evo specific, since there is more to rates than just the bitrate ;)
-static uint32_t rtl8812au_get_max_rate_5G_kbits(uint16_t mcs_index) {
+struct Rate20Mhz40Mhz{
+  uint32_t rate_20mhz;
+  uint32_t rate_40mhz;
+};
+static Rate20Mhz40Mhz rtl8812au_get_max_rate_5G_kbits(uint16_t mcs_index) {
   switch (mcs_index) {
     case 0:
-      //theoretical:6.5
-      // max injection rate possible measured on the bench: 5.7
+      //theoretical:6.5 | 13.5
+      // max injection rate possible measured on the bench: 5.7 | 11.2
       // OLD return 4500;
-      return 4700; // minus 1MBit/s
+      return {
+        4700, // minus 1MBit/s
+        12500, // minus 1MBit/s
+      };
     case 1:
-      //theoretical:13
-      // max injection rate possible measured on the bench: 10.8
+      //theoretical:13 | 27
+      // max injection rate possible measured on the bench: 10.8 | 19.8
       // OLD return 6500;
-      return 9800; // minus 1MBit/s
+      return {
+        9800, // minus 1MBit/s
+        17800, // minus 2MBit/s
+      };
     case 2:
       //@Norbert: Successfully flown on MCS2 and 7MBit/s video, aka 8.4MBit/s after FEC
-      //theoretical:19.5
-      // max injection rate possible measured on the bench: 15.2
+      //theoretical:19.5 | 40.5
+      // max injection rate possible measured on the bench: 15.2 | 26.6
       // OLD return 8500;
-      return 13200; // minus 2MBit/s
+      return {
+        13200, // minus 2MBit/s
+        23600, // minus 3MBit/s
+      };
     case 3:
-      //theoretical:26
-      // max injection rate possible measured on the bench: 19.2
+      //theoretical:26 | 54
+      // max injection rate possible measured on the bench: 19.2 | 30+ (out of capabilities of encoder)
       // OLD return 12000;
-      return 16200; // minus 3MBit/s
+      return {
+        16200, // minus 3MBit/s
+        27000, // minus 3MBit/s
+      };
     // In general, we only use / recommend MCS 0..3
     case 4:
-      return 17000; //theoretical:39
+      //theoretical:39
+      return {
+        17000, 30000
+      };
     case 5:
-      return 23000; //theoretical:52
+      //theoretical:52
+      return {
+        23000, 30000
+      };
     case 6:
-      return 36000; //theoretical:58.5
+      //theoretical:58.5
+      return {
+        30000, 30000
+      };
     case 7:
-      return 36000; //theoretical:65
+      //theoretical:65
+      return {
+        30000, 30000
+      };
     default:
       break;
   }
-  return 5000;
+  return {5000,5000};
 }
 
 static uint32_t rtl8812au_get_max_rate_5G_kbits(uint16_t mcs_index,bool is_40_mhz){
   auto rate_kbits= rtl8812au_get_max_rate_5G_kbits(mcs_index);
-  if(is_40_mhz){
-    // Theoretically 40Mhz should give exactly 2x the bitrate, but be a bit conservative here
-    rate_kbits = static_cast<uint32_t>(std::roundl(rate_kbits*1.7));
-  }
-  return rate_kbits;
-}
-
-static uint32_t rtl8812au_get_max_rate_2G_kbits(uint16_t mcs_index) {
-  // dirty, but 2G in general sucks
-  return rtl8812au_get_max_rate_5G_kbits(mcs_index) * 80/100;
+  return is_40_mhz ? rate_kbits.rate_40mhz : rate_kbits.rate_20mhz;
 }
 
 static uint32_t rtl8812au_get_max_rate_2G_kbits(uint16_t mcs_index,bool is_40_mhz){
-  auto rate_kbits= rtl8812au_get_max_rate_2G_kbits(mcs_index);
+  const auto rates_5G= rtl8812au_get_max_rate_5G_kbits(mcs_index);
   if(is_40_mhz){
-    // Theoretically 40Mhz should give exactly 2x the bitrate, but be a bit conservative here
-    rate_kbits = static_cast<uint32_t>(std::roundl(rate_kbits*1.7));
+    return rates_5G.rate_40mhz * 60/100;
   }
-  return rate_kbits;
+  return rates_5G.rate_20mhz * 80/100;
 }
 
 static uint32_t get_max_rate_possible_5G(const WiFiCard& card,uint16_t mcs_index,bool is_40Mhz){
