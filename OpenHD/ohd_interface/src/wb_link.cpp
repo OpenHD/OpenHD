747615a3b9d615b7f1718eb3050ee105756ba542
consti10 <geierconstantinabc@gmail.com>
2023-07-14 01:10:59 +0200

    telemetry retransmission(s) feature

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -755,7 +755,7 @@ bool WBLink::check_work_queue_empty() {
 
 void WBLink::transmit_telemetry_data(TelemetryTxPacket packet) {
   assert(packet.n_injections>=1);
-  m_console->debug("N injections:{}",packet.n_injections);
+  //m_console->debug("N injections:{}",packet.n_injections);
   const auto res=m_wb_tele_tx->try_enqueue_packet(packet.data,packet.n_injections);
   if(!res)m_console->debug("Enqueing tele packet failed");
   if(!m_broadcast_cards.at(0).supports_injection){
