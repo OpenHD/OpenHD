df1711a01fb62769c815480fee216b9c477f170e
consti10 <geierconstantinabc@gmail.com>
2023-08-11 15:09:33 +0200

    add key gen helper

---
+++ OpenHD/ohd_interface/inc/wb_link_key_gen_helper.h
@@ -0,0 +1,10 @@
+//
+// Created by consti10 on 11.08.23.
+//
+
+#ifndef OPENHD_WB_LINK_KEY_GEN_HELPER_H
+#define OPENHD_WB_LINK_KEY_GEN_HELPER_H
+
+class wb_link_key_gen_helper {};
+
+#endif  // OPENHD_WB_LINK_KEY_GEN_HELPER_H
---
+++ OpenHD/ohd_interface/src/wb_link_key_gen_helper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by consti10 on 11.08.23.
+//
+
+#include "../inc/wb_link_key_gen_helper.h"
