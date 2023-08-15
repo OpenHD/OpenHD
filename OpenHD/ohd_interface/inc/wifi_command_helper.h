0728043678de79aa24a19a31731bc82c5e2a742c
consti10 <geierconstantinabc@gmail.com>
2023-08-15 19:16:48 +0200

    wifi channels

--- OpenHD/ohd_interface/inc/wifi_command_helper.h
+++ OpenHD/ohd_interface/inc/wifi_command_helper.h
@@ -85,6 +85,7 @@ bool iw_supports_monitor_mode(int phy_index);
 // type 0= rtl8812au, 1=rtl88x2bu driver
 bool openhd_driver_set_frequency_and_channel_width(int type,const std::string &device, uint32_t freq_mhz, uint32_t channel_width);
 
+// RTL8812bu driver only so far
 bool openhd_driver_set_tx_power(const std::string& device,uint32_t tx_power_mBm);
 
 }
