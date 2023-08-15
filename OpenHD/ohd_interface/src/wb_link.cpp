56b2aeb4b912d605b2c5ab8b19eb1e331802a5b9
consti10 <geierconstantinabc@gmail.com>
2023-08-16 01:17:10 +0200

    debug mcs tmp

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -659,6 +659,9 @@ void WBLink::update_statistics() {
         }
     }
   }
+  if(true){
+      m_console->debug("Last received packet mcs:{} chan_width:{}",rxStats.last_received_packet_mcs_index,rxStats.last_received_packet_channel_width);
+  }
 }
 
 void WBLink::perform_rate_adjustment() {
