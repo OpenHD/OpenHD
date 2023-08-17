6abcfc8aeb2fbff901a2374d2b1faac5d1fd24ff
consti10 <geierconstantinabc@gmail.com>
2023-08-17 11:44:39 +0200

    improve rate

--- OpenHD/ohd_interface/inc/wb_link_rate_helper.hpp
+++ OpenHD/ohd_interface/inc/wb_link_rate_helper.hpp
@@ -118,6 +118,58 @@ static Rate20Mhz40Mhz rtl8812au_get_max_rate_5G_kbits(uint16_t mcs_index) {
   return {5000,5000};
 }
 
+static Rate20Mhz40Mhz rtl8812au_get_max_rate_2G_kbits(uint16_t mcs_index) {
+    switch (mcs_index) {
+        case 0:
+            //theoretical:6.5 | 13.5
+            // max injection rate possible measured on the bench: 5.7 | 10.4
+            // OLD return 4500;
+            return {
+                    5700-1000, // minus 1MBit/s
+                    10400-3000, // minus 3MBit/s
+            };
+        case 1:
+            //theoretical:13 | 27
+            // max injection rate possible measured on the bench: 10.8 | 18.8
+            // OLD return 6500;
+            return {
+                    10800-1000, // minus 1MBit/s
+                    18800-3500, // minus 3.5MBit/s
+            };
+        case 2:
+            //@Norbert: Successfully flown on MCS2 and 7MBit/s video, aka 8.4MBit/s after FEC
+            //theoretical:19.5 | 40.5
+            // max injection rate possible measured on the bench: 15.2 | 26.6
+            // OLD return 8500;
+            return {
+                    15200-2000, // minus 2MBit/s
+                    26600-4000, // minus 4MBit/s
+            };
+        case 3:
+            //theoretical:26 | 54
+            // max injection rate possible measured on the bench: 19.2 | 30+ (out of capabilities of encoder)
+            // OLD return 12000;
+            return {
+                    19200-3000, // minus 3MBit/s
+                    30000-5000, // minus 5MBit/s
+            };
+            // In general, we only use / recommend MCS 0..3
+        case 4:
+            //theoretical:39
+            return {
+                    20000, 30000
+            };
+        default:{
+            openhd::log::get_default()->warn("MCS >4 not recommended");
+            //theoretical:39
+            return {
+                    20000, 30000
+            };
+        }
+    }
+    assert(false);
+}
+
 static uint32_t rtl8812au_get_max_rate_5G_kbits(uint16_t mcs_index,bool is_40_mhz){
   auto rate_kbits= rtl8812au_get_max_rate_5G_kbits(mcs_index);
   return is_40_mhz ? rate_kbits.rate_40mhz : rate_kbits.rate_20mhz;
@@ -125,37 +177,34 @@ static uint32_t rtl8812au_get_max_rate_5G_kbits(uint16_t mcs_index,bool is_40_mh
 
 // Dirty, since 2.4G in general is not that important
 static uint32_t rtl8812au_get_max_rate_2G_kbits(uint16_t mcs_index,bool is_40_mhz){
-  const auto rates_5G= rtl8812au_get_max_rate_5G_kbits(mcs_index);
-  if(is_40_mhz){
-    return rates_5G.rate_40mhz * 60/100;
-  }
-  return rates_5G.rate_20mhz * 80/100;
+    auto rate_kbits= rtl8812au_get_max_rate_2G_kbits(mcs_index);
+    return is_40_mhz ? rate_kbits.rate_40mhz : rate_kbits.rate_20mhz;
 }
 
-static uint32_t get_max_rate_possible_5G(const WiFiCard& card,uint16_t mcs_index,bool is_40Mhz){
-  if(card.type==WiFiCardType::Realtek8812au){
+static uint32_t get_max_rate_possible_5G_kbits(const WiFiCard& card,uint16_t mcs_index,bool is_40Mhz){
+  if(card.type==WiFiCardType::Realtek8812au || card.type==WiFiCardType::Realtek88x2bu){
     return rtl8812au_get_max_rate_5G_kbits(mcs_index, is_40Mhz);
-  }
-  if(card.type==WiFiCardType::Realtek88x2bu){
-    // Hard coded, since "some rate" is hard coded in the driver.
-    return 10000;
   }
   // fallback for any other weak crap
   return 5000;
 }
+static uint32_t get_max_rate_possible_2G_kbits(const WiFiCard& card,uint16_t mcs_index,bool is_40Mhz){
+    if(card.type==WiFiCardType::Realtek8812au || card.type==WiFiCardType::Realtek88x2bu){
+        return rtl8812au_get_max_rate_5G_kbits(mcs_index, is_40Mhz);
+    }
+    // fallback for any other weak crap
+    return 5000;
+}
 
 static uint32_t get_max_rate_possible(const WiFiCard& card,const openhd::WifiSpace wifi_space,uint16_t mcs_index,bool is_40Mhz){
-  // Generally, 2G just sucks - we do not have proper rate control for it
-  if(wifi_space==WifiSpace::G2_4){
-    if(card.type==WiFiCardType::Realtek8812au){
-      return rtl8812au_get_max_rate_2G_kbits(mcs_index,is_40Mhz);
+    if(wifi_space==WifiSpace::G2_4){
+        if(card.type==WiFiCardType::Atheros9k || card.type==WiFiCardType::Atheros9khtc){
+            return 9*1000; // About what is fixed in the driver
+        }
+        return get_max_rate_possible_2G_kbits(card,mcs_index,is_40Mhz);
     }
-    if(card.type==WiFiCardType::Atheros9khtc){
-        return 9*1000; // About what is fixed in the driver
-    }
-    return 5000;
-  }
-  return get_max_rate_possible_5G(card,mcs_index,is_40Mhz);
+    assert(wifi_space==WifiSpace::G5_8);
+    return get_max_rate_possible_5G_kbits(card,mcs_index,is_40Mhz);
 }
 
 static uint32_t deduce_fec_overhead(uint32_t bandwidth_kbits,int fec_overhead_perc){
