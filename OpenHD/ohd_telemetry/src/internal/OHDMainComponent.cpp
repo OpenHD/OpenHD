4c35b9a6881007025c5ef34ee9784c7866349afc
consti10 <geierconstantinabc@gmail.com>
2023-08-19 01:00:39 +0200

    exp channels

--- OpenHD/ohd_telemetry/src/internal/OHDMainComponent.cpp
+++ OpenHD/ohd_telemetry/src/internal/OHDMainComponent.cpp
@@ -120,6 +120,7 @@ std::vector<MavlinkMessage> OHDMainComponent::process_mavlink_messages(std::vect
             const auto freq_bands=static_cast<uint32_t>(command.param1);
             const auto channel_widths=static_cast<uint32_t>(command.param2);
             m_console->debug("OPENHD_CMD_INITIATE_CHANNEL_SEARCH {} {}",freq_bands,channel_widths);
+            bool success= false;
             if((freq_bands==0 || freq_bands==1 || freq_bands==2) &&
                 (channel_widths==0 || channel_widths==1 || channel_widths==2)){
               const bool scan_2g=freq_bands==0 || freq_bands==1;
@@ -127,24 +128,21 @@ std::vector<MavlinkMessage> OHDMainComponent::process_mavlink_messages(std::vect
               const bool scan_20Mhz=channel_widths==0 || channel_widths==1;
               const bool scan_40Mhz=channel_widths==0 || channel_widths==2;
               if(m_opt_action_handler && m_opt_action_handler->wb_cmd_scan_channels){
-                const auto res=m_opt_action_handler->wb_cmd_scan_channels({scan_2g,scan_5g,scan_20Mhz,scan_40Mhz});
-                ret.push_back(ack_command(msg.m.sysid,msg.m.compid,command.command,res));
-              }else{
-                ret.push_back(ack_command(msg.m.sysid,msg.m.compid,command.command, false));
+                success=m_opt_action_handler->wb_cmd_scan_channels({scan_2g,scan_5g,scan_20Mhz,scan_40Mhz});
               }
             }
+            ret.push_back(ack_command(msg.m.sysid,msg.m.compid,command.command,success));
           }
         }else if(command.command==OPENHD_CMD_INITIATE_CHANNEL_ANALYZE){
             if(RUNS_ON_AIR){
                 m_console->debug("Scan channels is only a feature for ground unit");
                 break;
             }else{
-                if(m_opt_action_handler && m_opt_action_handler->wb_cmd_analyze_channels){
-                    const auto success=m_opt_action_handler->wb_cmd_analyze_channels();
-                    ret.push_back(ack_command(msg.m.sysid,msg.m.compid,command.command,success));
-                }else{
-                    ret.push_back(ack_command(msg.m.sysid,msg.m.compid,command.command, false));
+                bool success= false;
+                if(m_opt_action_handler && m_opt_action_handler->wb_cmd_analyze_channels) {
+                    success = m_opt_action_handler->wb_cmd_analyze_channels();
                 }
+                ret.push_back(ack_command(msg.m.sysid,msg.m.compid,command.command,success));
             }
         }else{
             m_console->debug("Unknown command {}",command.command);
