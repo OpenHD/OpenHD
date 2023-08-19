aa4ba435a012bb1a5533702b9616e60360e17b59
consti10 <geierconstantinabc@gmail.com>
2023-08-19 14:57:11 +0200

    exp channels

--- OpenHD/ohd_interface/test/test_wifi_commands.cpp
+++ OpenHD/ohd_interface/test/test_wifi_commands.cpp
@@ -6,6 +6,7 @@
 #include "wifi_command_helper.h"
 //#include "wifi_command_helper2.h"
 #include "wifi_card_discovery.h"
+#include "wb_link_helper.h"
 
 #include <vector>
 #include <utility>
@@ -14,10 +15,11 @@ static void test_all_supported_frequencies(const WiFiCard& card){
   openhd::log::get_default()->debug("test_all_supported_frequencies begin");
   std::vector<std::pair<int,bool>> results;
   for(auto frequency_mhz: card.supported_frequencies_5G){
-    const auto success=wifi::commandhelper::iw_set_frequency_and_channel_width(card.device_name,frequency_mhz,20);
+    //const auto success=wifi::commandhelper::iw_set_frequency_and_channel_width(card.device_name,frequency_mhz,20);
+    const auto success=openhd::wb::set_frequency_and_channel_width_for_all_cards(frequency_mhz,20,{card});
     results.emplace_back(frequency_mhz,success);
     // Weird, otherwise we might get error resource busy
-    std::this_thread::sleep_for(std::chrono::seconds (2));
+    std::this_thread::sleep_for(std::chrono::seconds (5));
   }
   for(const auto& res:results){
     const int freq=res.first;
@@ -35,7 +37,8 @@ int main(int argc, char *argv[]) {
   OHDUtil::terminate_if_not_root();
 
   // Hard coded for testing
-  const std::string card_name="wlx244bfeb71c05";
+  //const std::string card_name="wlx244bfeb71c05";
+  const std::string card_name="wlxac9e17596103";
 
   auto card_opt = DWifiCards::process_card(card_name);
   if(!card_opt.has_value()){
