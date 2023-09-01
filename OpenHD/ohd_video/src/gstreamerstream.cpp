d595caa9cfbcbf051ece1574d58123c59a81ec82
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-09-01 23:33:17 +0200

    added variable bitrate support for rock5

--- OpenHD/ohd_video/src/gstreamerstream.cpp
+++ OpenHD/ohd_video/src/gstreamerstream.cpp
@@ -507,7 +507,7 @@ void GStreamerStream::handle_change_bitrate_request(openhd::ActionHandler::LinkB
     }
   }else{
     const auto cam_type=m_camera_holder->get_camera().type;
-    if(cam_type==CameraType::RPI_CSI_LIBCAMERA || cam_type==CameraType::RPI_CSI_VEYE_V4l2){
+    if(cam_type==CameraType::RPI_CSI_LIBCAMERA || cam_type==CameraType::RPI_CSI_VEYE_V4l2 || cam_type==CameraType::ROCKCHIP_CSI){
       m_console->warn("Bitrate change requires restart");
       // These cameras are known to handle a restart quickly, but it still sucks v4l2h264enc does not support changing the bitrate at run time
       m_camera_holder->unsafe_get_settings().h26x_bitrate_kbits=bitrate_for_encoder_kbits;
