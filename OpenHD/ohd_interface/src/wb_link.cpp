2613537e4cbb32ae0dc58321a778184851cd7300
consti10 <geierconstantinabc@gmail.com>
2023-09-08 09:21:14 +0200

    openhd_global - single, global instance (replaces - kinda - openhd action handler)

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -1017,12 +1017,12 @@ WBLink::ScanResult WBLink::scan_channels(const openhd::ActionHandler::ScanChanne
           continue;
       }
       if(m_opt_action_handler){
-            openhd::ActionHandler::ScanChannelsProgress tmp{};
-            tmp.channel_mhz=(int)channel.frequency;
-            tmp.channel_width_mhz=channel_width;
-            tmp.success= false;
-            tmp.progress=OHDUtil::calculate_progress_perc(i+1,channels_to_scan.size());
-            m_opt_action_handler->add_scan_channels_progress(tmp);
+        openhd::ActionHandler::ScanChannelsProgress tmp{};
+        tmp.channel_mhz=(int)channel.frequency;
+        tmp.channel_width_mhz=channel_width;
+        tmp.success= false;
+        tmp.progress=OHDUtil::calculate_progress_perc(i+1,channels_to_scan.size());
+        m_opt_action_handler->add_scan_channels_progress(tmp);
       }
       // sleeep a bit - some cards /drivers might need time switching
       std::this_thread::sleep_for(std::chrono::milliseconds(200));
