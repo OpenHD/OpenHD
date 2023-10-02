7e689a79609ce78227a5949a6b42bf6c3580fabe
Pete A <peter.allenm@gmail.com>
2023-10-02 16:46:04 +0100

    Betaflight FC's are on sysID 0 (rather than 1 for every other FC)

--- OpenHD/ohd_telemetry/src/internal/OHDMainComponent.cpp
+++ OpenHD/ohd_telemetry/src/internal/OHDMainComponent.cpp
@@ -199,7 +199,7 @@ std::optional<MavlinkMessage> OHDMainComponent::handle_timesync_message(const Ma
 
 void OHDMainComponent::check_fc_messages_for_actions(const std::vector<MavlinkMessage>& messages) {
   for(const auto& msg:messages){
-    if(msg.m.sysid== OHD_SYS_ID_FC && msg.m.msgid==MAVLINK_MSG_ID_HEARTBEAT){
+    if(((msg.m.sysid== OHD_SYS_ID_FC) || (msg.m.sysid== OHD_SYS_ID_FC_BETAFLIGHT)) && msg.m.msgid==MAVLINK_MSG_ID_HEARTBEAT){
       mavlink_heartbeat_t heartbeat;
       mavlink_msg_heartbeat_decode(&msg.m, &heartbeat);
       const auto mode = (MAV_MODE_FLAG)heartbeat.base_mode;
@@ -210,7 +210,7 @@ void OHDMainComponent::check_fc_messages_for_actions(const std::vector<MavlinkMe
     }
     // We only change the mcs on the air unit (since downlink is the only thing that requires 'higher' bandwidth)
     if(RUNS_ON_AIR){
-      if(msg.m.sysid==OHD_SYS_ID_FC && msg.m.msgid==MAVLINK_MSG_ID_RC_CHANNELS){
+      if(((msg.m.sysid== OHD_SYS_ID_FC) || (msg.m.sysid== OHD_SYS_ID_FC_BETAFLIGHT)) && msg.m.msgid==MAVLINK_MSG_ID_RC_CHANNELS){
         mavlink_rc_channels_t rc_channels;
         mavlink_msg_rc_channels_decode(&msg.m, &rc_channels);
         const auto tmp=mavlink_msg_rc_channels_to_array(rc_channels);
