628837aa2280c8c605ce6844e82bfd37e48a2f25
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-10-12 22:03:58 +0200

    testing upload (#965)

--- package.sh
+++ package.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-PACKAGE_ARCH="${1}"
-OS="${2}"
+CUSTOM="${1}"
+PACKAGE_ARCH="${2}"
+OS="${3}"
 
-PACKAGE_NAME="openhd"
-PKGDIR="/tmp/${PACKAGE_NAME}-installdir"
+PKGDIR="/tmp/openhd-installdir"
 VERSION="2.5.1-evo-alpha-$(date '+%Y%m%d%H%M')-$(git rev-parse --short HEAD)"
 
 create_package_directory() {
@@ -34,48 +34,38 @@ create_package_directory() {
 }
 
 build_package() {
+  
+  if [[ "${PACKAGE_ARCH}" == "armhf" ]]; then
+      if [[ "${CUSTOM}" == "standard" ]]; then
+      echo "this is cake"
+      PACKAGE_NAME="openhd"
+      PACKAGES="-d libcamera-openhd -d gst-openhd-plugins -d iw -d nmap-d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
+      PLATFORM_CONFIGS=""
+      else
+      echo "this isn't cake"
+      PACKAGE_NAME="openhd-x20"
+      PACKAGES=""
+      PLATFORM_CONFIGS=""
+      fi
+  else
+    echo "this isn't cake and also not pi"
+    PACKAGE_NAME="openhd"
+    PACKAGES="-d iw -d nmap-d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
+    PLATFORM_CONFIGS=""
+  fi
+  
   rm "${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb" > /dev/null 2>&1 || true
   
   cmake OpenHD/
   make -j4
   cp openhd ${PKGDIR}/usr/local/bin/openhd
 
-  if [[ "${PACKAGE_ARCH}" == "armhf" ]]; then
-    PLATFORM_PACKAGES="-d libcamera-openhd -d gst-openhd-plugins"
-    PLATFORM_CONFIGS=""
-  elif [[ "${PACKAGE_ARCH}" != "arm64" ]]; then
-    PLATFORM_PACKAGES="-d libboost-filesystem1.74-dev"
-    PLATFORM_CONFIGS=""
-  else
-    PLATFORM_PACKAGES="-d libboost-dev -d libboost-filesystem-dev"
-    PLATFORM_CONFIGS=""
-  fi
-
   fpm -a "${PACKAGE_ARCH}" -s dir -t deb -n "${PACKAGE_NAME}" -v "${VERSION}" -C "${PKGDIR}" \
     ${PLATFORM_CONFIGS} \
     -p "${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb" \
     --after-install after-install.sh \
     --before-install before-install.sh \
-    ${PLATFORM_PACKAGES} \
-    -d "iw" \
-    -d "nmap" \
-    -d "aircrack-ng" \
-    -d "i2c-tools" \
-    -d "libv4l-dev" \
-    -d "libusb-1.0-0" \
-    -d "libpcap-dev" \
-    -d "libnl-3-dev" \
-    -d "libnl-genl-3-dev" \
-    -d "libsdl2-2.0-0" \
-    -d "libsodium-dev" \
-    -d "gstreamer1.0-plugins-base" \
-    -d "gstreamer1.0-plugins-good" \
-    -d "gstreamer1.0-plugins-bad" \
-    -d "gstreamer1.0-plugins-ugly" \
-    -d "gstreamer1.0-libav" \
-    -d "gstreamer1.0-tools" \
-    -d "gstreamer1.0-alsa" \
-    -d "gstreamer1.0-pulseaudio"
+    ${PACKAGES} 
 }
 
   #Main Build
