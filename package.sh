3dee9688fc3639a537cd2798315d0916a1cc4311
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-10-26 01:05:05 +0200

    fix arm64 builds (#967)
    * remove boost
    
    * fix raspberry to bullseye
    
    * Update build_package_rpi.yml
    
    * fix
    
    * trying to fix rpi build
    
    * Update build_package_rpi.yml
    
    * do not upload if it is in no release
    
    * debug upload
    
    * Update package.sh
    
    * Enable x20 builds (#964)
    
    * fix upload repo
    
    * refactor
    
    * Allow mavlink rc_channel_raw message to be used for mcs changes as well as rc_channel (betaflight)
    
    * Remove extra console log
    
    * fix building for arm64
    
    ---------
    
    Co-authored-by: Pete A <peter.allenm@gmail.com>

--- package.sh
+++ package.sh
@@ -39,7 +39,7 @@ build_package() {
       if [[ "${CUSTOM}" == "standard" ]]; then
       echo "this is cake"
       PACKAGE_NAME="openhd"
-      PACKAGES="-d libcamera-openhd -d gst-openhd-plugins -d iw -d nmap-d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
+      PACKAGES="-d libcamera-openhd -d gst-openhd-plugins -d iw -d nmap -d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
       PLATFORM_CONFIGS=""
       else
       echo "this isn't cake"
