2b8b3b26b04bfa26ab3dd602fb372ca7e91a668f
consti10 <geierconstantinabc@gmail.com>
2023-11-30 11:16:54 +0100

    build submodules independently - harsh exit on failure (we use it in the cis for testing)

--- OpenHD/build_submodules_independently.sh
+++ OpenHD/build_submodules_independently.sh
@@ -4,17 +4,17 @@
 # Here we build all the submodules independently from each other to make sure
 # no unwanted dependencies were introduced by accident during OpenHD development
 cd ohd_common || exit
-cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug
-cmake --build build_debug
+cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug || exit
+cmake --build build_debug || exit
 
 cd ../ohd_interface || exit
-cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug
-cmake --build build_debug
+cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug || exit
+cmake --build build_debug || exit
 
 cd ../ohd_telemetry || exit
-cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug
-cmake --build build_debug
+cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug || exit
+cmake --build build_debug || exit
 
 cd ../ohd_video || exit
-cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug
-cmake --build build_debug
+cmake -S . -B build_debug -DCMAKE_BUILD_TYPE=Debug || exit
+cmake --build build_debug || exit
