96e1be7982f2859d5feee9ae02ec7520f52e9352
consti10 <geierconstantinabc@gmail.com>
2024-01-28 10:16:18 +0100

    treat emulated as rtl8812au regarding rates on 2.4G too

--- OpenHD/ohd_interface/inc/wb_link_rate_helper.hpp
+++ OpenHD/ohd_interface/inc/wb_link_rate_helper.hpp
@@ -183,7 +183,8 @@ static uint32_t get_max_rate_possible_5G_kbits(const WiFiCard& card,uint16_t mcs
   return 5000;
 }
 static uint32_t get_max_rate_possible_2G_kbits(const WiFiCard& card,uint16_t mcs_index,bool is_40Mhz){
-    if(card.type==WiFiCardType::OPENHD_RTL_88X2AU || card.type==WiFiCardType::OPENHD_RTL_88X2BU || card.type==WiFiCardType::OPENHD_RTL_8852BU){
+    if(card.type==WiFiCardType::OPENHD_RTL_88X2AU || card.type==WiFiCardType::OPENHD_RTL_88X2BU || card.type==WiFiCardType::OPENHD_RTL_8852BU
+        || card.type==WiFiCardType::OPENHD_EMULATED){
         return rtl8812au_get_max_rate_2G_kbits(mcs_index, is_40Mhz);
     }
     // fallback for any other weak crap
