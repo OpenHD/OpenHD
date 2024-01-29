6ccd391bf7bcb204b85c69830bf0576a2f368a03
consti10 <geierconstantinabc@gmail.com>
2024-01-29 20:35:08 +0100

    framerate on file camera

--- OpenHD/ohd_video/inc/gst_helper.hpp
+++ OpenHD/ohd_video/inc/gst_helper.hpp
@@ -648,7 +648,8 @@ static std::string create_dummy_filesrc_stream(const OHDPlatform& platform,const
     ss<<"queue ! ";
     ss<<"videoscale ! ";
     ss<<"video/x-raw, format=I420";
-    ss<<fmt::format(",width={}, height={} ! ",settings.streamed_video_format.width,settings.streamed_video_format.height);
+    ss<<fmt::format(",width={}, height={}, framerate={}/1 ! ",settings.streamed_video_format.width,settings.streamed_video_format.height,
+                    settings.streamed_video_format.framerate);
     //ss<<createSwEncoder(settings);
     if(settings.force_sw_encode){
         ss << createSwEncoder(settings);
