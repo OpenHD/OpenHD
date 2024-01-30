6301afd80f79ef4dccb149034a8808e4ecc5e8c5
consti10 <geierconstantinabc@gmail.com>
2024-01-30 21:14:26 +0100

    add infiray colorpalete

--- OpenHD/ohd_video/inc/usb_thermal_cam_helper.h
+++ OpenHD/ohd_video/inc/usb_thermal_cam_helper.h
@@ -9,7 +9,7 @@
 namespace openhd{
 
 static bool is_valid_infiray_custom_control_zoom_absolute_value(int value){
-    return value>= 34817 &&  value<=34823;
+    return value>= 34816 &&  value<=34826;
 }
 
 // Apply the setting and hope it works ...
