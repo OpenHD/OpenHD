ff397114ec10f412d8eeaa61e3a718550d880b67
consti10 <geierconstantinabc@gmail.com>
2024-01-31 22:51:26 +0100

    add clang format script

---
+++ OpenHD/run_clang_format.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# From https://stackoverflow.com/a/65988393
+
+THIS_PATH="$(realpath "$0")"
+THIS_DIR="$(dirname "$THIS_PATH")"
+
+# Find all files in THIS_DIR which end in .ino, .cpp, etc., as specified
+# in the regular expression just below
+FILE_LIST="$(find "$THIS_DIR" | grep -E ".*(\.ino|\.cpp|\.c|\.h|\.hpp|\.hh)$")"
+
+echo -e "Files found to format = \n\"\"\"\n$FILE_LIST\n\"\"\""
+
+# Format each file.
+# - NB: do NOT put quotes around `$FILE_LIST` below or else the `clang-format` command will
+#   mistakenly see the entire blob of newline-separated file names as a SINGLE file name instead
+#   of as a new-line separated list of *many* file names!
+clang-format --verbose -i --style=file $FILE_LIST
