6e66137d6aaeddfd2191528e5bbd2da03713d325
consti10 <geierconstantinabc@gmail.com>
2024-02-01 11:21:59 +0100

    use the config file for more options

--- OpenHD/ohd_common/copy_config_file.sh
+++ OpenHD/ohd_common/copy_config_file.sh
@@ -1,4 +1,7 @@
 #bin/bash
 
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 sudo mkdir -p /boot/openhd
-sudo cp config/hardware.config /boot/openhd/hardware.config
+sudo cp $SCRIPTPATH/config/hardware.config /boot/openhd/hardware.config
