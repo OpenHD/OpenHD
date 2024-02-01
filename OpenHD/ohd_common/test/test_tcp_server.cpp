a2474575b6e947ff2a6cffa64d772db5da250a86
consti10 <geierconstantinabc@gmail.com>
2024-02-01 11:02:07 +0100

    wok on udp test

--- OpenHD/ohd_common/test/test_tcp_server.cpp
+++ OpenHD/ohd_common/test/test_tcp_server.cpp
@@ -6,17 +6,30 @@
 
 
 class TestServer:public openhd::TCPServer{
-  explicit TestServer(): openhd::TCPServer("Test",openhd::TCPServer::Config()){};
+ public:
+  explicit TestServer(): openhd::TCPServer("Test",openhd::TCPServer::Config{5760}){};
   void on_external_device(std::string ip, bool connected)override{
-
+      if(connected){
+        openhd::log::get_default()-> debug("Device {} connected",ip);
+      }else{
+        openhd::log::get_default()-> debug("Device {} disconnected",ip);
+      }
   };
   void on_packet_any_tcp_client(const uint8_t* data, int data_len)override{
-
+      // do nothing
+      openhd::log::get_default()-> debug("Got data {}",data_len);
   };
 };
 
 
 int main(int argc, char *argv[]) {
-
-
+  auto test_server=std::make_unique<TestServer>();
+  // Run netcat 0.0.0.0 5760 to test
+  while (true){
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::string buff="Hello\n";
+    const int buff_size=buff.length()+1;
+    test_server->send_message_to_all_clients((uint8_t*)buff.c_str(),buff_size);
+  }
+  std::this_thread::sleep_for(std::chrono::seconds(100));
 }
