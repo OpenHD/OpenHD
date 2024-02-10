6a39fa77069fe935ee783f4aac9e2f96b41500d9
consti10 <geierconstantinabc@gmail.com>
2024-02-10 21:08:45 +0100

    cameras - add USB generic and USB infiray

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -11,10 +11,6 @@
 
 // For development, always 'works' since fully emulated in SW.
 static constexpr int X_CAM_TYPE_DUMMY_SW = 0;  // Dummy sw picture
-// OpenHD supports any usb camera outputting raw video (with sw encoding).
-// H264 usb cameras are not supported, since in general, they do not support
-// changing bitrate/ encoding parameters.
-static constexpr int X_CAM_TYPE_USB = 1;
 // Manually feed camera data (encoded,rtp) to openhd. Bitrate control and more
 // is not working in this mode, making it only valid for development and in
 // extreme cases valid for users that want to use a specific ip camera.
@@ -26,47 +22,56 @@ static constexpr int X_CAM_TYPE_EXTERNAL_IP = 3;
 // For development, camera that reads input from a file, and then re-encodes it
 // using the platform encoder
 static constexpr int X_CAM_TYPE_DEVELOPMENT_FILESRC = 4;
+// ... reserved for development / custom cameras
+
+// OpenHD supports any usb camera outputting raw video (with sw encoding).
+// H264 usb cameras are not supported, since in general, they do not support
+// changing bitrate/ encoding parameters.
+static constexpr int X_CAM_TYPE_USB_GENERIC = 10;
+static constexpr int X_CAM_TYPE_USB_INFIRAY = 11;
+// ... reserved for future (Thermal) USB cameras
+
 //
 // RPI Specific starts here
 //
 // As of now, we have mmal only for the geekworm hdmi to csi adapter
-static constexpr int X_CAM_TYPE_RPI_MMAL_HDMI_TO_CSI = 10;
+static constexpr int X_CAM_TYPE_RPI_MMAL_HDMI_TO_CSI = 20;
 // ... 9 reserved for future use
 // ...
 // RPIF stands for RPI Foundation (aka original rpi foundation cameras)
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V1_OV5647 = 20;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V2_IMX219 = 21;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V3_IMX708 = 22;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_HQ_IMX477 = 23;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V1_OV5647 = 30;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V2_IMX219 = 31;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V3_IMX708 = 32;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_RPIF_HQ_IMX477 = 33;
 // .... 5 reserved for future use
 // Now to all the rpi libcamera arducam cameras
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_SKYMASTERHDR = 30;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_SKYVISIONPRO = 31;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX477M = 32;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX462 = 33;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX327 = 34;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX290 = 35;
-static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX462_LOWLIGHT_MINI = 36;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_SKYMASTERHDR = 40;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_SKYVISIONPRO = 41;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX477M = 42;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX462 = 43;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX327 = 44;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX290 = 45;
+static constexpr int X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX462_LOWLIGHT_MINI = 46;
 // ... 13 reserved for future use
-static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_2MP = 50;
-static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_CSIMX307 = 51;
-static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_CSSC132 = 52;
-static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_MVCAM = 53;
+static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_2MP = 60;
+static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_CSIMX307 = 61;
+static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_CSSC132 = 62;
+static constexpr int X_CAM_TYPE_RPI_V4L2_VEYE_MVCAM = 63;
 // ... 6 reserved for future use
 //
 // X20 Specific starts here
 //
 // Right now we only have one camera, but more (might) follow.
-static constexpr int X_CAM_TYPE_X20_RUNCAM_NANO = 60;
+static constexpr int X_CAM_TYPE_X20_RUNCAM_NANO = 70;
 // ... 9 reserved for future use
 //
 // ROCK Specific starts here
 //
-static constexpr int X_CAM_TYPE_ROCK_HDMI_IN = 70;
-static constexpr int X_CAM_TYPE_ROCK_IMX219 = 71;
+static constexpr int X_CAM_TYPE_ROCK_HDMI_IN = 80;
+static constexpr int X_CAM_TYPE_ROCK_IMX219 = 81;
 //
 // OpenIPC specific starts here
-static constexpr int X_CAM_TYPE_OPENIPC_SOMETHING=80;
+static constexpr int X_CAM_TYPE_OPENIPC_SOMETHING=90;
 //
 
 // ... rest is reserved for future use
@@ -78,14 +83,16 @@ static std::string x_cam_type_to_string(int camera_type) {
   switch (camera_type) {
     case X_CAM_TYPE_DUMMY_SW:
       return "DUMMY";
-    case X_CAM_TYPE_USB:
-      return "USB";
     case X_CAM_TYPE_EXTERNAL:
       return "EXTERNAL";
     case X_CAM_TYPE_EXTERNAL_IP:
       return "EXTERNAL_IP";
     case X_CAM_TYPE_DEVELOPMENT_FILESRC:
       return "DEV_FILESRC";
+    case X_CAM_TYPE_USB_GENERIC:
+      return "USB";
+    case X_CAM_TYPE_USB_INFIRAY:
+      return "INFIRAY";
     // All the rpi stuff begin
     case X_CAM_TYPE_RPI_MMAL_HDMI_TO_CSI:
       return "MMAL_HDMI";
@@ -151,16 +158,16 @@ struct XCamera {
     return camera_type == X_CAM_TYPE_RPI_MMAL_HDMI_TO_CSI;
   }
   bool requires_rpi_libcamera_pipeline() const {
-    return camera_type >= 20 && camera_type < 50;
+    return camera_type >= 30 && camera_type < 60;
   }
   bool requires_rpi_veye_pipeline() const {
-    return camera_type >= 50 && camera_type < 60;
-  }
-  bool requires_x20_cedar_pipeline() const {
     return camera_type >= 60 && camera_type < 70;
   }
-  bool requires_rockchip_mpp_pipeline() const {
+  bool requires_x20_cedar_pipeline() const {
     return camera_type >= 70 && camera_type < 80;
+  }
+  bool requires_rockchip_mpp_pipeline() const {
+    return camera_type >= 80 && camera_type < 90;
   }
   std::string cam_type_as_verbose_string() const {
     return x_cam_type_to_string(camera_type);
@@ -178,8 +185,10 @@ struct XCamera {
       return {1280, 720, 60};
     } else if (requires_rpi_libcamera_pipeline()) {
       return {1920, 1080, 30};
-    } else if (camera_type == X_CAM_TYPE_USB) {
-      // TODO properly
+    } else if (camera_type == X_CAM_TYPE_USB_GENERIC) {
+      // We do not know
+      return {640, 480, 30};
+    } else if(camera_type==X_CAM_TYPE_USB_INFIRAY){
       return {640, 480, 30};
     } else if (camera_type == X_CAM_TYPE_DUMMY_SW) {
       return {640, 480, 30};
@@ -199,9 +208,11 @@ struct XCamera {
     } else if (requires_x20_cedar_pipeline()) {
       // also easy, 720p60 only (for now)
       return {ResolutionFramerate{1280, 720, 60}};
-    } else if (camera_type == X_CAM_TYPE_USB) {  // TODO properly
+    } else if (camera_type == X_CAM_TYPE_USB_INFIRAY) {
       return {ResolutionFramerate{640, 480, 30}};
-    } else if (requires_rpi_libcamera_pipeline()) {  // TODO properly
+    } else if(camera_type==X_CAM_TYPE_USB_GENERIC){
+      return {ResolutionFramerate{640, 480, 30}};
+    } else if (requires_rpi_libcamera_pipeline()) {
       std::vector<ResolutionFramerate> ret;
       ret.push_back(ResolutionFramerate{1920, 1080, 30});
       ret.push_back(ResolutionFramerate{1280, 720, 60});
@@ -218,7 +229,7 @@ struct XCamera {
     return {ResolutionFramerate{1920, 1080, 30}};
   }
   bool is_camera_type_usb_infiray() const {
-    return camera_type == X_CAM_TYPE_USB;  // TODO how figure out if infiray
+    return camera_type == X_CAM_TYPE_USB_INFIRAY;
   };
 };
 
@@ -227,8 +238,11 @@ static bool is_valid_primary_cam_type(int cam_type) {
   return false;
 }
 static bool is_valid_secondary_cam_type(int cam_type) {
-  if (cam_type == X_CAM_TYPE_DUMMY_SW || cam_type == X_CAM_TYPE_USB ||
-      cam_type == X_CAM_TYPE_EXTERNAL || cam_type == X_CAM_TYPE_EXTERNAL_IP ||
+  if (cam_type == X_CAM_TYPE_DUMMY_SW ||
+      cam_type == X_CAM_TYPE_USB_INFIRAY ||
+      cam_type==X_CAM_TYPE_USB_GENERIC ||
+      cam_type == X_CAM_TYPE_EXTERNAL ||
+      cam_type == X_CAM_TYPE_EXTERNAL_IP ||
       cam_type == X_CAM_TYPE_DISABLED) {
     return true;
   }
