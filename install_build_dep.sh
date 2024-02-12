4248547b475acf541d2f651cce3b1bfb4d1b063b
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-12 23:48:33 +0100

    fix build error

--- install_build_dep.sh
+++ install_build_dep.sh
@@ -26,7 +26,7 @@ PLATFORM_PACKAGES_REMOVE=""
  apt install -y curl
  curl -1sLf 'https://dl.cloudsmith.io/public/openhd/release/setup.deb.sh'| sudo -E bash
  apt update
- apt upgrade -y
+ apt upgrade -y --allow-downgrades
 
 # Main function
  
