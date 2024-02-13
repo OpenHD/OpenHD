72be37ee5fbcda48dd5c495a04e6fb6c6c2aeb30
consti10 <geierconstantinabc@gmail.com>
2024-02-13 23:16:53 +0100

    update mavlink, use uint8_t values for version (instead of string)

--- OpenHD/ohd_common/inc/openhd_global_constants.hpp
+++ OpenHD/ohd_common/inc/openhd_global_constants.hpp
@@ -45,7 +45,7 @@ static std::string ohd_version_as_string(uint8_t major,uint8_t minor,uint8_t pat
   }else if(release_type==2){
     ss<<"-alpha";
   }else{
-    ss<<"unknwon";
+    ss<<"-unknown";
   }
   return ss.str();
 }
