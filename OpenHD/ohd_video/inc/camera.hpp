8b1215f7e68726ee4321ca16d76e6a90169c6975
consti10 <geierconstantinabc@gmail.com>
2024-02-14 22:05:05 +0100

    defaults for filesrc camera

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -222,11 +222,13 @@ struct XCamera {
       ret.push_back(ResolutionFramerate{640,480,30});
       ret.push_back(ResolutionFramerate{1280,720,30});
       ret.push_back(ResolutionFramerate{1280,720,60});
+      return ret;
     }else if(camera_type==X_CAM_TYPE_DEVELOPMENT_FILESRC){
       std::vector<ResolutionFramerate> ret;
       ret.push_back(ResolutionFramerate{848,480,60});
       ret.push_back(ResolutionFramerate{1280,720,60});
       ret.push_back(ResolutionFramerate{1920,1080,60});
+      return ret;
     }
     // Not mapped yet
     // return something that might work or might not work
