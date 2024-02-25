7a5e187b1aa69c90922a3d968e7b082ec7087067
Consti10 <geierconstantinabc@gmail.com>
2024-02-25 19:21:40 +0100

    Squashed ci stuff (#1010)
    * quick packaging fix ?!
    
    * Update package.sh
    
    * fix lunar builds
    
    * typo
    
    * Update package.sh
    
    * Update package.sh
    
    * Update package.sh
    
    ---------
    
    Co-authored-by: Raphael <68374617+raphaelscholle@users.noreply.github.com>

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-# set -euo pipefail
+#!/bin/bash
 
 CUSTOM="${1}"
 PACKAGE_ARCH="${2}"
@@ -8,20 +8,18 @@ PKGDIR="/tmp/openhd-installdir"
 VERSION="2.5.2-beta-$(date '+%Y%m%d%H%M')-$(git rev-parse --short HEAD)"
 
 create_package_directory() {
-  rm -rf "${PKGDIR}"
-  mkdir -p "${PKGDIR}"/{usr/local/bin,tmp,settings,etc/systemd/system}
-  # rpi only - copy the camera config files
-  if [[ "${OS}" == "raspbian" ]] && [[ "${PACKAGE_ARCH}" == "armhf" ]]; then
-    echo "________"
-    echo "building for raspberry"
-    echo "________"
-  elif [[ "${OS}" == "debian" ]] && [[ "${PACKAGE_ARCH}" == "arm64" ]]; then
-    echo "building for rock"
-  fi
+  rm -rf /tmp/openhd-installdir
+  mkdir -p /tmp/openhd-installdir/usr/local/bin
+  mkdir -p /tmp/openhd-installdir/tmp
+  #Nobody should create a topdir for such things like settings, but for now it'll stay
+  mkdir -p /tmp/openhd-installdir/settings
+  mkdir -p /tmp/openhd-installdir/etc/systemd/system
+
   # We do not copy the openhd service for x86, since there we have launcher on the desktop
   # (Otherwise, we always copy it)
   if [[ "${PACKAGE_ARCH}" != "x86_64" ]]; then
-    cp systemd/openhd.service "${PKGDIR}/etc/systemd/system/openhd.service" || exit 1
+    echo "we're not on x86"
+    cp systemd/openhd.service /tmp/openhd-installdir/etc/systemd/system/openhd.service || exit 1
   fi
   # always - copy the hardware.config file
   mkdir -p "${PKGDIR}/boot/openhd/"
@@ -29,40 +27,38 @@ create_package_directory() {
 }
 
 build_package() {
-  
+
   if [[ "${PACKAGE_ARCH}" == "armhf" ]]; then
-      if [[ "${CUSTOM}" == "standard" ]]; then
-      echo "this is cake"
+    if [[ "${CUSTOM}" == "standard" ]]; then
       PACKAGE_NAME="openhd"
       PACKAGES="-d libcamera-openhd -d gst-openhd-plugins -d iw -d nmap -d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
       PLATFORM_CONFIGS=""
-      else
-      echo "this isn't cake"
+    else
       PACKAGE_NAME="openhd-x20"
       PACKAGES="-d iw -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-tools"
       PLATFORM_CONFIGS=""
-      fi
+    fi
   else
-    echo "this isn't cake and also not pi"
     PACKAGE_NAME="openhd"
     PACKAGES="-d iw -d nmap -d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
     PLATFORM_CONFIGS=""
   fi
-  
+
   rm "${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb" > /dev/null 2>&1 || true
-  
+
   cmake OpenHD/
   make -j4
   cp openhd ${PKGDIR}/usr/local/bin/openhd
 
+  # Assuming fpm is installed and properly configured
   fpm -a "${PACKAGE_ARCH}" -s dir -t deb -n "${PACKAGE_NAME}" -v "${VERSION}" -C "${PKGDIR}" \
     ${PLATFORM_CONFIGS} \
     -p "${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb" \
     --after-install after-install.sh \
     --before-install before-install.sh \
-    ${PACKAGES} 
+    ${PACKAGES}
 }
 
-  #Main Build
-  create_package_directory
-  build_package
+#Main Build
+create_package_directory
+build_package
