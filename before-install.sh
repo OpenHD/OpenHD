346b0c6d1d053dfcef63374f3f5740e2e5f41825
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-03 19:50:06 +0100

    poc

--- before-install.sh
+++ before-install.sh
@@ -4,3 +4,7 @@
 mv /boot/openhd/scripts/custom_unmanaged_camera.sh /boot/openhd/scripts/custom_unmanaged_camera_old.sh
 # NOTE: Updating overwrites the .config file and also the service file
 rm -rf /boot/openhd/hardware.config
+
+if [ "$(uname -m)" = "x86_64" ]; then
+    whiptail --title "Confirmation" --yesno "Is it all right?" 10 50 && echo "Writing to /boot/yes.conf" > /boot/yes.conf || echo "Writing to /boot/no.conf" > /boot/no.conf
+fi
