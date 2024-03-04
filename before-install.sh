e66a54a0f3c373571d563b237d3f69dc39965785
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-04 21:38:58 +0100

    Update before-install.sh

--- before-install.sh
+++ before-install.sh
@@ -16,10 +16,10 @@ if [ "$(uname -m)" == "x86_64" ]; then
             if [ $? -eq 0 ]; then
                 if whiptail --title "Confirmation" --yesno "This action will remove old drivers. Do you want to proceed?" 10 50; then
                     echo "Removing old drivers"
-                    sudo dkms uninstall -m rtl8812au -v 5.2.20.2 --all
-                    sudo dkms remove -m rtl8812au -v 5.2.20.2 --all
-                    sudo dkms uninstall -m rtl88x2bu -v 5.13.1 --all
-                    sudo dkms remove -m rtl88x2bu -v 5.13.1 --all
+                    sudo dkms uninstall -m rtl8812au -v 5.2.20.2 --all || true
+                    sudo dkms remove -m rtl8812au -v 5.2.20.2 --all || true
+                    sudo dkms uninstall -m rtl88x2bu -v 5.13.1 --all || true
+                    sudo dkms remove -m rtl88x2bu -v 5.13.1 --all || true
                 else
                     echo "No drivers were removed!"
                 fi
