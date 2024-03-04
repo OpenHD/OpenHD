c7c55c581382dd92cdcaf3d67428571411c69452
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-04 20:33:53 +0100

    improve failing

--- before-install.sh
+++ before-install.sh
@@ -7,19 +7,23 @@ rm -rf /boot/openhd/hardware.config
 
 if [ "$(uname -m)" == "x86_64" ]; then
     if ! uname -a | grep -q "azure"; then
-        whiptail --title "OpenHD" --yesno "You are about to install OpenHD to your Computer. Please be aware that we do not allow military usage! Do you want to continue ?" 10 50
-        
-        if [ $? -eq 0 ]; then
-                whiptail --title "OpenHD" --yesno "Do you want to remove the old drivers ?" 10 50
-            if [ $? -eq 0 ]; then
-                echo "Removing old drivers"
-                sudo dkms uninstall -m rtl8812au -v 5.2.20.2 --all
-                sudo dkms remove -m rtl8812au -v 5.2.20.2 --all
-                sudo dkms uninstall -m rtl88x2bu -v 5.13.1 --all
-                sudo dkms remove -m rtl88x2bu -v 5.13.1 --all
-            fi
-        else
+        whiptail --title "OpenHD" --yesno "You are about to install OpenHD to your Computer. Please be aware that we do not allow military usage! Do you want to continue?" 10 50
+        if ! [ $? -eq 0 ]; then
+            echo "Operation cancelled."
             exit 1
+        else
+            whiptail --title "OpenHD" --yesno "Do you want to remove the old drivers?" 10 50
+            if [ $? -eq 0 ]; then
+                if whiptail --title "Confirmation" --yesno "This action will remove old drivers. Do you want to proceed?" 10 50; then
+                    echo "Removing old drivers"
+                    sudo dkms uninstall -m rtl8812au -v 5.2.20.2 --all
+                    sudo dkms remove -m rtl8812au -v 5.2.20.2 --all
+                    sudo dkms uninstall -m rtl88x2bu -v 5.13.1 --all
+                    sudo dkms remove -m rtl88x2bu -v 5.13.1 --all
+                else
+                    echo "No drivers were removed!"
+                fi
+            fi
         fi
     fi
 fi
