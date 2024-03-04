5487fec558fd33f79eaad7fb8a7a958002e59e76
Consti10 <geierconstantinabc@gmail.com>
2024-03-04 13:47:26 +0100

    led stuff

--- OpenHD/ohd_common/test/test_led.cpp
+++ OpenHD/ohd_common/test/test_led.cpp
@@ -5,20 +5,19 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
-#include <iostream>
 
 #include "openhd_led.h"
 #include "openhd_util.h"
 
 int main(int argc, char *argv[]) {
-  while (true){
-    std::cout<<"Set LEDs off"<<std::endl;
+  while (true) {
+    std::cout << "Set LEDs off" << std::endl;
     openhd::LEDManager::instance().set_red_led_status(
         openhd::LEDManager::STATUS_OFF);
     openhd::LEDManager::instance().set_green_led_status(
         openhd::LEDManager::STATUS_OFF);
     std::this_thread::sleep_for(std::chrono::seconds(3));
-    std::cout<<"Set LEDs ON"<<std::endl;
+    std::cout << "Set LEDs ON" << std::endl;
     openhd::LEDManager::instance().set_red_led_status(
         openhd::LEDManager::STATUS_ON);
     openhd::LEDManager::instance().set_green_led_status(
