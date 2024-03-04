9fc27364c492346527f07750a7e345f00c353196
Consti10 <geierconstantinabc@gmail.com>
2024-03-04 16:47:11 +0100

    exp mmal

--- OpenHD/ohd_video/inc/rpi_hdmi_to_csi_v4l2_helper.h
+++ OpenHD/ohd_video/inc/rpi_hdmi_to_csi_v4l2_helper.h
@@ -14,6 +14,8 @@ static void initialize_resolution(const int width, const int height, int fps) {
   openhd::log::get_default()->debug("rpi hdmi initialize_resolution {} {} {}",
                                     width, height, fps);
   // https://forums.raspberrypi.com/viewtopic.php?f=38&t=281972
+  // TODO: Create a script that (Installs ?) the correct timings (file)
+  OHDUtil::run_command(fmt::format("bash /usr/local/bin/ohd_rpi_hdmi_v4l2_setup.sh {} {} {}", width,height,fps),{});
   // to select the currently detected timings
   // v4l2-ctl --set-dv-bt-timings query
   OHDUtil::run_command("v4l2-ctl --set-dv-bt-timings query", {});
