f572d6132f77a8e0c11528d6180b25cde181d7c5
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-16 04:52:41 +0100

    testing hotspot card

--- OpenHD/ohd_interface/src/wifi_card_discovery.cpp
+++ OpenHD/ohd_interface/src/wifi_card_discovery.cpp
@@ -39,6 +39,9 @@ static WiFiCardType driver_to_wifi_card_type(const std::string& driver_name) {
   if (OHDUtil::contains_after_uppercase(driver_name, "bcmsdh_sdmmc")) {
     return WiFiCardType::BROADCOM;
   }
+  if (OHDUtil::contains_after_uppercase(driver_name, "aic8800_fdrv")) {
+    return WiFiCardType::BROADCOM;
+  }
   if (OHDUtil::contains_after_uppercase(driver_name, "88xxau")) {
     return WiFiCardType::RTL_88X2AU;
   }
