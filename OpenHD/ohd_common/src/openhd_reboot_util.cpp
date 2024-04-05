27027d3d2123218d2479d95c93b7bc8c72c4453a
Consti10 <geierconstantinabc@gmail.com>
2024-04-05 11:45:35 +0200

    zero3w - use reboot / shutdown commands (systemctl seems to be bugged)

--- OpenHD/ohd_common/src/openhd_reboot_util.cpp
+++ OpenHD/ohd_common/src/openhd_reboot_util.cpp
@@ -6,9 +6,14 @@
 
 #include <thread>
 
+#include "openhd_platform.h"
 #include "openhd_spdlog.h"
 #include "openhd_util.h"
+#include "openhd_util_async.h"
 
+/**
+ * Systemctl is the nicer way, but sometimes has issues.
+ */
 void openhd::reboot::systemctl_shutdown() {
   OHDUtil::run_command("systemctl", {"start", "poweroff.target"}, true);
 }
@@ -17,18 +22,32 @@ void openhd::reboot::systemctl_reboot() {
   OHDUtil::run_command("systemctl", {"start", "reboot.target"}, true);
 }
 
+static void command_reboot() { OHDUtil::run_command("reboot", {}, true); }
+static void command_shutdown() { OHDUtil::run_command("shutdown", {}, true); }
+
 void openhd::reboot::systemctl_power(bool shutdownOnly) {
   if (shutdownOnly) {
-    systemctl_shutdown();
+    // Zero3w seems to be bugged
+    if (OHDPlatform::instance().platform_type ==
+        X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA_ZERO3W) {
+      command_shutdown();
+    } else {
+      systemctl_shutdown();
+    }
   } else {
-    systemctl_reboot();
+    if (OHDPlatform::instance().platform_type ==
+        X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA_ZERO3W) {
+      command_reboot();
+    } else {
+      systemctl_reboot();
+    }
   }
 }
 
 void openhd::reboot::handle_power_command_async(std::chrono::milliseconds delay,
                                                 bool shutdownOnly) {
-  // This is okay, since we will restart anyways
-  static auto handle = std::thread([delay, shutdownOnly] {
+  const std::string tag = shutdownOnly ? "SHUTDOWN" : "REBOOT";
+  AsyncHandle::instance().execute_async(tag, [delay, shutdownOnly] {
     std::this_thread::sleep_for(delay);
     systemctl_power(shutdownOnly);
   });
