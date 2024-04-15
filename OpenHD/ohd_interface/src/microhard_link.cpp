c1d542c4ce11f5764958f95122997d7fc013e48d
Consti10 <geierconstantinabc@gmail.com>
2024-04-15 17:20:36 +0200

    update IPs

--- OpenHD/ohd_interface/src/microhard_link.cpp
+++ OpenHD/ohd_interface/src/microhard_link.cpp
@@ -12,7 +12,7 @@ static constexpr auto MICROHARD_AIR_IP = "192.168.168.11";
 static constexpr auto MICROHARD_GND_IP = "192.168.168.12";
 // The assigned IPs
 // NOTE: They have to be set correctly !
-static constexpr auto DEVICE_IP_GND = "192.168.168.148";
+static constexpr auto DEVICE_IP_GND = "192.168.168.122";
 static constexpr auto DEVICE_IP_AIR = "192.168.168.153";
 
 // We send data over those port(s)
