089a0c7c8ee29c8f58ea89a17b2b4977a9f74363
Consti10 <geierconstantinabc@gmail.com>
2024-04-27 14:47:19 +0200

    increase thermal limits to 75 / 79 degree

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -1610,8 +1610,8 @@ void WBLink::wt_perform_update_thermal_protection() {
     m_thermal_protection_level = THERMAL_PROTECTION_NONE;
     return;
   }
-  static auto THERMAL_LIMIT_VIDEO_REDUCED = 72;
-  static auto THERMAL_LIMIT_VIDEO_OFF = 75;
+  static auto THERMAL_LIMIT_VIDEO_REDUCED = 75;
+  static auto THERMAL_LIMIT_VIDEO_OFF = 79;
   uint8_t new_thermal_protection_level;
   if (temp >= THERMAL_LIMIT_VIDEO_OFF) {
     // >=X degree, disable video
