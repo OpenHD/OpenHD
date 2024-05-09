3577eaac3b2323a0ba9c3a6f300a35eba62e46a3
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-10 00:17:43 +0200

    Update OnboardComputerStatusProvider.cpp

--- OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
+++ OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
@@ -127,7 +127,7 @@ void OnboardComputerStatusProvider::calculate_other_until_terminate() {
     }
     else if (OHDFilesystemUtil::exists("/sys/class/power_supply/BAT1/capacity")) {
     curr_ina219_voltage = read_battery_percentage_linux();
-    curr_ina219_current = read_battery_charging_linux();
+    curr_ina219_current = read_battery_charging_linux()*100;
     }
 
     if (OHDPlatform::instance().is_rpi()) {
