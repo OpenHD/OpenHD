369fe7f486cfe87aa8d15e63bbfdd57375e50bbc
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-12 20:23:51 +0200

    Revert "Update openhd_platform.cpp"
    This reverts commit 5a9f35b25e2e3d37256eca2324ca66f14ad57b0e.

--- OpenHD/ohd_common/src/openhd_platform.cpp
+++ OpenHD/ohd_common/src/openhd_platform.cpp
@@ -120,11 +120,11 @@ std::string x_platform_type_to_string(int platform_type) {
       return "RPI 5";
     // RPI END
     case X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA:
-      return "RADXA RK3566";
+      return "RADXA ZERO3W";
     case X_PLATFORM_TYPE_ROCKCHIP_RK3588_RADXA_ROCK5_A:
-      return "RADXA RK3588S";
+      return "RADXA ROCK5 A";
     case X_PLATFORM_TYPE_ROCKCHIP_RK3588_RADXA_ROCK5_B:
-      return "RADXA RK3588";
+      return "RADXA ROCK5 B";
     case X_PLATFORM_TYPE_ROCKCHIP_RV1126_UNDEFINED:
       return "RV1126 UNDEFINED";
     // ROCK END
