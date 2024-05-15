8fa2aff88e2b6bcda4f1d2d7759adcf7fedc1fbd
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-15 18:13:33 +0200

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -2,7 +2,7 @@
 # This file is the install instruction for the CHROOT build
 # We're using cloudsmith-cli to upload the file in CHROOT
 
-sudo apt install -y python3-pip tree
+sudo apt install -y python3-pip tree git
 sudo pip3 install --upgrade cloudsmith-cli
 ls -a
 API_KEY=$(cat cloudsmith_api_key.txt)
