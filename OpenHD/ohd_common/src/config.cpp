206ce6a0ef3ce1218c92b866fe6ca0521f3a005b
Raphael Scholle <raphael@openhdfpv.org>
2024-07-15 04:19:19 +0200

    testing switchcase -- works :)

--- OpenHD/ohd_common/src/config.cpp
+++ OpenHD/ohd_common/src/config.cpp
@@ -3,5 +3,15 @@
 //
 #include "config.h"
 
+
+#ifdef X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA_ZERO3W
+const char* CONFIG_BASE_PATH = "/config/openhd/";
+#else
 const char* CONFIG_BASE_PATH = "/boot/openhd/";
+#endif
+
+#ifdef X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA_ZERO3W
 const char* VIDEO_PATH = "/home/openhd/Videos/";
+#else
+const char* VIDEO_PATH = "/home/openhd/Videos/";
+#endif
