17547922cc7a3372f645521b8c60839af2316d98
Raphael Scholle <raphael@openhdfpv.org>
2024-07-15 13:56:56 +0200

    add config partition for all platforms

--- OpenHD/ohd_common/src/config.cpp
+++ OpenHD/ohd_common/src/config.cpp
@@ -5,6 +5,14 @@
 
 #ifdef X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA_ZERO3W
 const char* CONFIG_BASE_PATH = "/config/openhd/";
+#elif X_PLATFORM_TYPE_ROCKCHIP_RK3588_RADXA_ROCK5_A
+const char* CONFIG_BASE_PATH = "/config/openhd/";
+#elif X_PLATFORM_TYPE_ROCKCHIP_RK3588_RADXA_ROCK5_B
+const char* CONFIG_BASE_PATH = "/config/openhd/";
+#elif X_PLATFORM_TYPE_ROCKCHIP_RK3566_RADXA_CM3
+const char* CONFIG_BASE_PATH = "/config/openhd/";
+#elif X_PLATFORM_TYPE_ALWINNER_X20
+const char* CONFIG_BASE_PATH = "/external/openhd/";
 #else
 const char* CONFIG_BASE_PATH = "/boot/openhd/";
 #endif
