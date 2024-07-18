fe1761914615296071ec1c4ee37ab75a7962910a
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-18 18:52:25 +0200

    Update config.cpp

--- OpenHD/ohd_common/src/config.cpp
+++ OpenHD/ohd_common/src/config.cpp
@@ -19,7 +19,7 @@ static char* VIDEO_PATH = nullptr;
 const char* getConfigBasePath() {
     const auto platform_debug = OHDPlatform::instance();
     std::cerr << "Setting paths for rock platform: " << platform_debug.to_string() << std::endl;
-    return CONFIG_BASE_PATH ? CONFIG_BASE_PATH : "/boot/openhd/";
+    return CONFIG_BASE_PATH ? CONFIG_BASE_PATH : "/config/openhd/";
 }
 
 const char* getVideoPath() {
