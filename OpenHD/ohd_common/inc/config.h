af626d4d4791eeb89013dd7ce6086fef634f4aee
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-18 17:22:26 +0200

    fml

--- OpenHD/ohd_common/inc/config.h
+++ OpenHD/ohd_common/inc/config.h
@@ -3,11 +3,10 @@
 // Made to hold several variables that can be used within OpenHD
 //
 
-// config.h
 #ifndef CONFIG_H
 #define CONFIG_H
 
-extern char* CONFIG_BASE_PATH = "/home/openhd/Videos/";
-extern char* VIDEO_PATH = "/home/openhd/Videos/";
+extern char* CONFIG_BASE_PATH;
+extern char* VIDEO_PATH;
 
 #endif  // CONFIG_H
--- OpenHD/ohd_common/src/config.cpp
+++ OpenHD/ohd_common/src/config.cpp
@@ -1,3 +1,7 @@
 //
 // Created by Raphael on 7/15/24.
 //
+#include "config.h"
+
+char* CONFIG_BASE_PATH = "/home/openhd/Videos/";
+char* VIDEO_PATH = "/home/openhd/Videos/";
