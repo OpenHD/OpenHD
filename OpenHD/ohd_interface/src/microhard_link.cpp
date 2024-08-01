5b94747e44ca790a730abbf1a86284dff3d7b4e7
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-01 22:37:07 +0200

    Update microhard_link.cpp

--- OpenHD/ohd_interface/src/microhard_link.cpp
+++ OpenHD/ohd_interface/src/microhard_link.cpp
@@ -22,13 +22,13 @@
 // Macro to log function entry
 #define LOG_FUNCTION_ENTRY() openhd::log::get_default()->warn("Entering function: {}", __FUNCTION__)
 
-// Master
+// Constants
 static constexpr auto MICROHARD_AIR_IP = "192.168.168.11";
-// Client
 static constexpr auto MICROHARD_GND_IP = "192.168.168.12";
-// The assigned IPs
-// NOTE: They have to be set correctly!
 static constexpr auto DEVICE_IP_AIR = "192.168.168.153";
+static constexpr int MICROHARD_UDP_PORT_VIDEO_AIR_TX = 5910;
+static constexpr int MICROHARD_UDP_PORT_TELEMETRY_AIR_TX = 5920;
+static const std::string DEFAULT_DEVICE_IP_GND = "192.168.168.122";
 
 // Helper function to retrieve IP addresses starting with a specific prefix
 std::vector<std::string> get_ip_addresses(const std::string& prefix) {
@@ -64,7 +64,7 @@ std::vector<std::string> get_ip_addresses(const std::string& prefix) {
             perror("ioctl");
             continue;
         }
-        struct sockaddr_in* addr = (struct sockaddr_in*)&ifr.ifr_addr;
+        auto* addr = (struct sockaddr_in*)&ifr.ifr_addr;
         std::string ip = inet_ntoa(addr->sin_addr);
         if (ip.find(prefix) == 0) {
             ip_addresses.push_back(ip);
@@ -77,10 +77,7 @@ std::vector<std::string> get_ip_addresses(const std::string& prefix) {
 
 std::string get_gateway_ip() {
     openhd::log::get_default()->warn("Getting gateway IP...");
-
-    // Command to get the default gateway IP and filter it
     std::string cmd = "ip route show default | awk '/default/ {print $3}' | grep '^192\\.168\\.168'";
-
     FILE* pipe = popen(cmd.c_str(), "r");
     if (!pipe) {
         openhd::log::get_default()->warn("Failed to run command: {}", cmd.c_str());
@@ -99,11 +96,47 @@ std::string get_gateway_ip() {
         result.erase(result.find_last_not_of("\n\r") + 1);
     }
 
-    // Log the result
     openhd::log::get_default()->warn("Filtered Gateway IP: {}", result.c_str());
-
     return result;
 }
+
+bool check_ip_alive(const std::string& ip, int port = 23) {
+    LOG_FUNCTION_ENTRY();
+    openhd::log::get_default()->warn("Checking if IP {} is alive on port {}", ip, port);
+    
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        openhd::log::get_default()->warn("Socket creation failed for IP {}", ip);
+        return false;
+    }
+
+    struct sockaddr_in addr = {AF_INET, htons(port), inet_addr(ip.c_str())};
+    bool connected = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == 0;
+    close(sockfd);
+
+    if (connected) {
+        openhd::log::get_default()->warn("IP {} is alive", ip);
+    } else {
+        openhd::log::get_default()->warn("IP {} is not alive", ip);
+    }
+
+    return connected;
+}
+
+std::string find_device_ip_gnd() {
+    LOG_FUNCTION_ENTRY();
+    auto ip_addresses = get_ip_addresses("192.168.168");
+    for (const auto& ip : ip_addresses) {
+        if (ip != MICROHARD_AIR_IP && ip != MICROHARD_GND_IP) {
+            return ip;
+        }
+    }
+    openhd::log::get_default()->warn("No suitable IP address found for DEVICE_IP_GND. Using default.");
+    return DEFAULT_DEVICE_IP_GND;
+}
+
+// The assigned IP
+static const std::string DEVICE_IP_GND = find_device_ip_gnd();
 
 void log_ip_addresses() {
     LOG_FUNCTION_ENTRY();
@@ -123,7 +156,6 @@ std::string get_detected_ip_address() {
     LOG_FUNCTION_ENTRY();
     auto ip_addresses = get_ip_addresses("192.168.168");
     if (!ip_addresses.empty()) {
-        // Return the first detected IP address
         return ip_addresses.front();
     } else {
         openhd::log::get_default()->warn("No IP addresses starting with 192.168.168 found.");
@@ -131,64 +163,17 @@ std::string get_detected_ip_address() {
     }
 }
 
-std::string find_device_ip_gnd() {
+static void wait_for_microhard_module(bool is_air) {
     LOG_FUNCTION_ENTRY();
-    auto ip_addresses = get_ip_addresses("192.168.168");
-    for (const auto& ip : ip_addresses) {
-        if (ip != MICROHARD_AIR_IP && ip != MICROHARD_GND_IP) {
-            return ip;
-        }
-    }
-    openhd::log::get_default()->warn("No suitable IP address found for DEVICE_IP_GND. Using default.");
-    return "192.168.168.122";
-}
-
-// The assigned IP
-static const std::string DEVICE_IP_GND = find_device_ip_gnd();
-
-// We send data over those port(s)
-static constexpr int MICROHARD_UDP_PORT_VIDEO_AIR_TX = 5910;
-static constexpr int MICROHARD_UDP_PORT_TELEMETRY_AIR_TX = 5920;
-
-static bool check_ip_alive(const std::string &ip, int port = 23) {
-    LOG_FUNCTION_ENTRY();
-    std::string gateway_ip = get_gateway_ip();
-    openhd::log::get_default()->warn("Checking if IP {} is alive on port {}", gateway_ip, port);
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        openhd::log::get_default()->warn("Socket creation failed for IP {}", gateway_ip);
-        return false;
-    }
-    
-    struct sockaddr_in addr = {AF_INET, htons(port), inet_addr(gateway_ip.c_str())};
-    bool connected = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == 0;
-    close(sockfd);
-    
-    if (connected) {
-        openhd::log::get_default()->warn("IP {} is alive", gateway_ip);
-    } else {
-        openhd::log::get_default()->warn("IP {} is not alive", gateway_ip);
-    }
-    
-    return connected;
-}
-
-
-static void wait_for_microhard_module(bool air) {
-    LOG_FUNCTION_ENTRY();
-
-    // Get the detected IP address
     const std::string microhard_device_ip = get_detected_ip_address();
 
-    // Check if the IP address was successfully retrieved
     if (microhard_device_ip.empty()) {
         openhd::log::get_default()->warn("No microhard device IP address detected. Exiting.");
         return;
     }
 
     while (true) {
-        auto available = check_ip_alive(microhard_device_ip);
-        if (available) {
+        if (check_ip_alive(microhard_device_ip)) {
             openhd::log::get_default()->debug("Microhard module found at {}", microhard_device_ip);
             break;
         }
@@ -198,52 +183,56 @@ static void wait_for_microhard_module(bool air) {
 
 MicrohardLink::MicrohardLink(OHDProfile profile) : m_profile(profile) {
     LOG_FUNCTION_ENTRY();
-    // Wait for the module to become available
     wait_for_microhard_module(m_profile.is_air);
 
     if (m_profile.is_air) {
-        // We send video
         m_video_tx = std::make_unique<openhd::UDPForwarder>(DEVICE_IP_GND, MICROHARD_UDP_PORT_VIDEO_AIR_TX);
-        // We send and receive telemetry
-        auto cb_telemetry_rx = [this](const uint8_t *data, const std::size_t data_len) {
+        auto cb_telemetry_rx = [this](const uint8_t* data, std::size_t data_len) {
             auto shared = std::make_shared<std::vector<uint8_t>>(data, data + data_len);
             on_receive_telemetry_data(shared);
         };
         m_telemetry_tx_rx = std::make_unique<openhd::UDPReceiver>(DEVICE_IP_AIR, MICROHARD_UDP_PORT_TELEMETRY_AIR_TX, cb_telemetry_rx);
-        m_telemetry_tx_rx->runInBackground();
     } else {
-        auto cb_video_rx = [this](const uint8_t *payload, const std::size_t payloadSize) {
+        auto cb_video_rx = [this](const uint8_t* payload, std::size_t payloadSize) {
             on_receive_video_data(0, payload, payloadSize);
         };
         m_video_rx = std::make_unique<openhd::UDPReceiver>(DEVICE_IP_GND, MICROHARD_UDP_PORT_VIDEO_AIR_TX, cb_video_rx);
-        m_video_rx->runInBackground();
-        auto cb_telemetry_rx = [this](const uint8_t *data, const std::size_t data_len) {
+
+        auto cb_telemetry_rx = [this](const uint8_t* data, std::size_t data_len) {
             auto shared = std::make_shared<std::vector<uint8_t>>(data, data + data_len);
             on_receive_telemetry_data(shared);
         };
         m_telemetry_tx_rx = std::make_unique<openhd::UDPReceiver>(DEVICE_IP_GND, MICROHARD_UDP_PORT_TELEMETRY_AIR_TX, cb_telemetry_rx);
+    }
+
+    if (m_telemetry_tx_rx) {
         m_telemetry_tx_rx->runInBackground();
+    }
+    if (m_video_tx) {
+        m_video_tx->runInBackground();
+    }
+    if (m_video_rx) {
+        m_video_rx->runInBackground();
     }
 }
 
 void MicrohardLink::transmit_telemetry_data(OHDLink::TelemetryTxPacket packet) {
     LOG_FUNCTION_ENTRY();
     const auto destination_ip = m_profile.is_air ? DEVICE_IP_GND : DEVICE_IP_AIR;
-    m_telemetry_tx_rx->forwardPacketViaUDP(
-        destination_ip, MICROHARD_UDP_PORT_TELEMETRY_AIR_TX, packet.data->data(), packet.data->size());
+    m_telemetry_tx_rx->forwardPacketViaUDP(destination_ip, MICROHARD_UDP_PORT_TELEMETRY_AIR_TX, packet.data->data(), packet.data->size());
 }
 
-void MicrohardLink::transmit_video_data(int stream_index, const openhd::FragmentedVideoFrame &fragmented_video_frame) {
+void MicrohardLink::transmit_video_data(int stream_index, const openhd::FragmentedVideoFrame& fragmented_video_frame) {
     LOG_FUNCTION_ENTRY();
     assert(m_profile.is_air);
     if (stream_index == 0) {
-        for (auto &fragment : fragmented_video_frame.rtp_fragments) {
+        for (const auto& fragment : fragmented_video_frame.rtp_fragments) {
             m_video_tx->forwardPacketViaUDP(fragment->data(), fragment->size());
         }
     }
 }
 
-void MicrohardLink::transmit_audio_data(const openhd::AudioPacket &audio_packet) {
+void MicrohardLink::transmit_audio_data(const openhd::AudioPacket& audio_packet) {
     LOG_FUNCTION_ENTRY();
     // not implemented
 }
@@ -251,8 +240,8 @@ void MicrohardLink::transmit_audio_data(const openhd::AudioPacket &audio_packet)
 std::vector<openhd::Setting> MicrohardLink::get_all_settings() {
     LOG_FUNCTION_ENTRY();
     using namespace openhd;
-    std::vector<openhd::Setting> ret{};
-    auto change_dummy = openhd::IntSetting{0, [this](std::string, int value) { return true; }};
-    ret.push_back(Setting{"MICROHARD_DUMMY0", change_dummy});
-    return ret;
+    std::vector<Setting> settings;
+    auto change_dummy = IntSetting{0, [this](std::string, int value) { return true; }};
+    settings.push_back(Setting{"MICROHARD_DUMMY0", change_dummy});
+    return settings;
 }
