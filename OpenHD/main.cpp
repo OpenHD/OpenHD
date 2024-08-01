f11383bd96254111127967fa1fb08e23c53bdf52
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-01 19:40:52 +0200

    Update main.cpp

--- OpenHD/main.cpp
+++ OpenHD/main.cpp
@@ -220,7 +220,7 @@ int main(int argc, char *argv[]) {
   openhd::LEDManager::instance().set_status_loading();
   // Generate the keys and delete pw if needed
   OHDInterface::generate_keys_from_pw_if_exists_and_delete();
-  ohdInterface.checkMicrohardDeviceStatus();
+  OHDInterface.checkMicrohardDeviceStatus();
 
   // Parse the program arguments
   // This is the console we use inside main, in general different openhd
