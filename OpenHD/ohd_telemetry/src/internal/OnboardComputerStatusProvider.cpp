87545c1bbf1cc133dd0f03c272ad5b9c501cfe75
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-02 02:20:44 +0200

    Update OnboardComputerStatusProvider.cpp

--- OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
+++ OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
@@ -219,7 +219,7 @@ OnboardComputerStatusProvider::get_current_status_as_mavlink_message(
 }
 
 void OnboardComputerStatusProvider::ina219_log_warning_once() {
-  if (!m_ina219_warning_logged && (m_ina_219.voltage() > 0)) {
+  if (!m_ina219_warning_logged && (m_ina_219.voltage() > 1)) {
     openhd::log::get_default()->warn(m_ina_219.voltage());
     openhd::log::get_default()->warn("INA219 detected!");
     m_ina219_warning_logged = true;
