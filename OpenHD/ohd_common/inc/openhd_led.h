e7f6a602518ec58154df07e0883ccb990313664b
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-05 00:21:45 +0200

    Update openhd_led.h

--- OpenHD/ohd_common/inc/openhd_led.h
+++ OpenHD/ohd_common/inc/openhd_led.h
@@ -1,4 +1,3 @@
-//
 // Created by consti10 on 01.02.24.
 //
 
@@ -6,19 +5,22 @@
 #define OPENHD_OPENHD_LED_H
 
 #include <atomic>
+#include <memory>
 #include <thread>
+
 namespace openhd {
 
 /**
  * OpenHD uses 2 leds (green and red) for displaying 'stuff' to the user.
- * Weather those leds exists or not depends on the HW - here we abstract that
+ * Whether those leds exist or not depends on the HW - here we abstract that
  * away.
  */
 class LEDManager {
  public:
   static LEDManager& instance();
   static constexpr int STATUS_OFF = 0;
-  static constexpr auto STATUS_ON = 1;
+  static constexpr int STATUS_ON = 1;
+  
   void set_red_led_status(int status);
   void set_green_led_status(int status);
 
@@ -33,16 +35,17 @@ class LEDManager {
  private:
   explicit LEDManager();
   ~LEDManager();
-  void loop();
+  
+  void start_loading_thread();
+  void stop_loading_thread();
+  void loading_loop();
 
  private:
-  // bool m_run= true;
-  // std::unique_ptr<std::thread> m_manage_thread;
+  std::unique_ptr<std::thread> m_loading_thread;
+  std::atomic<bool> m_running;
   bool m_has_error = false;
 };
 
 }  // namespace openhd
-
-class openhd_led {};
 
 #endif  // OPENHD_OPENHD_LED_H
