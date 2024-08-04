540653ace47631c3048287655af081affbcb5848
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-05 01:34:51 +0200

    Update openhd_led.cpp

--- OpenHD/ohd_common/src/openhd_led.cpp
+++ OpenHD/ohd_common/src/openhd_led.cpp
@@ -199,6 +199,6 @@ void openhd::LEDManager::set_status_error() {
 
 void openhd::LEDManager::set_status_stopped() {
   stop_loading_thread();
-  set_primary_led_status(STATUS_ON);
-  set_secondary_led_status(STATUS_ON);
+  set_primary_led_status(STATUS_OFF);
+  set_secondary_led_status(STATUS_OFF);
 }
