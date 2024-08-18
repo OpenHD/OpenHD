0c072a8163fffa0af488ab4fd4e2aa5686ead4dd
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-18 22:32:58 +0200

    Update wb_link.cpp

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -1132,7 +1132,7 @@ void WBLink::wt_perform_rate_adjustment() {
   // Extra x20 - thermal protection
 if (OHDPlatform::instance().is_x20()) {
     const int factor = !m_is_armed ? 20 : 100;
-    const int lastLevel = 0;
+    int lastLevel = 0;
     const int x20_rate =
         m_thermal_protection_level > 0
             ? m_recommended_video_bitrate_kbits * 30 / 100 * factor / 100
