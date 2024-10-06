bc072c1a242a3f0165007a075e73399c8fa75315
Raphael <scholle@uni-wuppertal.de>
2024-10-06 20:51:06 +0200

    Update openhd.mk

--- Buildroot/openhd/openhd.mk
+++ Buildroot/openhd/openhd.mk
@@ -18,15 +18,21 @@ OPENHD_GIT_SUBMODULES = YES
 # Subdirectory inside the Git repo, if needed (if OpenHD is not in the root)
 OPENHD_SUBDIR = OpenHD
 
-# No need to install to the staging directory, only target
-OPENHD_INSTALL_STAGING = NO
+# Install to both the staging directory and target, for linking and runtime
+OPENHD_INSTALL_STAGING = YES
 OPENHD_INSTALL_TARGET = YES
 
-# Additional configuration options for the CMake build
-OPENHD_CONF_OPTS = -DENABLE_USB_CAMERAS=OFF
-
 # List of dependencies that must be built before OpenHD
-OPENHD_DEPENDENCIES = libsodium gstreamer1 gst1-plugins-base libpcap host-pkgconf
+OPENHD_DEPENDENCIES = poco libsodium gstreamer1 gst1-plugins-base libpcap host-pkgconf
+
+# Additional configuration options for the CMake build
+OPENHD_CONF_OPTS = \
+    -DENABLE_USB_CAMERAS=OFF \
+    -DCMAKE_TOOLCHAIN_FILE=$(BR2_TOOLCHAIN_FILE) \
+    -DCMAKE_SYSROOT=$(STAGING_DIR) \
+    -DCMAKE_PREFIX_PATH=$(STAGING_DIR)/usr \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr
 
 # Use Buildroot's CMake package infrastructure to handle the build
 $(eval $(cmake-package))
