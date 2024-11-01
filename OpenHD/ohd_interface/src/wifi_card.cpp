a557582c1bfc421d8092b61b25698a3f64a97723
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-01 23:26:51 +0100

    Update wifi_card.cpp

--- OpenHD/ohd_interface/src/wifi_card.cpp
+++ OpenHD/ohd_interface/src/wifi_card.cpp
@@ -11,6 +11,7 @@ NLOHMANN_JSON_SERIALIZE_ENUM(
                       {WiFiCardType::OPENHD_RTL_88X2AU, "OPENHD_RTL_88X2AU"},
                       {WiFiCardType::OPENHD_RTL_88X2BU, "OPENHD_RTL_88X2BU"},
                       {WiFiCardType::OPENHD_RTL_88X2CU, "OPENHD_RTL_88X2CU"},
+                      {WiFiCardType::OPENHD_RTL_88X2EU, "OPENHD_RTL_88X2EU"},
                       {WiFiCardType::OPENHD_RTL_8852BU, "OPENHD_RTL_8852BU"},
                       {WiFiCardType::RTL_88X2AU, "RTL_88X2AU"},
                       {WiFiCardType::RTL_88X2BU, "RTL_88X2BU"},
@@ -20,6 +21,7 @@ NLOHMANN_JSON_SERIALIZE_ENUM(
                       {WiFiCardType::INTEL, "INTEL"},
                       {WiFiCardType::BROADCOM, "BROADCOM"},
                       {WiFiCardType::AIC, "AIC"}
+                      {WiFiCardType::BROADCOM, "BROADCOM"},
                       //{WiFiCardType::, ""},
                   });
 
