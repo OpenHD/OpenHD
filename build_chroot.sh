d3b90ebe4659ee854576f0da980eed8d2a1cf23f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-02 15:42:41 +0100

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -21,9 +21,9 @@ echo ${CUSTOM}
 echo ${ARCH}
 
 if [[ "${DISTRO}" == "focal" ]]; then
-    apt update && apt upgrade -y
-    ./install_build_dep.sh rock5
-    apt install -y libv4l-dev sudo
+    sudo apt update && apt upgrade -y
+    sudo ./install_build_dep.sh rock5
+    sudo apt install -y libv4l-dev sudo
     echo "agx"
 elif [[ "${ARCH}" == "arm64" ]]; then
     ./install_build_dep.sh rock5
