14f4995dfac36fcd20bc82a78ba5b7e89f4171d5
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-02 19:04:51 +0100

    add qualcomm "postfix"

--- package.sh
+++ package.sh
@@ -7,65 +7,67 @@ OS="${3}"
 PKGDIR="/out/openhd-installdir/"
 VERSION="2.6.2-$(date '+%Y%m%d%H%M')-$(git rev-parse --short HEAD)"
 
+# Function to create the package directory structure
 create_package_directory() {
   rm -rf /tmp/openhd-installdir
   mkdir -p ${PKGDIR}usr/local/bin
   mkdir -p ${PKGDIR}tmp
-  #Nobody should create a topdir for such things like settings, but for now it'll stay
   mkdir -p ${PKGDIR}settings
   mkdir -p ${PKGDIR}etc/systemd/system
 
-  # We do not copy the openhd service for x86, since there we have launcher on the desktop
-  # (Otherwise, we always copy it)
+  # Copy systemd service based on architecture and custom type
   if [[ "${PACKAGE_ARCH}" != "x86_64" ]]; then
-    echo "we're not on x86"
-      if [[ "${CUSTOM}" == "standard" ]]; then
+    echo "Not x86 architecture"
+    if [[ "${CUSTOM}" == "standard" ]]; then
       cp systemd/openhd.service ${PKGDIR}etc/systemd/system/openhd.service || exit 1
-      else
-      cp systemd/openhd-x20.service ${PKGDIR}etc/systemd/system/openhd.service || exit 1
-      fi
     else
-      mkdir -p ${PKGDIR}usr/share/applications/
-      cp shortcuts/* ${PKGDIR}usr/share/applications/
-      mkdir -p ${PKGDIR}usr/local/share/openhd_misc/
-      cp shortcuts/OpenHD.ico ${PKGDIR}usr/local/share/openhd_misc/
-      mkdir -p ${PKGDIR}etc/profile.d/
-      cp desktop-truster.sh ${PKGDIR}etc/profile.d/
-      sudo chmod +777 ${PKGDIR}etc/profile.d/desktop-truster.sh
+      cp systemd/openhd-x20.service ${PKGDIR}etc/systemd/system/openhd.service || exit 1
+    fi
+  else
+    mkdir -p ${PKGDIR}usr/share/applications/
+    cp shortcuts/* ${PKGDIR}usr/share/applications/
+    mkdir -p ${PKGDIR}usr/local/share/openhd_misc/
+    cp shortcuts/OpenHD.ico ${PKGDIR}usr/local/share/openhd_misc/
+    mkdir -p ${PKGDIR}etc/profile.d/
+    cp desktop-truster.sh ${PKGDIR}etc/profile.d/
+    sudo chmod +777 ${PKGDIR}etc/profile.d/desktop-truster.sh
   fi
-  # always - copy the hardware.config file
+
+  # Copy hardware.config based on architecture and custom type
   if [[ "${PACKAGE_ARCH}" == "armhf" ]]; then
-      if [[ "${CUSTOM}" == "standard" ]]; then
+    if [[ "${CUSTOM}" == "standard" ]]; then
       mkdir -p "${PKGDIR}/boot/openhd/"
       cp OpenHD/ohd_common/config/hardware.config "${PKGDIR}/boot/openhd/hardware.config" || exit 1
-      else
+    else
       mkdir -p "${PKGDIR}/config/openhd/"
       cp OpenHD/ohd_common/config/hardware.config "${PKGDIR}/config/openhd/hardware.config" || exit 1
-      fi
-      mkdir -p "${PKGDIR}/config/openhd/"
-      cp OpenHD/ohd_common/config/hardware.config "${PKGDIR}/config/openhd/hardware.config" || exit 1
+    fi
   fi
- }
+}
+
+# Function to build the package
 build_package() {
 
+  # Set initial package name
   if [[ "${PACKAGE_ARCH}" == "armhf" ]]; then
     if [[ "${CUSTOM}" == "standard" ]]; then
       PACKAGE_NAME="openhd"
       PACKAGES="-d libpoco-dev -d libcamera-openhd -d gst-openhd-plugins -d iw -d nmap -d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
-      PLATFORM_CONFIGS=""
     else
       PACKAGE_NAME="openhd-x20"
       PACKAGES="-d libpoco-dev -d iw -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-tools"
-      PLATFORM_CONFIGS=""
     fi
   elif [[ "${PACKAGE_ARCH}" == "x86_64" ]]; then
     PACKAGE_NAME="openhd"
     PACKAGES="-d libpoco-dev -d dkms -d qopenhd -d git -d iw -d nmap -d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
-    PLATFORM_CONFIGS=""
   else
     PACKAGE_NAME="openhd"
     PACKAGES="-d libpoco-dev -d iw -d nmap -d aircrack-ng -d i2c-tools -d libv4l-dev -d libusb-1.0-0 -d libpcap-dev -d libnl-3-dev -d libnl-genl-3-dev -d libsdl2-2.0-0 -d libsodium-dev -d gstreamer1.0-plugins-base -d gstreamer1.0-plugins-good -d gstreamer1.0-plugins-bad -d gstreamer1.0-plugins-ugly -d gstreamer1.0-libav -d gstreamer1.0-tools -d gstreamer1.0-alsa -d gstreamer1.0-pulseaudio"
-    PLATFORM_CONFIGS=""
+  fi
+
+  # Check for the presence of qti-gstreamer1.0-plugins-bad-waylandsink and add -QCom to package name if installed
+  if dpkg -l | grep -q "qti-gstreamer1.0-plugins-bad-waylandsink"; then
+    PACKAGE_NAME="${PACKAGE_NAME}-QCom"
   fi
 
   rm "${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb" > /dev/null 2>&1 || true
@@ -73,18 +75,18 @@ build_package() {
   make -j4
   mkdir -p ${PKGDIR}/usr/local/bin/
   cp openhd ${PKGDIR}/usr/local/bin/ || exit 1
-  echo "$PWD"
-  ls -a  
-   # Assuming fpm is installed and properly configured
+
+  # Build the package using fpm
   fpm -a "${PACKAGE_ARCH}" -s dir -t deb -n "${PACKAGE_NAME}" -v "${VERSION}" -C "${PKGDIR}" \
-    ${PLATFORM_CONFIGS} \
     -p "${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb" \
     --after-install after-install.sh \
     --before-install before-install.sh \
     ${PACKAGES}
-  ls -a
+  
+  # Copy the resulting .deb package to the output directory
   cp *.deb /out/
 }
-#Main Build
+
+# Main execution
 create_package_directory
 build_package
