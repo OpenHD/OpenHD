e8488a8c35da5a41ac8a9771012fc3a0c24d1eff
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-12 19:58:01 +0100

    Update wifi_card_discovery.cpp

--- OpenHD/ohd_interface/src/wifi_card_discovery.cpp
+++ OpenHD/ohd_interface/src/wifi_card_discovery.cpp
@@ -18,21 +18,25 @@ static WiFiCardType driver_to_wifi_card_type(const std::string& driver_name) {
     return WiFiCardType::OPENHD_RTL_88X2AU;
   }
   if (OHDUtil::equal_after_uppercase(driver_name, "rtl88x2bu_ohd")) {
-    // NOTE: "rtw_8822bu" is the bad kernel driver which is fucking horrible.
+  // NOTE: "rtw_8822bu" is the bad kernel driver which is fucking horrible.
     return WiFiCardType::OPENHD_RTL_88X2BU;
+  }
+  // Experimental, not fully working card(s)
+  if (OHDUtil::equal_after_uppercase(driver_name, "rtl88x2eu_ohd")) {
+    return WiFiCardType::OPENHD_RTL_88X2EU;
+  }
+  if (OHDUtil::equal_after_uppercase(driver_name, "cnss_pci")) {
+    return WiFiCardType::QUALCOMM;
+  }
+  // The not supported, but maybe sometime in the future working card(s)
+  if (OHDUtil::equal_after_uppercase(driver_name, "rtl8852bu_ohd")) {
+    // NOTE: "rtw_8822bu" is the bad kernel driver which is fucking horrible.
+    return WiFiCardType::OPENHD_RTL_8852BU;
   }
   if (OHDUtil::equal_after_uppercase(driver_name, "rtl88x2cu_ohd")) {
     // NOTE: "rtw_8822cu" is the bad kernel driver which is fucking horrible.
     return WiFiCardType::OPENHD_RTL_88X2CU;
   }
-  if (OHDUtil::equal_after_uppercase(driver_name, "rtl88x2eu_ohd")) {
-    return WiFiCardType::OPENHD_RTL_88X2EU;
-  }
-  if (OHDUtil::equal_after_uppercase(driver_name, "rtl8852bu_ohd")) {
-    // NOTE: "rtw_8822bu" is the bad kernel driver which is fucking horrible.
-    return WiFiCardType::OPENHD_RTL_8852BU;
-  }
-  // The not supported, but perhaps working card(s)
   if (OHDUtil::contains_after_uppercase(driver_name, "ath9k")) {
     return WiFiCardType::ATHEROS;
   }
