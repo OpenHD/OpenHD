daaa43818fb3327327c37295b7ea0f5472ecb232
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-12 12:44:08 +0100

    set to 30 ecause qualcomm locked this to 30fps

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -521,8 +521,8 @@ struct XCamera {
     } else if (camera_type == X_CAM_TYPE_QC_IMX577) {
       std::vector<ResolutionFramerate> ret;
       // correct specs still missing
-      ret.push_back(ResolutionFramerate{1280, 720, 60});
-      ret.push_back(ResolutionFramerate{1920, 1080, 60});
+      ret.push_back(ResolutionFramerate{1280, 720, 30});
+      ret.push_back(ResolutionFramerate{1920, 1080, 30});
       return ret;
     } else if (camera_type == X_CAM_TYPE_QC_OV9282) {
       std::vector<ResolutionFramerate> ret;
