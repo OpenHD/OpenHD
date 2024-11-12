85096bd793cfe84e1dd7b2b0da70862830d00fbd
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-12 12:59:52 +0100

    testing features

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -520,9 +520,11 @@ struct XCamera {
       return ret;
     } else if (camera_type == X_CAM_TYPE_QC_IMX577) {
       std::vector<ResolutionFramerate> ret;
-      // correct specs still missing
+      //very basic integration, driver is missing a lot of settings
       ret.push_back(ResolutionFramerate{1280, 720, 30});
       ret.push_back(ResolutionFramerate{1920, 1080, 30});
+      // tested features:
+      // SHDR = Not implemented
       return ret;
     } else if (camera_type == X_CAM_TYPE_QC_OV9282) {
       std::vector<ResolutionFramerate> ret;
