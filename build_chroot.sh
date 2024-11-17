7111692350c232e7c8c3319bfd6b6625ff615e24
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-17 22:47:24 +0100

    Update build_chroot.sh

--- build_chroot.sh
+++ build_chroot.sh
@@ -39,7 +39,6 @@ echo "Arch: ${QCOM}"
 
 # Install dependencies based on DISTRO or ARCH
 if [[ "${QCOM}" == "coretronic" ]]; then
-echo "Hello World"
 sudo apt-get update && mkdir -p openhd_deps && cd openhd_deps && apt-cache depends --recurse --no-recommends --no-suggests openhd | grep "^\s*Depends:" | awk '{print $2}' | sort -u | xargs -I{} bash -c "apt-get download {} || echo 'Failed to download {}'" && tar -czf ../openhd_dependencies.tar.gz *.deb && cd .. && rm -rf openhd_deps
 ls -a
 else
