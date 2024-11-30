e4c986490f6fd3da1d06f58624f6ea71a37c9fc4
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-30 20:34:15 +0100

    Update wifi_command_helper.cpp

--- OpenHD/ohd_interface/src/wifi_command_helper.cpp
+++ OpenHD/ohd_interface/src/wifi_command_helper.cpp
@@ -125,24 +125,24 @@ bool wifi::commandhelper::iw_set_tx_power(const std::string &device,
   }
 
   // Generic logic for other devices
-  get_logger()->warn("Setting tx_power for device: {} to {} mBm", device,
+  get_logger()->debug("Setting tx_power for device: {} to {} mBm", device,
                      tx_power_mBm);
 
   std::vector<std::string> args{
       "dev", device, "set", "txpower", "fixed", std::to_string(tx_power_mBm)};
-  get_logger()->warn("Running command: iw with arguments: [{}]",
+  get_logger()->debug("Running command: iw with arguments: [{}]",
                      fmt::join(args, ", "));
 
   const auto ret = OHDUtil::run_command("iw", args);
   if (ret != 0) {
-    get_logger()->warn(
+    get_logger()->debug(
         "Failed to set tx_power for device: {}. Power: {} mBm, Return Code: "
         "{}. Command Args: [{}]",
         device, tx_power_mBm, ret, fmt::join(args, ", "));
     return false;
   }
 
-  get_logger()->warn("Successfully set tx_power for device: {} to {} mBm",
+  get_logger()->debug("Successfully set tx_power for device: {} to {} mBm",
                      device, tx_power_mBm);
   return true;
 }
