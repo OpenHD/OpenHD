0626a9b2e62695b7f05cec7ba718efa84e759c4f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-30 23:25:35 +0100

    Update OnboardComputerStatusProvider.cpp

--- OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
+++ OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
@@ -7,7 +7,7 @@
 #include "onboard_computer_status_rpi.hpp"
 #include "openhd_spdlog_include.h"
 #include "openhd_util_filesystem.h"
-#include "wb_link_settings.h"
+#include "../../ohd_interface/inc/wb_link_settings.h"
 
 // INA219 stuff
 constexpr float SHUNT_OHMS = 0.1f;
