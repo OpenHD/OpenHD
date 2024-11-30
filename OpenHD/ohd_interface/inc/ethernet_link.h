d007339fb077a96d257a7f18d755a23931916ce6
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-30 18:39:32 +0100

    Update ethernet_link.h

--- OpenHD/ohd_interface/inc/ethernet_link.h
+++ OpenHD/ohd_interface/inc/ethernet_link.h
@@ -10,7 +10,7 @@
 
 class EthernetLink : public OHDLink {
 public:
-    explicit EthernetLink(OHDProfile profile);
+    EthernetLink(const openhd::Config& config, OHDProfile profile);
     ~EthernetLink();
 
     // OHDLink implementations
@@ -20,12 +20,12 @@ public:
 
 private:
     OHDProfile m_profile;
-
-    // Configuration variables (private)
-    std::string GROUND_UNIT_IP = "192.168.2.1"; // Default IP
-    std::string AIR_UNIT_IP = "192.168.2.18";    // Default IP
-    int VIDEO_PORT = 5910;                      // Default video port
-    int TELEMETRY_PORT = 5920;                  // Default telemetry port
+    openhd::Config m_config;
+    // Configuration variables (defaults if not overridden)
+    std::string GROUND_UNIT_IP = "192.168.2.1";
+    std::string AIR_UNIT_IP = "192.168.2.18";
+    int VIDEO_PORT = 5910;
+    int TELEMETRY_PORT = 5920;
 
     std::unique_ptr<openhd::UDPForwarder> m_video_tx;   // Video transmitter
     std::unique_ptr<openhd::UDPReceiver> m_video_rx;   // Video receiver
