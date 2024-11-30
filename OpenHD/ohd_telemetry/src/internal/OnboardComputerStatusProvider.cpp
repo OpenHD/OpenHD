aab11e280ebfb40d6b112bcdab8d3094405ab88d
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-11-30 23:48:06 +0100

    Update OnboardComputerStatusProvider.cpp

--- OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
+++ OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
@@ -175,10 +175,9 @@ void OnboardComputerStatusProvider::calculate_other_until_terminate() {
       curr_rpi_undervolt = openhd::onboard::rpi::vcgencmd_get_undervolt();
     } else {
       const auto cpu_temp = (int8_t)openhd::onboard::readTemperature();
-      if (OHDFilesystemUtil::exists("/proc/net/rtl88x2eu/")) {
-      const auto txc_temp = 66;
-      }else{
       const auto txc_temp = 0;
+      if (OHDFilesystemUtil::exists("/proc/net/rtl88x2eu/")) {
+      txc_temp = 66;
       }
       const auto platform = OHDPlatform::instance();
       curr_temperature_core = cpu_temp;
