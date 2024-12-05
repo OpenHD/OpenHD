43e984aad4e312d400dc165f12e6971396e89d5c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-05 22:56:38 +0100

    more sh files

--- OpenHD/ohd_video/test/debug_audio.sh
+++ OpenHD/ohd_video/test/debug_audio.sh
@@ -1,5 +1,28 @@
 # bin/bash
 
+################################################################################
+# OpenHD
+# 
+# Licensed under the GNU General Public License (GPL) Version 3.
+# 
+# This software is provided "as-is," without warranty of any kind, express or 
+# implied, including but not limited to the warranties of merchantability, 
+# fitness for a particular purpose, and non-infringement. For details, see the 
+# full license in the LICENSE file provided with this source code.
+# 
+# Non-Military Use Only:
+# This software and its associated components are explicitly intended for 
+# civilian and non-military purposes. Use in any military or defense 
+# applications is strictly prohibited unless explicitly and individually 
+# licensed otherwise by the OpenHD Team.
+# 
+# Contributors:
+# A full list of contributors can be found at the OpenHD GitHub repository:
+# https://github.com/OpenHD
+# 
+# © OpenHD, All Rights Reserved.
+###############################################################################
+
 #gst-launch-1.0 -v udpsrc port=5610 ! "application/x-rtp,media=(string)audio, \
 #clock-rate=(int)44100, width=16, height=16, encoding-name=(string)L16,\
 #encoding-params=(string)1, channels=(int)1, channel-positions=(int)1, \
--- OpenHD/ohd_video/test/main_stream_debug.sh
+++ OpenHD/ohd_video/test/main_stream_debug.sh
@@ -1,4 +1,28 @@
 # bin/bash
+
+################################################################################
+# OpenHD
+# 
+# Licensed under the GNU General Public License (GPL) Version 3.
+# 
+# This software is provided "as-is," without warranty of any kind, express or 
+# implied, including but not limited to the warranties of merchantability, 
+# fitness for a particular purpose, and non-infringement. For details, see the 
+# full license in the LICENSE file provided with this source code.
+# 
+# Non-Military Use Only:
+# This software and its associated components are explicitly intended for 
+# civilian and non-military purposes. Use in any military or defense 
+# applications is strictly prohibited unless explicitly and individually 
+# licensed otherwise by the OpenHD Team.
+# 
+# Contributors:
+# A full list of contributors can be found at the OpenHD GitHub repository:
+# https://github.com/OpenHD
+# 
+# © OpenHD, All Rights Reserved.
+###############################################################################
+
 # run this to see if encoded data is coming in via udp
 # ( Garbage on the console means data is coming out ;) )
 # Use this on the air pi when OpenHD is not running, but test_video or test_dummy_gstreamer is running.
