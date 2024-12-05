415d3d19a36b968f162b1cfd1c13120f8ecb4019
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-05 23:04:15 +0100

    header

--- OpenHD/ohd_common/inc/config_paths.h
+++ OpenHD/ohd_common/inc/config_paths.h
@@ -1,7 +1,26 @@
-//
-// Created by Raphael on 7/15/24.
-// Made to hold several variables that can be used within OpenHD
-//
+/******************************************************************************
+ * OpenHD
+ * 
+ * Licensed under the GNU General Public License (GPL) Version 3.
+ * 
+ * This software is provided "as-is," without warranty of any kind, express or 
+ * implied, including but not limited to the warranties of merchantability, 
+ * fitness for a particular purpose, and non-infringement. For details, see the 
+ * full license in the LICENSE file provided with this source code.
+ * 
+ * Non-Military Use Only:
+ * This software and its associated components are explicitly intended for 
+ * civilian and non-military purposes. Use in any military or defense 
+ * applications is strictly prohibited unless explicitly and individually 
+ * licensed otherwise by the OpenHD Team.
+ * 
+ * Contributors:
+ * A full list of contributors can be found at the OpenHD GitHub repository:
+ * https://github.com/OpenHD
+ * 
+ * © OpenHD, All Rights Reserved.
+ ******************************************************************************/
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
