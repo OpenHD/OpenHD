455d10cc9790319cd646ed8cb0cb39e00df6027f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-05 22:43:51 +0100

    add .sh headers part 1

--- desktop-truster.sh
+++ desktop-truster.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+################################################################################
+# OpenHD
+# 
+# Licensed under the GNU General Public License (GPL) Version 3.
+# 
+# This software is provided "as-is," without warranty of any kind, express or 
+# implied, including but not limited to the warranties of merchantability, 
+# fitness for a particular purpose, and non-infringement. For details, see the 
+# full license in the LICENSE file provided with this source code.
+# 
+# Non-Military Use Only:
+# This software and its associated components are explicitly intended for 
+# civilian and non-military purposes. Use in any military or defense 
+# applications is strictly prohibited unless explicitly and individually 
+# licensed otherwise by the OpenHD Team.
+# 
+# Contributors:
+# A full list of contributors can be found at the OpenHD GitHub repository:
+# https://github.com/OpenHD
+# 
+# © OpenHD, All Rights Reserved.
+################################################################################
+
 # Trust all desktop files
 for i in ~/Desktop/*.desktop; do
   [ -f "${i}" ] || break
