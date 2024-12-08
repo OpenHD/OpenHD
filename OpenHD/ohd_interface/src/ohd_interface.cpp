cc43f16916d1831975db9cfc94ae0b25b5688019
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-08 22:41:35 +0100

    Update ohd_interface.cpp

--- OpenHD/ohd_interface/src/ohd_interface.cpp
+++ OpenHD/ohd_interface/src/ohd_interface.cpp
@@ -69,7 +69,7 @@ OHDInterface::OHDInterface(OHDProfile profile1)
   m_monitor_mode_cards = {};
   m_opt_hotspot_card = std::nullopt;
   const auto config = openhd::load_config();
-  bool microhard_device_present = is_microhard_device_present();
+  bool microhard_device_present = false;
 
   if (OHDFilesystemUtil::exists(std::string(getConfigBasePath()) +
                                 "ethernet.txt")) {
