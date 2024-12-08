6d8558e6c49988eb5d3ca41bde1ccd380217f5e3
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-08 04:01:03 +0100

    Update openhd_config.cpp

--- OpenHD/ohd_common/src/openhd_config.cpp
+++ OpenHD/ohd_common/src/openhd_config.cpp
@@ -40,6 +40,7 @@ void openhd::set_config_file(const std::string& config_file_path) {
 static openhd::Config load_or_default() {
   try {
     openhd::Config ret{};
+    std::cerr << "EXECUTED CONFIG SCRIPT" << std::endl;
     if (!OHDFilesystemUtil::exists(CONFIG_FILE_PATH)) {
       std::cerr << "WARN: No config file [" << CONFIG_FILE_PATH << "] used!" << std::endl;
       return ret;
