e3e9280f7c3808c38a9303fa81119acd894a96a0
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-08 23:04:21 +0100

    Update microhard_link.cpp

--- OpenHD/ohd_interface/src/microhard_link.cpp
+++ OpenHD/ohd_interface/src/microhard_link.cpp
@@ -60,19 +60,29 @@ const std::string command6 = "AT+MWNOISEFLOOR\n";
 const std::string command7 = "AT+MWSNR\n";
 
 // Parse hardware.config
-const auto config = openhd::load_config();
-static const auto MICROHARD_IP_RANGE = config.MICROHARD_IP_AIR;
-static const auto MICROHARD_AIR_IP = config.MICROHARD_IP_AIR;
-static const auto MICROHARD_GND_IP = config.MICROHARD_IP_GROUND;
-static const int MICROHARD_UDP_PORT_TELEMETRY_AIR_TX =
-    config.MICROHARD_TELEMETRY_PORT;
-static const int MICROHARD_UDP_PORT_VIDEO_AIR_TX = config.MICROHARD_VIDEO_PORT;
-static const std::string DEFAULT_DEVICE_IP_GND = config.GROUND_UNIT_IP;
-static const std::string DEFAULT_DEVICE_IP_AIR = config.AIR_UNIT_IP;
-const std::string username = config.MICROHARD_USERNAME + "\n";
-const std::string password = config.MICROHARD_PASSWORD + "\n";
+// const auto config = openhd::load_config();
+// static const auto MICROHARD_IP_RANGE = config.MICROHARD_IP_AIR;
+// static const auto MICROHARD_AIR_IP = config.MICROHARD_IP_AIR;
+// static const auto MICROHARD_GND_IP = config.MICROHARD_IP_GROUND;
+// static const int MICROHARD_UDP_PORT_TELEMETRY_AIR_TX =
+//     config.MICROHARD_TELEMETRY_PORT;
+// static const int MICROHARD_UDP_PORT_VIDEO_AIR_TX = config.MICROHARD_VIDEO_PORT;
+// static const std::string DEFAULT_DEVICE_IP_GND = config.GROUND_UNIT_IP;
+// static const std::string DEFAULT_DEVICE_IP_AIR = config.AIR_UNIT_IP;
+// const std::string username = config.MICROHARD_USERNAME + "\n";
+// const std::string password = config.MICROHARD_PASSWORD + "\n";
 
-// Helper function to retrieve IP addresses starting with a specific prefix
+static const auto MICROHARD_IP_RANGE = "192.168.168";
+static const auto MICROHARD_AIR_IP = "";
+static const auto MICROHARD_GND_IP = "";
+static const int MICROHARD_UDP_PORT_TELEMETRY_AIR_TX = 5000;
+static const int MICROHARD_UDP_PORT_VIDEO_AIR_TX = 5001;
+static const std::string DEFAULT_DEVICE_IP_GND = "";
+static const std::string DEFAULT_DEVICE_IP_AIR = "";
+const std::string username = "admin\n";
+const std::string password = "qwertz1\n";
+
+//Helper function to retrieve IP addresses starting with a specific prefix
 std::vector<std::string> get_ip_addresses(const std::string& prefix) {
   std::vector<std::string> ip_addresses;
   int sockfd;
