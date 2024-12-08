481295d638f901041ffc6490f9ec252b457f4136
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-12-08 22:26:09 +0100

    Update microhard_link.cpp

--- OpenHD/ohd_interface/src/microhard_link.cpp
+++ OpenHD/ohd_interface/src/microhard_link.cpp
@@ -59,18 +59,6 @@ const std::string command5 = "AT+MWVRATE\n";
 const std::string command6 = "AT+MWNOISEFLOOR\n";
 const std::string command7 = "AT+MWSNR\n";
 
-// Parse hardware.config
-// static const auto MICROHARD_IP_RANGE = openhd::load_config().MICROHARD_IP_RANGE;
-// static const auto MICROHARD_AIR_IP = openhd::load_config().MICROHARD_IP_AIR;
-// static const auto MICROHARD_GND_IP = openhd::load_config().MICROHARD_IP_GROUND;
-// static const int MICROHARD_UDP_PORT_TELEMETRY_AIR_TX =
-//     openhd::load_config().MICROHARD_TELEMETRY_PORT;
-// static const int MICROHARD_UDP_PORT_VIDEO_AIR_TX = openhd::load_config().MICROHARD_VIDEO_PORT;
-// static const std::string DEFAULT_DEVICE_IP_GND = openhd::load_config().GROUND_UNIT_IP;
-// static const std::string DEFAULT_DEVICE_IP_AIR = openhd::load_config().AIR_UNIT_IP;
-// const std::string username = openhd::load_config().MICROHARD_USERNAME + "\n";
-// const std::string password = openhd::load_config().MICROHARD_PASSWORD + "\n";
-
 // Helper function to retrieve IP addresses starting with a specific prefix
 std::vector<std::string> get_ip_addresses(const std::string& prefix) {
   std::vector<std::string> ip_addresses;
@@ -165,13 +153,13 @@ void communicate_with_device(const std::string& ip,
     // Login to the device
     std::this_thread::sleep_for(
         std::chrono::seconds(1));  // Wait for a second to process username
-    openhd::log::get_default()->debug("Sending username: {}", username);
-    stream << username << std::flush;
+    openhd::log::get_default()->debug("Sending username: {}", openhd::load_config().MICROHARD_USERNAME);
+    stream << openhd::load_config().MICROHARD_USERNAME << std::flush;
     std::this_thread::sleep_for(
         std::chrono::seconds(1));  // Wait for a second to process username
 
-    openhd::log::get_default()->debug("Sending password: {}", password);
-    stream << password << std::flush;
+    openhd::log::get_default()->debug("Sending password: {}", openhd::load_config().MICROHARD_PASSWORD);
+    stream << openhd::load_config().MICROHARD_PASSWORD << std::flush;
     std::this_thread::sleep_for(
         std::chrono::seconds(3));  // Wait for a second to process password
 
@@ -228,11 +216,11 @@ void communicate_with_device_slow(const std::string& ip,
     // Login to the device
     std::this_thread::sleep_for(std::chrono::seconds(1));
     openhd::log::get_default()->debug("Sending username: {}", username);
-    stream << username << std::flush;
+    stream << openhd::load_config().MICROHARD_USERNAME << std::flush;
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     openhd::log::get_default()->debug("Sending password: {}", password);
-    stream << password << std::flush;
+    stream << openhd::load_config().MICROHARD_PASSWORD << std::flush;
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
     const std::vector<std::pair<std::string, std::regex>> commands = {
