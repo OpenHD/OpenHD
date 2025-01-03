969fcb9cf2cbf4fab25102fcb1559ed58f8c12ea
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-03 23:44:44 +0100

    Update camera_settings.hpp

--- OpenHD/ohd_video/inc/camera_settings.hpp
+++ OpenHD/ohd_video/inc/camera_settings.hpp
@@ -68,6 +68,8 @@ struct CameraSettings {
   // using only telemetry / HUD. Default to true, otherwise we'd have conflicts
   // with the "always a picture without changing any settings" paradigm.
   bool enable_streaming = true;
+  int qp_max = qp_max;
+  int qp_min = qp_min;
   // The video format selected by the user. If the user sets a video format that
   // isn't supported (for example, he might select h264|1920x1080@120 but the
   // camera can only do 60fps) the camera might stop streaming, and the user has
@@ -161,6 +163,7 @@ static bool requires_hflip(const CameraSettings& settings) {
     return true;
   return false;
 }
+
 static bool requires_vflip(const CameraSettings& settings) {
   if (settings.openhd_flip == OPENHD_FLIP_VERTICAL ||
       settings.openhd_flip == OPENHD_FLIP_VERTICAL_AND_HORIZONTAL)
