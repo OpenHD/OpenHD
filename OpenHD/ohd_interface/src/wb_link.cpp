936fdefc0bfe799a5af2ed3316a4a40348d5958c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-03 22:35:52 +0100

    Update wb_link.cpp

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -665,7 +665,7 @@ std::vector<openhd::Setting> WBLink::get_all_settings() {
         openhd::IntSetting{(int)settings.enable_wb_video_variable_bitrate,
                            cb_enable_wb_video_variable_bitrate}});
     auto cb_wb_qp_max = [this](std::string, int value) {
-    if !(value < 0 || value > 51) { // Assuming valid QP range is 0 to 51
+    if (value > 0 || value < 51) {
         m_console->warn("Invalid wb_qp_max value: {}", value);
         return false;
     }
