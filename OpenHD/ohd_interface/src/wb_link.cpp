6e5a73efe3d132c9f5b397725399d6733764b89b
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-03 22:31:38 +0100

    Update wb_link.cpp

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -665,6 +665,7 @@ std::vector<openhd::Setting> WBLink::get_all_settings() {
         openhd::IntSetting{(int)settings.enable_wb_video_variable_bitrate,
                            cb_enable_wb_video_variable_bitrate}});
     auto cb_wb_qp_max = [this](std::string, int value) {
+    m_console->warn("muup", value);
     if (value < 0 || value > 51) { // Assuming valid QP range is 0 to 51
         m_console->warn("Invalid wb_qp_max value: {}", value);
         return false;
@@ -675,11 +676,10 @@ std::vector<openhd::Setting> WBLink::get_all_settings() {
 };
 ret.push_back(Setting{
     WB_QP_MAX,
-    m_console->warn("muup", value);
     openhd::IntSetting{(int)settings.wb_qp_max, cb_wb_qp_max}});
 
 auto cb_wb_qp_min = [this](std::string, int value) {
-          m_console->warn("moop", value);
+    m_console->warn("moop", value);
     if (value < 0 || value > 51 || value > m_settings->unsafe_get_settings().wb_qp_max) {
         m_console->warn("Invalid wb_qp_min value: {}", value);
         return false;
