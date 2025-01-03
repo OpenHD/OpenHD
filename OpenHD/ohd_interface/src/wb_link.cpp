8db7d200b8b6c9bae91bc5764f6c97516338b83a
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-03 14:44:33 +0100

    Update wb_link.cpp

--- OpenHD/ohd_interface/src/wb_link.cpp
+++ OpenHD/ohd_interface/src/wb_link.cpp
@@ -658,16 +658,19 @@ std::vector<openhd::Setting> WBLink::get_all_settings() {
     auto cb_enable_wb_video_variable_bitrate = [this](std::string, int value) {
       return set_air_enable_wb_video_variable_bitrate(value);
     };
-    ret.push_back(Setting{
-        WB_VIDEO_VARIABLE_BITRATE,
-        openhd::IntSetting{(int)settings.enable_wb_video_variable_bitrate,
-                           cb_enable_wb_video_variable_bitrate}});
     auto cb_wb_qp_max = [](std::string, int) {
-    return 4; // DEBUG
-    ret.push_back(Setting{
-    WB_QP_MAX,
-    openhd::IntSetting{4, cb_wb_qp_max}}); // DEBUG
+        return 4; // Static value
     };
+    ret.push_back(Setting{
+        WB_QP_MAX,
+        openhd::IntSetting{4, cb_wb_qp_max}});
+
+    auto cb_wb_qp_min = [](std::string, int) {
+        return 4; // Static value
+    };
+    ret.push_back(Setting{
+        WB_QP_MIN,
+        openhd::IntSetting{4, cb_wb_qp_min}});
     auto cb_wb_qp_min = [](std::string, int) {
     return 4; // DEBUG
     ret.push_back(Setting{
