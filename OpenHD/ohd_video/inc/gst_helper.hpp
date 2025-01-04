1ce538c04446dbff4dc06172da9174458f1c218f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-01-04 22:43:16 +0100

    Update gst_helper.hpp

--- OpenHD/ohd_video/inc/gst_helper.hpp
+++ OpenHD/ohd_video/inc/gst_helper.hpp
@@ -529,7 +529,7 @@ static std::string createRockchipEncoderPipeline(
     ss << "mpph265enc ";
   }
   ss << "rc-mode=cbr ";
-  // ss << "qp-min=" << settings.qp_min;
+  ss << "qp-min=" << settings.qp_min;
   // ss << "qp-max=" << settings.qp_max;
   ss << "bps="<< bps;
   ss << " width=" << settings.streamed_video_format.width;
