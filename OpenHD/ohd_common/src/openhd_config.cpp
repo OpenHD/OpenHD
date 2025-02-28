e181bf5c84bb2739fd000b7c42b6c6b8ae7e7b32
Raphael <raphael@openhdfpv.org>
2025-02-28 21:58:54 +0100

    Update openhd_config.cpp

--- OpenHD/ohd_common/src/openhd_config.cpp
+++ OpenHD/ohd_common/src/openhd_config.cpp
@@ -89,6 +89,7 @@ static openhd::Config load_or_default() {
     std::cout << "DEBUG: TELEMETRY_PORT: " << ret.TELEMETRY_PORT << std::endl;
 
     // Parse Ethernet link Microhard configuration
+    if (OHDFilesystemUtil::exists("/usr/local/share/openhd/debug.txt")) {
     std::cout << "WARN: Parsing Ethernet link Microhard configuration"
               << std::endl;
     ret.DISABLE_MICROHARD_DETECTION =
@@ -125,14 +126,13 @@ static openhd::Config load_or_default() {
         r.Get<int>("microhard", "MICROHARD_TELEMETRY_PORT", 5920);
     std::cout << "DEBUG: MICROHARD_TELEMETRY_PORT: " << ret.TELEMETRY_PORT
               << std::endl;
-
+            }
     // Parse Generic configuration
     ret.GEN_ENABLE_LAST_KNOWN_POSITION =
         r.Get<bool>("generic", "GEN_ENABLE_LAST_KNOWN_POSITION", false);
     ret.GEN_RF_METRICS_LEVEL = r.Get<int>("generic", "GEN_RF_METRICS_LEVEL", 0);
     ret.GEN_NO_QOPENHD_AUTOSTART =
         r.Get<bool>("generic", "GEN_NO_QOPENHD_AUTOSTART", false);
-
     return ret;
   } catch (std::exception& exception) {
     std::cerr << "ERROR: Ill-formatted config file: " << exception.what()
