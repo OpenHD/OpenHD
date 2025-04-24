bd6df38550f21413e8ed5cf16a35b43dea333be8
Raphael <raphael@openhdfpv.org>
2025-04-24 22:58:09 +0200

    Update camera.hpp

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -350,6 +350,7 @@ struct XCamera {
       std::vector<ResolutionFramerate> ret;
       ret.push_back(ResolutionFramerate{256, 192, 25});
       ret.push_back(ResolutionFramerate{0, 0, 0});
+      return ret;
     } else if (camera_type == X_CAM_TYPE_USB_INFIRAY_P2_PRO) {
       return {ResolutionFramerate{256, 192, 25}};
     } else if (camera_type == X_CAM_TYPE_USB_INFIRAY_X2) {
