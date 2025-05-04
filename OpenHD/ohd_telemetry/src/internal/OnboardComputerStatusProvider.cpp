3e461b710f6e2745ed3a32fb4ec92dba822806ed
Raphael <raphael@openhdfpv.org>
2025-05-04 18:50:24 +0200

    Update OnboardComputerStatusProvider.cpp

--- OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
+++ OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
@@ -207,21 +207,27 @@ void OnboardComputerStatusProvider::calculate_other_until_terminate() {
 
           size_t iface_index = 0;
           for (const auto& iface : interface_dirs) {
-            if (iface_index > 1) break;  // Only support two cards for now
-          
+            if (iface_index > 1) break;  // Only handle two Wi-Fi cards
+
             const std::string thermal_state_file = rtl88x2eu_proc_dir + iface + "/thermal_state";
-          
+
             if (OHDFilesystemUtil::exists(thermal_state_file)) {
               const std::string thermal_content = OHDFilesystemUtil::read_file(thermal_state_file);
               int temp = extract_temperature(thermal_content);
-          
-              if (iface_index == 0) {
-                m_curr_onboard_computer_status.temperature_core[1] = static_cast<int8_t>(temp);
-              } else if (iface_index == 1) {
-                m_curr_onboard_computer_status.temperature_core[2] = static_cast<int8_t>(temp);
+
+              if (temp > 0) {
+                if (iface_index == 0) {
+                  m_curr_onboard_computer_status.temperature_core[1] = static_cast<int8_t>(temp);
+                } else if (iface_index == 1) {
+                  m_curr_onboard_computer_status.temperature_core[2] = static_cast<int8_t>(temp);
+                }
+              } else {
+                openhd::log::get_default()->warn("Invalid temperature from [{}]: [{}]", iface, thermal_content);
               }
+            } else {
+              openhd::log::get_default()->warn("Missing thermal_state for interface [{}]", iface);
             }
-          
+
             ++iface_index;
           }
         }          
