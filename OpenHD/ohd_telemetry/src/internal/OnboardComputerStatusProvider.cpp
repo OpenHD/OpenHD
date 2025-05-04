70e8dd4c0897b0986dcf149ab1724e1ec3b95163
Raphael <raphael@openhdfpv.org>
2025-05-04 18:45:05 +0200

    Update OnboardComputerStatusProvider.cpp

--- OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
+++ OpenHD/ohd_telemetry/src/internal/OnboardComputerStatusProvider.cpp
@@ -205,23 +205,26 @@ void OnboardComputerStatusProvider::calculate_other_until_terminate() {
       const auto interface_dirs =
           OHDFilesystemUtil::getAllMatchingDirectoriesByPrefix(rtl88x2eu_proc_dir, "wl");
 
-      for (const auto& iface : interface_dirs) {
-        const std::string thermal_state_file = rtl88x2eu_proc_dir + iface + "/thermal_state";
-
-        if (OHDFilesystemUtil::exists(thermal_state_file)) {
-          const std::string thermal_content = OHDFilesystemUtil::read_file(thermal_state_file);
-          int temp = extract_temperature(thermal_content);
-
-          std::cout << "Interface: " << iface << ", Temperature: " << temp << "°C\n";
-
-          // Optionally update the global current temp
-          if (temp > curr_temperature_txc) {
-            curr_temperature_txc = static_cast<int8_t>(temp);
+          size_t iface_index = 0;
+          for (const auto& iface : interface_dirs) {
+            if (iface_index > 1) break;  // Only support two cards for now
+          
+            const std::string thermal_state_file = rtl88x2eu_proc_dir + iface + "/thermal_state";
+          
+            if (OHDFilesystemUtil::exists(thermal_state_file)) {
+              const std::string thermal_content = OHDFilesystemUtil::read_file(thermal_state_file);
+              int temp = extract_temperature(thermal_content);
+          
+              if (iface_index == 0) {
+                m_curr_onboard_computer_status.temperature_core[1] = static_cast<int8_t>(temp);
+              } else if (iface_index == 1) {
+                m_curr_onboard_computer_status.temperature_core[2] = static_cast<int8_t>(temp);
+              }
+            }
+          
+            ++iface_index;
           }
-        }
-      }
-    }
-
+        }          
     if (OHDPlatform::instance().is_rpi()) {
       curr_temperature_core =
           (int8_t)openhd::onboard::rpi::read_temperature_soc_degree();
