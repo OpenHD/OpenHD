d9d5fa7d7dbe297a0a6dcbeb0d3da9d6f7cf3b9f
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-07-24 22:38:37 +0200

    Update gst_helper.hpp

--- OpenHD/ohd_video/inc/gst_helper.hpp
+++ OpenHD/ohd_video/inc/gst_helper.hpp
@@ -632,60 +632,47 @@ static std::string createAllwinnerStream(const CameraSettings& settings) {
 /**
  * For WILLY Cameras
  */
-static int nxp_calculate_number_of_mbs_in_a_slice(int frame_height_px, int n_slices) {
-  if (n_slices < 2)
-    return 0;
-
-  int frame_mb_rows = (frame_height_px + 15) / 16;
-  if (n_slices > frame_mb_rows) {
-    openhd::log::get_default()->warn(
-        "Too many slices requested: frame_mb_rows={}, n_slices={}", frame_mb_rows, n_slices);
-    return frame_mb_rows;
-  }
-
-  int slice_row_mb = frame_mb_rows / n_slices;
-  if (frame_mb_rows % n_slices)
-    slice_row_mb++;
-
-  openhd::log::get_default()->debug(
-      "NXP slice calculation -> frame_height_px={}, n_slices={}, frame_mb_rows={}, mbs_per_slice={}",
-      frame_height_px, n_slices, frame_mb_rows, slice_row_mb);
-
-  return slice_row_mb;
-}
-
 static std::string create_willy_camera1_stream(const int device_index,
                                                const CameraSettings& settings) {
   std::stringstream ss;
-  const int bps = static_cast<int>(settings.h26x_bitrate_kbits * 800);
-  const bool use_slicing = settings.h26x_num_slices >= 2;
+  const int bps = (settings.h26x_bitrate_kbits * 0.8);
+  const int rotation = get_rotation_degree_qcom(settings);
 
-  std::string slicing_str;
-  if (use_slicing) {
-    const int mbs_per_slice = nxp_calculate_number_of_mbs_in_a_slice(
-        settings.streamed_video_format.height, settings.h26x_num_slices);
-    slicing_str = fmt::format(",number_of_mbs_in_a_slice={}", mbs_per_slice);
+  ss << fmt::format("v4l2src device=/dev/video3 ! ");
+  ss << fmt::format(
+      "video/x-raw,width=960,height=720,framerate=120/1,format=NV12 ! ");
+
+  const int qp_min = settings.qp_min;
+  const int qp_max = settings.qp_max;
+  const int gop_size = settings.h26x_keyframe_interval;
+  const int num_slices = std::max(settings.h26x_num_slices, 1);
+
+  switch (settings.streamed_video_format.videoCodec) {
+    case VideoCodec::H264:
+      ss << "vpuenc_h264 ";
+      ss << "bitrate=" << bps << " ";
+      ss << "gop-size=" << gop_size << " ";
+      ss << "qp-min=" << qp_min << " ";
+      ss << "qp-max=" << qp_max << " ";
+      ss << "stream-multislice=" << num_slices << " ";
+      ss << "! video/x-h264,stream-format=byte-stream,alignment=nal ! ";
+      break;
+
+    case VideoCodec::H265:
+      ss << "vpuenc_hevc ";
+      ss << "bitrate=" << bps << " ";
+      ss << "gop-size=" << gop_size << " ";
+      ss << "qp-min=" << qp_min << " ";
+      ss << "qp-max=" << qp_max << " ";
+      ss << "stream-multislice=" << num_slices << " ";
+      ss << "! video/x-h265,variant=itu ! ";
+      break;
+
+    default:
+      openhd::log::get_default()->error("WILLY camera: Unsupported codec");
+      return "";
   }
-  if (settings.streamed_video_format.videoCodec == VideoCodec::H264) {
-    ss << "vpuenc_h264 ";
-    ss << "bitrate=" << bitrate_kbps << " ";
-    ss << "gop-size=" << gop_size << " ";
-    ss << "qp-min=" << qp_min << " ";
-    ss << "qp-max=" << qp_max << " ";
-    ss << "stream-multislice=" << num_slices << " ";
-    ss << "! video/x-h264,stream-format=byte-stream,alignment=nal ! ";
-  } else if (settings.streamed_video_format.videoCodec == VideoCodec::H265) {
-    ss << "vpuenc_hevc ";
-    ss << "bitrate=" << bitrate_kbps << " ";
-    ss << "gop-size=" << gop_size << " ";
-    ss << "qp-min=" << qp_min << " ";
-    ss << "qp-max=" << qp_max << " ";
-    ss << "stream-multislice=" << num_slices << " ";
-    ss << "! video/x-h265,variant=itu ! ";
-  } else {
-    openhd::log::get_default()->error("IMX platform: unsupported codec selected");
-    return "";
-  }
+
   return ss.str();
 }
 
