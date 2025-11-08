24d682f6826e3e15028d117a8c8ebe252adca88d
Luka Panio <lukapanio@gmail.com>
2025-11-08 18:28:26 +0100

    ohd_video: camera: rv_pipline: Set 720p60 as default mode

--- OpenHD/ohd_video/inc/camera.hpp
+++ OpenHD/ohd_video/inc/camera.hpp
@@ -367,10 +367,10 @@ struct XCamera {
       return ret;
     } else if (requires_rockchip_rv_pipeline()) {
       std::vector<ResolutionFramerate> ret;
-      ret.push_back(ResolutionFramerate{1280, 720, 60});
       ret.push_back(ResolutionFramerate{1296, 968, 60});
       ret.push_back(ResolutionFramerate{1920, 1080, 25});
       ret.push_back(ResolutionFramerate{2592, 1944, 25});
+      ret.push_back(ResolutionFramerate{1280, 720, 60});
       return ret;
     } else if (camera_type == X_CAM_TYPE_USB_INFIRAY_P2_PRO) {
       return {ResolutionFramerate{256, 192, 25}};
