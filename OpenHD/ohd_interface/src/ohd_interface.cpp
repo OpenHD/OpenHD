90cf8e6f9895f108b982091bf56a3abe03769671
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-11-09 12:52:12 +0100

    Revert "Codex/clarify key usage during setup 0jize6 (#1245)"
    This reverts commit 695a81b55d3e8436cf668518d3bd6f5fb408e7ba.

--- OpenHD/ohd_interface/src/ohd_interface.cpp
+++ OpenHD/ohd_interface/src/ohd_interface.cpp
@@ -62,9 +62,8 @@ bool is_microhard_device_present() {
   return false;
 }
 
-OHDInterface::OHDInterface(OHDProfile profile1, bool disable_wifi_hotspot)
-    : m_profile(std::move(profile1)),
-      m_disable_wifi_hotspot(disable_wifi_hotspot) {
+OHDInterface::OHDInterface(OHDProfile profile1)
+    : m_profile(std::move(profile1)) {
   m_console = openhd::log::create_or_get("interface");
   assert(m_console);
   m_monitor_mode_cards = {};
@@ -126,7 +125,7 @@ OHDInterface::OHDInterface(OHDProfile profile1, bool disable_wifi_hotspot)
     // m_nw_settings.get_settings().ethernet_operating_mode
   }
   // Wi-Fi hotspot functionality if possible.
-    if (!m_disable_wifi_hotspot && m_opt_hotspot_card.has_value()) {
+  if (m_opt_hotspot_card.has_value()) {
     if (WiFiClient::create_if_enabled()) {
       // Wifi client active
     } else {
