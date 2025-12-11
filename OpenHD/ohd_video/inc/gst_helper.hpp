f49a6ed5e387ccb4c779329386f3ffdbc701c901
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-12-12 00:09:59 +0100

    Align A733 pipeline with gst-launch reference

--- OpenHD/ohd_video/inc/gst_helper.hpp
+++ OpenHD/ohd_video/inc/gst_helper.hpp
@@ -122,9 +122,9 @@ static std::string gst_create_rtp_caps(const VideoCodec& videoCodec) {
 static std::string create_rtp_packetize_for_codec(const VideoCodec codec,
                                                   const uint32_t mtu) {
   if (codec == VideoCodec::H264)
-    return fmt::format("rtph264pay mtu={} ! ", mtu);
+    return fmt::format("rtph264pay mtu={} pt=96 config-interval=-1 ! ", mtu);
   if (codec == VideoCodec::H265)
-    return fmt::format("rtph265pay mtu={} ! ", mtu);
+    return fmt::format("rtph265pay mtu={} config-interval=-1 ! ", mtu);
   assert(false);
   return "";
 }
@@ -616,8 +616,6 @@ static std::string createAllwinnerCsiStream(const CameraSettings& settings,
   ss << "queue max-size-buffers=4 leaky=downstream ! ";
   ss << "omxh264videoenc target-bitrate=" << bitrate_bits_per_second
      << " control-rate=constant ! ";
-  // Keep SPS / PPS inserted regularly for downstream compatibility
-  ss << "h264parse config-interval=1 ! ";
   return ss.str();
 }
 
@@ -825,6 +823,7 @@ static std::string create_parse_and_rtp_packetize(
   std::stringstream ss;
   ss << "queue ! ";
   ss << create_parse_for_codec(videoCodec);
+  ss << create_caps_nal(videoCodec, true);
   ss << create_rtp_packetize_for_codec(videoCodec, rtp_fragment_size);
   return ss.str();
 }
