7d51525d52ac630c78dc2db4aa9ab0be5d02eed4
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2026-01-08 00:00:10 +0100

    Update config_paths.cpp

--- OpenHD/ohd_common/src/config_paths.cpp
+++ OpenHD/ohd_common/src/config_paths.cpp
@@ -23,14 +23,9 @@
 
 #include "config_paths.h"
 
-#include <csignal>
 #include <cstdlib>
 #include <cstring>
-#include <fstream>
-#include <iostream>
-#include <memory>
 
-#include "openhd_platform.h"
 #include "openhd_util_filesystem.h"
 
 // Initial default paths
@@ -38,55 +33,15 @@ static char* CONFIG_BASE_PATH = nullptr;
 static char* VIDEO_PATH = nullptr;
 
 const char* getConfigBasePath() {
-  const auto platform_debug = OHDPlatform::instance();
-  if (platform_debug.is_rock()) {
-    return "/config/openhd/";
-  } else if (platform_debug.is_x20()) {
-    return "/config/openhd/";
-  } else {
-    return "/boot/openhd/";
-  }
+  return "/Config/";
 }
 
 const char* getVideoPath() {
-  static std::string cachedPath;
-  static const char* CACHE_FILE = "/usr/local/share/openhd/recording.txt";
-  static const char* FILENAME1 = "/Videos/external_video_part.txt";
-  static const char* FILENAME2 = "/external/Videos/external_video_part.txt";
-
-  if (!cachedPath.empty()) {
-    return cachedPath.c_str();
+  static const char* VIDEO_DIR = "/Video/";
+  if (!OHDFilesystemUtil::exists(VIDEO_DIR)) {
+    OHDFilesystemUtil::create_directories(VIDEO_DIR);
   }
-
-  if (OHDFilesystemUtil::exists(CACHE_FILE)) {
-    std::ifstream infile(CACHE_FILE);
-    if (infile) {
-      std::getline(infile, cachedPath);
-      return cachedPath.c_str();
-    }
-  }
-
-  const char* selectedPath = nullptr;
-
-  if (OHDFilesystemUtil::exists(FILENAME1)) {
-    selectedPath = VIDEO_PATH ? VIDEO_PATH : "/Videos/";
-    OHDFilesystemUtil::remove_if_existing(FILENAME1);
-  } else if (OHDFilesystemUtil::exists(FILENAME2)) {
-    selectedPath = VIDEO_PATH ? VIDEO_PATH : "/external/Videos/";
-    OHDFilesystemUtil::remove_if_existing(FILENAME2);
-  } else {
-    selectedPath = "/home/openhd/Videos/";
-    OHDFilesystemUtil::create_directories(selectedPath);
-  }
-
-  cachedPath = selectedPath;
-  OHDFilesystemUtil::create_directories("/usr/local/share/openhd/");
-  std::ofstream outfile(CACHE_FILE);
-  if (outfile) {
-    outfile << cachedPath;
-  }
-
-  return cachedPath.c_str();
+  return VIDEO_DIR;
 }
 
 void setConfigBasePath(const char* path) {
