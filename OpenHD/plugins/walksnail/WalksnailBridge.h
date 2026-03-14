920d8c81cec7b1a46082dc39b28836b5f46ab713
h1fired <misha01213@gmail.com>
2026-03-14 18:01:02 +0200

    Clean up

--- OpenHD/plugins/walksnail/WalksnailBridge.h
+++ OpenHD/plugins/walksnail/WalksnailBridge.h
@@ -3,7 +3,7 @@
 #include <thread>
 #include <mutex>
 
-#define WALKSNAIL_DEFAULT_UART      '/tmp/ttyV0'
+#define WALKSNAIL_DEFAULT_UART      "/tmp/ttyV0"
 #define WALKSNAIL_DEFAULT_BAUDRATE  115200
 
 class WalksnailBridge
