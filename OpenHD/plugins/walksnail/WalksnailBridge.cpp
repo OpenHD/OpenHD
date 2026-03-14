2dc8c077164676851be7defdf5f6fc2964a97c11
h1fired <misha01213@gmail.com>
2026-03-14 18:13:01 +0200

    Clean up

--- OpenHD/plugins/walksnail/WalksnailBridge.cpp
+++ OpenHD/plugins/walksnail/WalksnailBridge.cpp
@@ -5,8 +5,8 @@
 void WalksnailBridge::setup_bridge()
 {
     m_walksnail_serial = std::make_unique<Serial>(
-        WALKSNAIL_DEFAULT_UART,
-        WALKSNAIL_DEFAULT_BAUDRATE
+        "/tmp/ttyV0",
+        115200
     );
     if (!m_walksnail_serial->open())
     {
