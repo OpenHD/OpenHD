7eab8082c9289564d38f30a310f47f77451b9523
h1fired <misha01213@gmail.com>
2026-03-16 15:15:29 +0200

    Clean up

--- OpenHD/plugins/walksnail/WalksnailGround.cpp
+++ OpenHD/plugins/walksnail/WalksnailGround.cpp
@@ -43,7 +43,6 @@ void WalksnailGround::reading_loop()
         packet.push_back('\r');
         packet.push_back('\n');
 
-        std::cout << "Received packet (str):" << packet << std::endl;
         std::cout << "Packet (" << packet.size() << " bytes): ";
         for (unsigned char c : packet)
             std::cout << std::hex << std::setw(2) << std::setfill('0')
