c5964f32f8964d6db183d676c30e562cb2286820
h1fired <misha01213@gmail.com>
2026-03-19 14:14:31 +0200

    Added packet printing for air unit

--- OpenHD/plugins/walksnail/WalksnailAir.cpp
+++ OpenHD/plugins/walksnail/WalksnailAir.cpp
@@ -1,6 +1,7 @@
 #include "WalksnailAir.h"
 #include "WalksnailUtils.h"
 #include <iostream>
+#include <iomanip>
 
 void WalksnailAir::setup_bridge()
 {
@@ -30,6 +31,12 @@ void WalksnailAir::process_ground_messages(std::vector<MavlinkMessage> messages)
         if (msg.m.msgid == MAVLINK_MSG_ID_TUNNEL) {
             mavlink_tunnel_t tunnel;
             mavlink_msg_tunnel_decode(&msg.m, &tunnel);
+
+            std::cout << "Payload (" << tunnel.payload_length << " bytes): ";
+            for (uint8_t i = 0; i < tunnel.payload_length; i++)
+                std::cout << std::hex << std::setw(2) << std::setfill('0')
+                        << static_cast<int>(tunnel.payload[i]) << " ";
+            std::cout << std::dec << std::endl;
 
             if (m_walksnail_serial->isOpen()) {
                 m_walksnail_serial->write(tunnel.payload, tunnel.payload_length);
