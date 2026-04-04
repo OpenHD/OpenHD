249f343135045e59c997004680968409cf696d15
Luka Panio <lukapanio@gmail.com>
2026-04-04 14:53:39 +0200

    Initial X21(B) support

--- OpenHD/ohd_video/src/gstreamerstream.cpp
+++ OpenHD/ohd_video/src/gstreamerstream.cpp
@@ -221,6 +221,9 @@ std::string GStreamerStream::create_source_encode_pipeline(
   } else if (camera.camera_type == X_CAM_TYPE_ROCKCHIP_RV110X) {
     openhd::log::get_default()->warn("Using Rockchip RV camera type.");
     pipeline << OHDGstHelper::createRv1106Stream(setting);
+  } else if (camera.camera_type == X_CAM_TYPE_ROCKCHIP_RV1126) {
+    openhd::log::get_default()->warn("Using Rockchip RV1126 camera type.");
+    pipeline << OHDGstHelper::createRv1126Stream(setting);
   } else {
     openhd::log::get_default()->warn("UNKNOWN CAMERA TYPE");
     pipeline << OHDGstHelper::createDummyStreamX(setting);
